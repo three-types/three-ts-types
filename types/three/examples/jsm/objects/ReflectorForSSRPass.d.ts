fc2d0193c871c4f67965c316b52c08894dba0be4
Josh Ellis <joshua.ellis18@gmail.com>
2021-02-24 10:25:14 +0000

    feat: add ReflectorforSSRPass

---
+++ types/three/examples/jsm/objects/ReflectorForSSRPass.d.ts
@@ -0,0 +1,52 @@
+import {
+    Mesh,
+    ShaderMaterial,
+    WebGLRenderTarget,
+    BufferGeometry,
+    WebGLRenderer,
+    Scene,
+    Camera,
+} from '../../../src/Three';
+
+export interface ReflectorShader {
+    defines: {
+        isDistanceAttenuation: boolean;
+        isFresnel: boolean;
+    };
+    uniforms: {
+        [key: string]: any;
+    };
+    vertexShader: string;
+    fragmentShader: string;
+}
+
+export interface ReflectorOptions {
+    clipBias: number;
+    textureWidth: number;
+    textureHeight: number;
+    color: number;
+    useDepthTexture: boolean;
+    shader: ReflectorShader;
+}
+
+export class Reflector<TGeometry extends BufferGeometry = BufferGeometry> extends Mesh {
+    type: string;
+    options: object;
+
+    ReflectorShader: ReflectorShader;
+
+    needsUpdate: boolean;
+    maxDistance: number;
+    opacity: number;
+
+    _isDistanceAttenuation: boolean;
+    _isFresnel: boolean;
+
+    material: ShaderMaterial;
+
+    renderTarget: WebGLRenderTarget;
+
+    constructor(geometry: TGeometry, options: ReflectorOptions);
+
+    doRender: <TCamera extends Camera = Camera>(renderer: WebGLRenderer, scene: Scene, camera: TCamera) => void;
+}
