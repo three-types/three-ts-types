c8fd9542ec7a5a358798b98a28bc9832e98c557a
Josh Ellis <joshua.ellis18@gmail.com>
2021-03-30 23:39:20 +0100

    feat: add IFC Loader type

---
+++ types/three/examples/jsm/loaders/IFCLoader.d.ts
@@ -0,0 +1,17 @@
+import { Loader, LoadingManager, Object3D } from '../../../src/Three';
+
+interface IFC extends Object3D {
+    [key: string]: any;
+}
+
+export class IFCLoader extends Loader {
+    constructor(manager?: LoadingManager);
+    load(
+        url: string,
+        onLoad: (ifc: IFC) => void,
+        onProgress?: (event: ProgressEvent) => void,
+        onError?: (event: ErrorEvent) => void,
+    ): IFC;
+
+    parse(data: ArrayBuffer | string): void;
+}
