daed7e0d3a43b5d7a213081be790aaad8181bde8
ycw <ycw7788@gmail.com>
2021-06-03 12:55:21 +0800

    prettier --write test file

--- types/three/test/postprocessing/postprocessing-effectcomposer-pass.ts
+++ types/three/test/postprocessing/postprocessing-effectcomposer-pass.ts
@@ -2,28 +2,30 @@ import * as THREE from 'three';
 import { EffectComposer, Pass, FullScreenQuad } from 'three/examples/jsm/postprocessing/EffectComposer';
 
 class FooPass extends Pass {
-  fsQuad: FullScreenQuad;
-  constructor() {
-    super();
-    this.fsQuad = new FullScreenQuad(new THREE.ShaderMaterial({
-      vertexShader: `
+    fsQuad: FullScreenQuad;
+    constructor() {
+        super();
+        this.fsQuad = new FullScreenQuad(
+            new THREE.ShaderMaterial({
+                vertexShader: `
       varying vec2 vUv;
       void main() {
         vUv = uv;
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
       }`,
-      fragmentShader: `
+                fragmentShader: `
       varying vec2 vUv;
       void main() {
         gl_FragColor = vec4(vUv, 1., 1.);
-      }`
-    }));
-  }
-  render(renderer: THREE.WebGLRenderer, writeBuffer: THREE.WebGLRenderTarget) {
-    renderer.setRenderTarget(this.renderToScreen ? null : writeBuffer);
-    if (this.clear) renderer.clear();
-    this.fsQuad.render(renderer);
-  }
+      }`,
+            }),
+        );
+    }
+    render(renderer: THREE.WebGLRenderer, writeBuffer: THREE.WebGLRenderTarget) {
+        renderer.setRenderTarget(this.renderToScreen ? null : writeBuffer);
+        if (this.clear) renderer.clear();
+        this.fsQuad.render(renderer);
+    }
 }
 
 const renderer = new THREE.WebGLRenderer();
@@ -32,10 +34,10 @@ composer.addPass(new FooPass());
 renderer.setAnimationLoop(() => composer.render());
 
 function resize(w = 0, h = 0, dpr = devicePixelRatio) {
-  renderer.setPixelRatio(dpr);
-  renderer.setSize(w, h, false);
-  composer.setPixelRatio(dpr);
-  composer.setSize(w, h);
+    renderer.setPixelRatio(dpr);
+    renderer.setSize(w, h, false);
+    composer.setPixelRatio(dpr);
+    composer.setSize(w, h);
 }
 addEventListener('resize', () => resize(innerWidth, innerHeight));
 dispatchEvent(new Event('resize'));
