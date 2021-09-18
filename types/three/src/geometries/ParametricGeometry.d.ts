e0988a747e3fa96e93325c472798d4078f1835b9
Josh <37798644+joshuaellis@users.noreply.github.com>
2021-09-18 18:45:47 +0100

    feat: make params optional

--- types/three/src/geometries/ParametricGeometry.d.ts
+++ types/three/src/geometries/ParametricGeometry.d.ts
@@ -2,7 +2,7 @@ import { Vector3 } from './../math/Vector3';
 import { BufferGeometry } from './../core/BufferGeometry';
 
 export class ParametricGeometry extends BufferGeometry {
-    constructor(func: (u: number, v: number, dest: Vector3) => void, slices: number, stacks: number);
+    constructor(func?: (u: number, v: number, dest: Vector3) => void, slices?: number, stacks?: number);
 
     /**
      * @default 'ParametricGeometry'
