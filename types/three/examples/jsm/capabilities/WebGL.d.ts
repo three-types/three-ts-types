5c1be79fe66ef458c6bf277eedbc9438b5dc1349
Tom <t.schwarz@axial3d.com>
2022-02-22 16:10:37 +0000

    WebGL export type changed to default

--- types/three/examples/jsm/capabilities/WebGL.d.ts
+++ types/three/examples/jsm/capabilities/WebGL.d.ts
@@ -1,7 +1,9 @@
-export namespace WebGL {
+declare namespace WebGL {
     function isWebGLAvailable(): boolean;
     function isWebGL2Available(): boolean;
     function getWebGLErrorMessage(): HTMLElement;
     function getWebGL2ErrorMessage(): HTMLElement;
     function getErrorMessage(version: number): HTMLElement;
 }
+
+export default WebGL;
