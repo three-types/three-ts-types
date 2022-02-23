4aa3504225901ca39bf496ae1678bf0fafc8534d
Michał Mrozek <Michsior14@users.noreply.github.com>
2022-02-23 21:35:21 +0100

    fix: adjust webgl example location and types (#168)
    * fix: adjust webgl example location and types
    
    * test: fix webgl paths
    
    * fix: switch back to namespace for WEBGL

--- types/three/examples/jsm/capabilities/WebGL.d.ts
+++ types/three/examples/jsm/capabilities/WebGL.d.ts
@@ -1,7 +1,9 @@
-export namespace WEBGL {
+declare namespace WEBGL {
     function isWebGLAvailable(): boolean;
     function isWebGL2Available(): boolean;
     function getWebGLErrorMessage(): HTMLElement;
     function getWebGL2ErrorMessage(): HTMLElement;
     function getErrorMessage(version: number): HTMLElement;
 }
+
+export default WEBGL;
