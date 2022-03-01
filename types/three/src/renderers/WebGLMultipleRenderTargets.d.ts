f2f556f392b60f1cd388b2131516d0394f0b8d24
0b5vr <0b5vr@0b5vr.com>
2022-03-01 16:56:36 +0900

    refactor (WebGLMultipleRenderTargets): fix a lint issue

--- types/three/src/renderers/WebGLMultipleRenderTargets.d.ts
+++ types/three/src/renderers/WebGLMultipleRenderTargets.d.ts
@@ -15,7 +15,8 @@ export class WebGLMultipleRenderTargets extends EventDispatcher {
      * @param width The width of the render target.
      * @param height The height of the render target.
      * @param count The number of render targets.
-     * @param options object that holds texture parameters for an auto-generated target texture and depthBuffer/stencilBuffer booleans. For an explanation of the texture parameters see {@link Texture}.
+     * @param options object that holds texture parameters for an auto-generated target texture and depthBuffer/stencilBuffer booleans.
+     * For an explanation of the texture parameters see {@link Texture}.
      */
     constructor(width: number, height: number, count: number, options?: WebGLRenderTargetOptions);
 
