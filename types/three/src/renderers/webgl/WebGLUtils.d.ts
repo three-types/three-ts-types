e6de39eff649aa6bb25896c2dbe8ea65605c4f1e
David Peicho <davidhenri.peicho@siemens-healthineers.com>
2022-03-16 17:20:33 +0100

    WebGLUtils.d.ts: make convert() return null or a number

--- types/three/src/renderers/webgl/WebGLUtils.d.ts
+++ types/three/src/renderers/webgl/WebGLUtils.d.ts
@@ -3,5 +3,5 @@ import { CompressedPixelFormat, PixelFormat, TextureEncoding } from '../../const
 export class WebGLUtils {
     constructor(gl: WebGLRenderingContext | WebGL2RenderingContext, extensions: any, capabilities: any);
 
-    convert(p: PixelFormat | CompressedPixelFormat, encoding?: TextureEncoding | null): void;
+    convert(p: PixelFormat | CompressedPixelFormat, encoding?: TextureEncoding | null): number | null;
 }
