3a8a718cb480a2c169de0b74c9abeb8ff605409b
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-03-26 16:42:09 +0000

    feat: add computeTangents

--- types/three/examples/jsm/utils/BufferGeometryUtils.d.ts
+++ types/three/examples/jsm/utils/BufferGeometryUtils.d.ts
@@ -15,3 +15,4 @@ export function estimateBytesUsed(geometry: BufferGeometry): number;
 export function mergeVertices(geometry: BufferGeometry, tolerance?: number): BufferGeometry;
 export function toTrianglesDrawMode(geometry: BufferGeometry, drawMode: TrianglesDrawModes): BufferGeometry;
 export function computeMorphedAttributes(object: Mesh | Line | Points): object;
+export function computeTangents(geometry: BufferGeometry, negateSign?: boolean): BufferGeometry;
