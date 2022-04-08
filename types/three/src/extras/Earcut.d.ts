c30622375e63d85adf7b58b40ccb9005254306a8
Subhankar Pal <subhankar@trisetra.com>
2022-04-08 18:28:58 +0530

    fixed relative import issue

--- types/three/src/extras/Earcut.d.ts
+++ types/three/src/extras/Earcut.d.ts
@@ -1,4 +1,4 @@
-import { Triangle } from 'three';
+import { Triangle } from '../Three';
 export namespace Earcut {
     function triangulate(data: number[], holeIndices: number[], dim: number): Triangle[];
 }
