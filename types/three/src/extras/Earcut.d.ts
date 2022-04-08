f3ad1f36198a0ea2f7b8d6bcb7bb7e7ffb6c5f76
Subhankar Pal <subhankar@trisetra.com>
2022-04-08 07:57:08 +0530

    update

--- types/three/src/extras/Earcut.d.ts
+++ types/three/src/extras/Earcut.d.ts
@@ -1,4 +1,4 @@
 import { Triangle } from 'three';
 export namespace Earcut {
-    function triangulate( data: number[], holeIndices: number[], dim: number): Triangle[];
+    function triangulate(data: number[], holeIndices: number[], dim: number): Triangle[];
 }
