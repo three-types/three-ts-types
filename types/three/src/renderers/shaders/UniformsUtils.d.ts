87c67e639ac2e1e354af54a12f2dc3e7af90840c
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-07-28 16:00:59 +0100

    feat: add UniformGroups

--- types/three/src/renderers/shaders/UniformsUtils.d.ts
+++ types/three/src/renderers/shaders/UniformsUtils.d.ts
@@ -1,5 +1,10 @@
-export function cloneUniforms(uniforms_src: any): any;
-export function mergeUniforms(uniforms: any[]): any;
+import { Uniform } from '../../core/Uniform';
+import { UniformsGroup } from '../../core/UniformsGroup';
+
+export function cloneUniforms(uniforms_src: Uniform): Uniform;
+export function mergeUniforms(uniforms: Uniform[]): Uniform;
+
+export function cloneUniformsGroups(src: UniformsGroup[]): UniformsGroup[];
 
 export namespace UniformsUtils {
     export { mergeUniforms as merge, cloneUniforms as clone };
