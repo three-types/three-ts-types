b0444cf4623fab21676817abf735e8aec8293343
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-09-09 13:08:47 -0500

    fix(Mesh): material default is MeshBasicMaterial

--- types/three/src/objects/Mesh.d.ts
+++ types/three/src/objects/Mesh.d.ts
@@ -2,11 +2,12 @@ import { Material } from './../materials/Material';
 import { Raycaster } from './../core/Raycaster';
 import { Object3D } from './../core/Object3D';
 import { BufferGeometry } from '../core/BufferGeometry';
+import { MeshBasicMaterial } from '../materials/MeshBasicMaterial';
 import { Intersection } from '../core/Raycaster';
 
 export class Mesh<
     TGeometry extends BufferGeometry = BufferGeometry,
-    TMaterial extends Material | Material[] = Material | Material[],
+    TMaterial extends Material | Material[] = MeshBasicMaterial,
 > extends Object3D {
     constructor(geometry?: TGeometry, material?: TMaterial);
 
