93fc941c3885f253f0d45c03dfedc4d46726d080
Servin Nissen <servin.nissen@gmail.com>
2022-11-06 19:26:31 +0100

    Remove comments

--- types/three/src/textures/CompressedArrayTexture.d.ts
+++ types/three/src/textures/CompressedArrayTexture.d.ts
@@ -2,13 +2,6 @@ import { CompressedTexture } from './CompressedTexture';
 import { Wrapping, CompressedPixelFormat, TextureDataType } from '../constants';
 
 export class CompressedArrayTexture extends CompressedTexture {
-    /**
-     * @param mipmaps
-     * @param width
-     * @param height
-     * @param [format=THREE.RGBAFormat]
-     * @param [type=THREE.UnsignedByteType]
-     */
     constructor(
         mipmaps: ImageData[],
         width: number,
