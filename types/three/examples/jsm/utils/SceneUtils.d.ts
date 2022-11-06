80222510096da98da4a8ae3920eabdbe585aa690
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-11-06 20:15:28 +0000

    feat: sceneUtils add reduceVertices

--- types/three/examples/jsm/utils/SceneUtils.d.ts
+++ types/three/examples/jsm/utils/SceneUtils.d.ts
@@ -1,4 +1,4 @@
-import { BufferGeometry, Group, InstancedMesh, Material, Object3D, Scene, Mesh } from '../../../src/Three';
+import { BufferGeometry, Group, InstancedMesh, Material, Object3D, Scene, Mesh, Vector3 } from '../../../src/Three';
 
 export function createMeshesFromInstancedMesh(instancedMesh: InstancedMesh): Group;
 export function createMultiMaterialObject(geometry: BufferGeometry, materials: Material[]): Group;
@@ -14,3 +14,9 @@ export function attach(child: Object3D, scene: Scene, parent: Object3D): void;
 export function createMeshesFromMultiMaterialMesh(mesh: Mesh): Group;
 
 export function sortInstancedMesh(mesh: InstancedMesh, compareFn: (a: number, b: number) => number): void;
+
+export function reduceVertices<TValue>(
+    object: Object3D,
+    func: (value: TValue, vertex: Vector3) => TValue,
+    initialValue: TValue,
+): TValue;
