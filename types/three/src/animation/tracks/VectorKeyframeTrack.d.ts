fe1448558fefbfef74f89a9aa2c80e5ff89437b4
Suprhimp <aswwwcom@gmail.com>
2022-12-26 09:33:06 +0900

    type to ArrayLike

--- types/three/src/animation/tracks/VectorKeyframeTrack.d.ts
+++ types/three/src/animation/tracks/VectorKeyframeTrack.d.ts
@@ -2,7 +2,7 @@ import { KeyframeTrack } from './../KeyframeTrack';
 import { InterpolationModes } from '../../constants';
 
 export class VectorKeyframeTrack extends KeyframeTrack {
-    constructor(name: string, times: number[], values: number[], interpolation?: InterpolationModes);
+    constructor(name: string, times: ArrayLike<number>, values: ArrayLike<number>, interpolation?: InterpolationModes);
 
     /**
      * @default 'vector'
