acc545d16bb5b25dec5a0dba5d32a64c2e199314
Dan Rose <DanOfTheRoses@gmail.com>
2023-02-09 15:43:49 -0600

    recognize #hex and rgb/hsl colors

--- types/three/src/utils.d.ts
+++ types/three/src/utils.d.ts
@@ -1,3 +1,6 @@
 import { Color, ColorKeyword } from './math/Color';
 
-export type ColorRepresentation = Color | ColorKeyword | number;
+export type ColorModelString = `${'rgb' | 'hsl'}(${string})`;
+export type HexColorString = `#${string & { length: 3 | 6 }}`;
+
+export type ColorRepresentation = Color | ColorKeyword | ColorModelString | HexColorString | number;
