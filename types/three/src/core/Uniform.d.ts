0a270a4edf309d7d124e6f07f822d9ad0691432c
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2023-02-20 15:36:27 -0800

    chore: Remove deprecated code

--- types/three/src/core/Uniform.d.ts
+++ types/three/src/core/Uniform.d.ts
@@ -1,21 +1,6 @@
 export class Uniform<T = any> {
-    constructor(value: T);
-    /**
-     * @deprecated
-     */
-    constructor(type: string, value: T);
-    /**
-     * @deprecated
-     */
-    type: string;
-    value: T;
-    /**
-     * @deprecated Use {@link Object3D#onBeforeRender object.onBeforeRender()} instead.
-     */
-    dynamic: boolean;
 
-    /**
-     * @deprecated Use {@link Object3D#onBeforeRender object.onBeforeRender()} instead.
-     */
-    onUpdate(callback: () => void): Uniform<T>;
+    constructor(value: T);
+
+    value: T;
 }
