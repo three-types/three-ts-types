30ca42f2eddf38f69372304d53141e2b3eb79f95
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2023-02-20 15:21:30 -0800

    docs: Update TSDocs

--- types/three/src/core/Clock.d.ts
+++ types/three/src/core/Clock.d.ts
@@ -9,24 +9,24 @@
 export class Clock {
     /**
      * Create a new instance of {@link THREE.Clock | Clock}
-     * @param autoStart - Whether to automatically start the clock when {@link Clock.getDelta | .getDelta}() is called for the first time. Default `true`
+     * @param autoStart - Whether to automatically start the clock when {@link getDelta | .getDelta()} is called for the first time. Default `true`
      */
     constructor(autoStart?: boolean);
 
     /**
-     * If set, starts the clock automatically when {@link Clock.getDelta | .getDelta}() is called for the first time.
+     * If set, starts the clock automatically when {@link getDelta | .getDelta()} is called for the first time.
      * @defaultValue `true`
      */
     autoStart: boolean;
 
     /**
-     * Holds the time at which the clock's {@link Clock.start | start} method was last called.
+     * Holds the time at which the clock's {@link start | .start()} method was last called.
      * @defaultValue `0`
      */
     startTime: number;
 
     /**
-     * Holds the time at which the clock's {@link Clock.start | start}, {@link Clock.getElapsedTime | .getElapsedTime}() or {@link Clock.getDelta | .getDelta}() methods were last called.
+     * Holds the time at which the clock's {@link start | .start()}, {@link getElapsedTime | .getElapsedTime()} or {@link getDelta | .getDelta()} methods were last called.
      * @defaultValue `0`
      */
     oldTime: number;
@@ -46,27 +46,27 @@ export class Clock {
     /**
      * Starts clock
      * @remarks
-     * Also sets the {@link Clock.startTime | .startTime} and {@link Clock.oldTime | .oldTime} to the current time,
-     * sets {@link Clock.elapsedTime | .elapsedTime} to `0` and {@link Clock.running | .running} to `true`.
+     * Also sets the {@link startTime | .startTime} and {@link oldTime | .oldTime} to the current time,
+     * sets {@link elapsedTime | .elapsedTime} to `0` and {@link running | .running} to `true`.
      */
     start(): void;
 
     /**
-     * Stops clock and sets {@link Clock.oldTime | oldTime} to the current time.
+     * Stops clock and sets {@link oldTime | oldTime} to the current time.
      */
     stop(): void;
 
     /**
-     * Get the seconds passed since the clock started and sets {@link Clock.oldTime | .oldTime} to the current time.
+     * Get the seconds passed since the clock started and sets {@link oldTime | .oldTime} to the current time.
      * @remarks
-     * If {@link Clock.autoStart | .autoStart} is `true` and the clock is not running, also starts the clock.
+     * If {@link autoStart | .autoStart} is `true` and the clock is not running, also starts the clock.
      */
     getElapsedTime(): number;
 
     /**
-     * Get the seconds passed since the time {@link Clock.oldTime | .oldTime} was set and sets {@link Clock.oldTime | .oldTime} to the current time.
+     * Get the seconds passed since the time {@link oldTime | .oldTime} was set and sets {@link oldTime | .oldTime} to the current time.
      * @remarks
-     * If {@link Clock.autoStart | .autoStart} is `true` and the clock is not running, also starts the clock.
+     * If {@link autoStart | .autoStart} is `true` and the clock is not running, also starts the clock.
      */
     getDelta(): number;
 }
