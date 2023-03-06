45023bc85d22218b14534cdb6dc40db63da310b4
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2023-03-06 00:31:05 -0800

    docs: Update TSDocs

--- types/three/src/constants.d.ts
+++ types/three/src/constants.d.ts
@@ -627,6 +627,58 @@ export type GLSLVersion = typeof GLSL1 | typeof GLSL3;
 
 /**
  * For use with a texture's {@link THREE.Texture.internalFormat} property, these define how elements of a {@link THREE.Texture}, or texels, are stored on the GPU.
+ * - `R8` stores the red component on 8 bits.
+ * - `R8_SNORM` stores the red component on 8 bits. The component is stored as normalized.
+ * - `R8I` stores the red component on 8 bits. The component is stored as an integer.
+ * - `R8UI` stores the red component on 8 bits. The component is stored as an unsigned integer.
+ * - `R16I` stores the red component on 16 bits. The component is stored as an integer.
+ * - `R16UI` stores the red component on 16 bits. The component is stored as an unsigned integer.
+ * - `R16F` stores the red component on 16 bits. The component is stored as floating point.
+ * - `R32I` stores the red component on 32 bits. The component is stored as an integer.
+ * - `R32UI` stores the red component on 32 bits. The component is stored as an unsigned integer.
+ * - `R32F` stores the red component on 32 bits. The component is stored as floating point.
+ * - `RG8` stores the red and green components on 8 bits each.
+ * - `RG8_SNORM` stores the red and green components on 8 bits each. Every component is stored as normalized.
+ * - `RG8I` stores the red and green components on 8 bits each. Every component is stored as an integer.
+ * - `RG8UI` stores the red and green components on 8 bits each. Every component is stored as an unsigned integer.
+ * - `RG16I` stores the red and green components on 16 bits each. Every component is stored as an integer.
+ * - `RG16UI` stores the red and green components on 16 bits each. Every component is stored as an unsigned integer.
+ * - `RG16F` stores the red and green components on 16 bits each. Every component is stored as floating point.
+ * - `RG32I` stores the red and green components on 32 bits each. Every component is stored as an integer.
+ * - `RG32UI` stores the red and green components on 32 bits. Every component is stored as an unsigned integer.
+ * - `RG32F` stores the red and green components on 32 bits. Every component is stored as floating point.
+ * - `RGB8` stores the red, green, and blue components on 8 bits each. RGB8_SNORM` stores the red, green, and blue components on 8 bits each. Every component is stored as normalized.
+ * - `RGB8I` stores the red, green, and blue components on 8 bits each. Every component is stored as an integer.
+ * - `RGB8UI` stores the red, green, and blue components on 8 bits each. Every component is stored as an unsigned integer.
+ * - `RGB16I` stores the red, green, and blue components on 16 bits each. Every component is stored as an integer.
+ * - `RGB16UI` stores the red, green, and blue components on 16 bits each. Every component is stored as an unsigned integer.
+ * - `RGB16F` stores the red, green, and blue components on 16 bits each. Every component is stored as floating point
+ * - `RGB32I` stores the red, green, and blue components on 32 bits each. Every component is stored as an integer.
+ * - `RGB32UI` stores the red, green, and blue components on 32 bits each. Every component is stored as an unsigned integer.
+ * - `RGB32F` stores the red, green, and blue components on 32 bits each. Every component is stored as floating point
+ * - `R11F_G11F_B10F` stores the red, green, and blue components respectively on 11 bits, 11 bits, and 10bits. Every component is stored as floating point.
+ * - `RGB565` stores the red, green, and blue components respectively on 5 bits, 6 bits, and 5 bits.
+ * - `RGB9_E5` stores the red, green, and blue components on 9 bits each.
+ * - `RGBA8` stores the red, green, blue, and alpha components on 8 bits each.
+ * - `RGBA8_SNORM` stores the red, green, blue, and alpha components on 8 bits. Every component is stored as normalized.
+ * - `RGBA8I` stores the red, green, blue, and alpha components on 8 bits each. Every component is stored as an integer.
+ * - `RGBA8UI` stores the red, green, blue, and alpha components on 8 bits. Every component is stored as an unsigned integer.
+ * - `RGBA16I` stores the red, green, blue, and alpha components on 16 bits. Every component is stored as an integer.
+ * - `RGBA16UI` stores the red, green, blue, and alpha components on 16 bits. Every component is stored as an unsigned integer.
+ * - `RGBA16F` stores the red, green, blue, and alpha components on 16 bits. Every component is stored as floating point.
+ * - `RGBA32I` stores the red, green, blue, and alpha components on 32 bits. Every component is stored as an integer.
+ * - `RGBA32UI` stores the red, green, blue, and alpha components on 32 bits. Every component is stored as an unsigned integer.
+ * - `RGBA32F` stores the red, green, blue, and alpha components on 32 bits. Every component is stored as floating point.
+ * - `RGB5_A1` stores the red, green, blue, and alpha components respectively on 5 bits, 5 bits, 5 bits, and 1 bit.
+ * - `RGB10_A2` stores the red, green, blue, and alpha components respectively on 10 bits, 10 bits, 10 bits and 2 bits.
+ * - `RGB10_A2UI` stores the red, green, blue, and alpha components respectively on 10 bits, 10 bits, 10 bits and 2 bits. Every component is stored as an unsigned integer.
+ * - `SRGB8` stores the red, green, and blue components on 8 bits each.
+ * - `SRGB8_ALPHA8` stores the red, green, blue, and alpha components on 8 bits each.
+ * - `DEPTH_COMPONENT16` stores the depth component on 16bits.
+ * - `DEPTH_COMPONENT24` stores the depth component on 24bits.
+ * - `DEPTH_COMPONENT32F` stores the depth component on 32bits. The component is stored as floating point.
+ * - `DEPTH24_STENCIL8` stores the depth, and stencil components respectively on 24 bits and 8 bits. The stencil component is stored as an unsigned integer.
+ * - `DEPTH32F_STENCIL8` stores the depth, and stencil components respectively on 32 bits and 8 bits. The depth component is stored as floating point, and the stencil component as an unsigned integer.
  * @remark Note that the texture must have the correct {@link THREE.Texture.type} set, as well as the correct {@link THREE.Texture.format}.
  * @see {@link WebGLRenderingContext.texImage2D} and {@link WebGLRenderingContext.texImage3D} for more details regarding the possible combination
  * of {@link THREE.Texture.format}, {@link THREE.Texture.internalFormat}, and {@link THREE.Texture.type}.
