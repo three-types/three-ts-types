69f3b81bd2b0da7c35d6bacec0d48c5f3eac7665
Shakhriddin <82777338+Shakhriddin@users.noreply.github.com>
2023-04-10 12:11:43 +0500

    fix(Utils): add string type to ColorRepresentation
    Is it possible to add a string type to ColorRepresentation, now I want to update the version of three.js and I have an error on creating Color class, because the data comes as a string and the string is not specified here. Otherwise, it will be necessary to rewrite "as ColorRepresentation" everywhere

--- types/three/src/utils.d.ts
+++ types/three/src/utils.d.ts
@@ -3,4 +3,4 @@ import { Color, ColorKeyword } from './math/Color';
 export type ColorModelString = `${'rgb' | 'hsl'}(${string})`;
 export type HexColorString = `#${string}`;
 
-export type ColorRepresentation = Color | ColorKeyword | ColorModelString | HexColorString | number;
+export type ColorRepresentation = Color | ColorKeyword | ColorModelString | HexColorString | number | string;
