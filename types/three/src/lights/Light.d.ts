da546595db16aee1f39c0fa9564d19253dee3444
Nathan Bierema <nbierema@gmail.com>
2023-04-27 10:27:46 -0400

    Fix import

--- types/three/src/lights/Light.d.ts
+++ types/three/src/lights/Light.d.ts
@@ -1,7 +1,6 @@
-import { Color } from './../math/Color';
+import { Color, ColorRepresentation } from '../math/Color';
 import { LightShadow } from './LightShadow';
-import { Object3D } from './../core/Object3D';
-import { ColorRepresentation } from '../utils';
+import { Object3D } from '../core/Object3D';
 
 /**
  * Abstract base class for lights.
