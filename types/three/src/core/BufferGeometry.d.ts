8d87dc5bad6ac3103b3709223928a59f8f8af11a
Nathan Bierema <nbierema@gmail.com>
2023-04-30 19:20:29 -0400

    Docs: Improve BufferGeometry page.

--- types/three/src/core/BufferGeometry.d.ts
+++ types/three/src/core/BufferGeometry.d.ts
@@ -23,20 +23,46 @@ export type NormalOrGLBufferAttributes = Record<
  * @example
  * ```typescript
  * const geometry = new THREE.BufferGeometry();
+ *
  * // create a simple square shape. We duplicate the top left and bottom right
  * // vertices because each vertex needs to appear once per triangle.
- * const vertices = new Float32Array([
- *         -1.0, -1.0, 1.0,
- *          1.0, -1.0, 1.0,
- *          1.0,  1.0, 1.0,
+ * const vertices = new Float32Array( [
+ *   -1.0, -1.0,  1.0, // v0
+ *    1.0, -1.0,  1.0, // v1
+ *    1.0,  1.0,  1.0, // v2
  *
- *          1.0,  1.0, 1.0,
- *         -1.0,  1.0, 1.0,
- *         -1.0, -1.0, 1.0]);
+ *    1.0,  1.0,  1.0, // v3
+ *   -1.0,  1.0,  1.0, // v4
+ *   -1.0, -1.0,  1.0  // v5
+ * ] );
+ *
  * // itemSize = 3 because there are 3 values (components) per vertex
- * geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
- * const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
- * const mesh = new THREE.Mesh(geometry, material);
+ * geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+ * const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+ * const mesh = new THREE.Mesh( geometry, material );
+ * ```
+ * @example
+ * ```typescript
+ * const geometry = new THREE.BufferGeometry();
+ *
+ * const vertices = new Float32Array( [
+ *   -1.0, -1.0,  1.0, // v0
+ *    1.0, -1.0,  1.0, // v1
+ *    1.0,  1.0,  1.0, // v2
+ *   -1.0,  1.0,  1.0, // v3
+ * ] );
+ * geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+ *
+ * const indices = [
+ *   0, 1, 2,
+ *   2, 3, 0,
+ * ];
+ *
+ * geometry.setIndex( indices );
+ * geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+ *
+ * const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+ * const mesh = new THREE.Mesh( geometry, material );
  * ```
  * @see Example: {@link https://threejs.org/examples/#webgl_buffergeometry | Mesh with non-indexed faces}
  * @see Example: {@link https://threejs.org/examples/#webgl_buffergeometry_indexed | Mesh with indexed faces}
