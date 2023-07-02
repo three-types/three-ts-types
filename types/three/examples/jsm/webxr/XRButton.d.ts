38a30ba5fc77b868678c020abcf0f075a95ee1eb
Yönet <3523671+Yonet@users.noreply.github.com>
2023-07-02 15:39:18 +0000

    add(webxr): XRButton type

--- types/three/examples/jsm/webxr/XRButton.d.ts
+++ types/three/examples/jsm/webxr/XRButton.d.ts
@@ -1,5 +1,6 @@
 import { WebGLRenderer } from '../../../src/Three';
 
+// tslint:disable-next-line:no-unnecessary-class
 export class XRButton {
     static createButton(renderer: WebGLRenderer): HTMLElement;
 }
