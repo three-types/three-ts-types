05bb5d2fc8da8b1c163d2e6e25d3543a9e25e4ff
Nathan Bierema <nbierema@gmail.com>
2023-07-04 14:54:04 -0400

    Add misc_controls_fly example (#522)
    * Add example
    
    * Update patch

--- examples-testing/index.js
+++ examples-testing/index.js
@@ -391,7 +391,7 @@ const files = {
         'misc_boxselection',
         'misc_controls_arcball',
         'misc_controls_drag',
-        // 'misc_controls_fly',
+        'misc_controls_fly',
         'misc_controls_map',
         'misc_controls_orbit',
         'misc_controls_pointerlock',
