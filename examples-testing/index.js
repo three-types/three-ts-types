75cfe4662e502fa7cdb3edf56ed1ae74aa267c0f
Nathan Bierema <nbierema@gmail.com>
2023-07-05 21:33:41 -0400

    Add example

--- examples-testing/index.js
+++ examples-testing/index.js
@@ -298,7 +298,7 @@ const files = {
         'webgl2_buffergeometry_attributes_integer',
         'webgl2_buffergeometry_attributes_none',
         // 'webgl2_materials_texture2darray',
-        // 'webgl2_materials_texture3d',
+        'webgl2_materials_texture3d',
         // 'webgl2_materials_texture3d_partialupdate',
         // 'webgl2_multiple_rendertargets',
         // 'webgl2_multisampled_renderbuffers',
