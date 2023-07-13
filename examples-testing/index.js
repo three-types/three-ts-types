d0d1700f259149305e1aa57fa5ebfd60e4afe667
Nathan Bierema <nbierema@gmail.com>
2023-07-12 22:56:44 -0400

    Remove example

--- examples-testing/index.js
+++ examples-testing/index.js
@@ -361,7 +361,7 @@ const files = {
         'webxr_vr_panorama_depth',
         'webxr_vr_rollercoaster',
         'webxr_vr_sandbox',
-        'webxr_vr_teleport',
+        // 'webxr_vr_teleport',
         'webxr_vr_video',
         'webxr_xr_ballshooter',
         'webxr_xr_cubes',
