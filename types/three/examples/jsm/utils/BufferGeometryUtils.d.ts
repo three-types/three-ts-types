ef9874c9bb91a4ee12d360dcb9fe1afab93cd591
Nathan Bierema <nbierema@gmail.com>
2023-07-18 07:49:29 -0400

    fix: toCreasedNormals(): call toNonIndexed() only on non-indexed geometries

--- types/three/examples/jsm/utils/BufferGeometryUtils.d.ts
+++ types/three/examples/jsm/utils/BufferGeometryUtils.d.ts
@@ -28,7 +28,8 @@ export function deinterleaveAttribute(geometry: BufferGeometry): void;
 export function deinterleaveGeometry(geometry: BufferGeometry): void;
 
 /**
- * Creates a new, non-indexed geometry with smooth normals everywhere except faces that meet at an angle greater than the crease angle.
+ * Modifies the supplied geometry if it is non-indexed, otherwise creates a new, non-indexed geometry. Returns the
+ * geometry with smooth normals everywhere except faces that meet at an angle greater than the crease angle.
  *
  * @param geometry The input geometry.
  * @param creaseAngle The crease angle.
