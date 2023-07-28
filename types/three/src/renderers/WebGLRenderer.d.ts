cea2e3ffbb341bf9f3ea917ff63ea31836500985
Nathan Bierema <nbierema@gmail.com>
2023-07-27 21:33:52 -0400

    WebGLRenderer: Deprecate useLegacyLights, change default to false. (#542)
    * WebGLRenderer: Deprecate useLegacyLights, change default to false.
    
    * Update WebGLRenderer.js

--- types/three/src/renderers/WebGLRenderer.d.ts
+++ types/three/src/renderers/WebGLRenderer.d.ts
@@ -211,6 +211,8 @@ export class WebGLRenderer implements Renderer {
     get coordinateSystem(): typeof WebGLCoordinateSystem;
 
     /**
+     * @deprecated Migrate your lighting according to the following guide:
+     * https://discourse.threejs.org/t/updates-to-lighting-in-three-js-r155/53733.
      * @default true
      */
     useLegacyLights: boolean;
