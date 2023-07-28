6b0c6b68255c7814b6d6e67f7a837b61d6afd2e2
Nathan Bierema <nbierema@gmail.com>
2023-07-27 20:43:58 -0400

    Backwards compat

--- types/three/src/renderers/WebGLRenderTarget.d.ts
+++ types/three/src/renderers/WebGLRenderTarget.d.ts
@@ -1,6 +1,9 @@
 import { Texture } from '../textures/Texture.js';
 import { RenderTarget, RenderTargetOptions } from '../core/RenderTarget.js';
 
+// tslint:disable-next-line:no-empty-interface
+export interface WebGLRenderTargetOptions extends RenderTargetOptions {}
+
 export class WebGLRenderTarget<TTexture extends Texture | Texture[] = Texture> extends RenderTarget<TTexture> {
     constructor(width?: number, height?: number, options?: RenderTargetOptions);
 
