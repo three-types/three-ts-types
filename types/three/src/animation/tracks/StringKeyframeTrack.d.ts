ee5de5bb8213f6920666b4a156cab136804aa2ce
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-28 11:30:33 +0800

    Update StringKeyframeTrack.d.ts

--- types/three/src/animation/tracks/StringKeyframeTrack.d.ts
+++ types/three/src/animation/tracks/StringKeyframeTrack.d.ts
@@ -2,7 +2,7 @@ import { KeyframeTrack } from './../KeyframeTrack.js';
 import { InterpolationModes } from '../../constants.js';
 
 export class StringKeyframeTrack extends KeyframeTrack {
-    constructor(name: string, times: ArrayLike<number>, values: ArrayLike<any>, interpolation?: InterpolationModes);
+    constructor(name: string, times: ArrayLike<number>, values?: ArrayLike<any>, interpolation?: InterpolationModes);
 
     /**
      * @default 'string'
