d7fad4158dd1df1fead36d11de25610f2a83614c
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-28 11:30:08 +0800

    Update NumberKeyframeTrack.d.ts

--- types/three/src/animation/tracks/NumberKeyframeTrack.d.ts
+++ types/three/src/animation/tracks/NumberKeyframeTrack.d.ts
@@ -2,7 +2,7 @@ import { KeyframeTrack } from './../KeyframeTrack.js';
 import { InterpolationModes } from '../../constants.js';
 
 export class NumberKeyframeTrack extends KeyframeTrack {
-    constructor(name: string, times: ArrayLike<number>, values: ArrayLike<number>, interpolation?: InterpolationModes);
+    constructor(name: string, times: ArrayLike<number>, values?: ArrayLike<number>, interpolation?: InterpolationModes);
 
     /**
      * @default 'number'
