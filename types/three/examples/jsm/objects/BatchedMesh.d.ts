30756582bb411c56b41d07638ef2a5a38546c808
Nathan Bierema <nbierema@gmail.com>
2023-10-27 10:34:38 -0400

    BatchedMesh: Clean up. (#648)

--- types/three/examples/jsm/objects/BatchedMesh.d.ts
+++ types/three/examples/jsm/objects/BatchedMesh.d.ts
@@ -5,35 +5,52 @@ declare class BatchedMesh extends Mesh<BufferGeometry, Material> {
     _vertexCounts: number[];
     _indexStarts: number[];
     _indexCounts: number[];
-    _visibles: boolean[];
-    _alives: boolean[];
+
+    _visible: boolean[];
+    _active: boolean[];
+
     _maxGeometryCount: number;
     _maxVertexCount: number;
     _maxIndexCount: number;
+
     _geometryInitialized: boolean;
     _geometryCount: number;
     _vertexCount: number;
     _indexCount: number;
+
     _matrices: Matrix4[];
-    _matricesArray: Float32Array | null;
     _matricesTexture: DataTexture | null;
-    _matricesTextureSize: number | null;
+
     _customUniforms: Record<string, IUniform>;
+
     constructor(maxGeometryCount: number, maxVertexCount: number, maxIndexCount?: number, material?: Material);
+
     _initMatricesTexture(): void;
+
     _initShader(): void;
+
+    _initializeGeometry(reference: BufferGeometry): void;
+
     getGeometryCount(): number;
+
     getVertexCount(): number;
+
     getIndexCount(): number;
+
     applyGeometry(geometry: BufferGeometry): number;
+
     deleteGeometry(geometryId: number): this;
-    optimize(): this;
+
+    optimize(): never;
+
     setMatrixAt(geometryId: number, matrix: Matrix4): this;
+
     getMatrixAt(geometryId: number, matrix: Matrix4): Matrix4;
-    setVisibleAt(geometryId: number, visible: boolean): this;
+
+    setVisibleAt(geometryId: number, value: boolean): this;
+
     getVisibleAt(geometryId: number): boolean;
-    copy(source: BatchedMesh): this;
-    toJSON(meta: any): any;
+
     dispose(): this;
 }
 
