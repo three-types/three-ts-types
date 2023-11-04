7f83ce35449f37167f7158c3aef81d82de1dce69
Cody Bennett <hi@codyb.co>
2023-11-03 22:30:40 -0500

    BatchedMesh: geometry methods, error signatures (#660)
    * BatchedMesh: geometry methods, error signatures
    
    * Update examples
    
    ---------
    
    Co-authored-by: Nathan Bierema <nbierema@gmail.com>

--- types/three/examples/jsm/objects/BatchedMesh.d.ts
+++ types/three/examples/jsm/objects/BatchedMesh.d.ts
@@ -6,6 +6,12 @@ declare class BatchedMesh extends Mesh<BufferGeometry, Material> {
     _indexStarts: number[];
     _indexCounts: number[];
 
+    _reservedRanges: {
+        vertexStart: number;
+        vertexCount: number;
+        indexStart: number;
+        indexCount: number;
+    }[];
     _visible: boolean[];
     _active: boolean[];
 
@@ -15,9 +21,6 @@ declare class BatchedMesh extends Mesh<BufferGeometry, Material> {
 
     _geometryInitialized: boolean;
     _geometryCount: number;
-    _vertexCount: number;
-    _indexCount: number;
-
     _matrices: Matrix4[];
     _matricesTexture: DataTexture | null;
 
@@ -26,30 +29,27 @@ declare class BatchedMesh extends Mesh<BufferGeometry, Material> {
     constructor(maxGeometryCount: number, maxVertexCount: number, maxIndexCount?: number, material?: Material);
 
     _initMatricesTexture(): void;
-
     _initShader(): void;
-
     _initializeGeometry(reference: BufferGeometry): void;
+    _validateGeometry(geometry: BufferGeometry): void;
 
     getGeometryCount(): number;
-
     getVertexCount(): number;
-
     getIndexCount(): number;
-
-    applyGeometry(geometry: BufferGeometry): number;
-
+    addGeometry(geometry: BufferGeometry, vertexCount?: number, indexCount?: number): number;
+    setGeometryAt(id: number, geometry: BufferGeometry): number;
     deleteGeometry(geometryId: number): this;
 
-    optimize(): never;
-
     setMatrixAt(geometryId: number, matrix: Matrix4): this;
-
     getMatrixAt(geometryId: number, matrix: Matrix4): Matrix4;
-
     setVisibleAt(geometryId: number, value: boolean): this;
-
     getVisibleAt(geometryId: number): boolean;
+
+    // Not implemented
+    optimize(): never;
+    raycast(): void; // no-op
+    copy(): never;
+    toJSON(): never;
 
     dispose(): this;
 }
