f60abc611d3e1a576e2ba1d9d697ec57d8ecba40
Nathan Bierema <nbierema@gmail.com>
2023-12-05 12:32:31 -0500

    Optional

--- types/three/src/textures/CompressedCubeTexture.d.ts
+++ types/three/src/textures/CompressedCubeTexture.d.ts
@@ -5,5 +5,5 @@ export class CompressedCubeTexture extends CompressedTexture {
     readonly isCompressedCubeTexture: true;
     readonly isCubeTexture: true;
 
-    constructor(images: { width: number; height: number }[], format: CompressedPixelFormat, type: TextureDataType);
+    constructor(images: { width: number; height: number }[], format?: CompressedPixelFormat, type?: TextureDataType);
 }
