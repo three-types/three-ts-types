27ba19287b1e6c47fae6b2ac9d9864dd7a6c2c15
Raoul v. R <vanruesc@users.noreply.github.com>
2023-12-21 19:09:24 +0100

    Update types/three/src/renderers/shaders/UniformsUtils.d.ts
    Co-authored-by: Nathan Bierema <nbierema@gmail.com>

--- types/three/src/renderers/shaders/UniformsUtils.d.ts
+++ types/three/src/renderers/shaders/UniformsUtils.d.ts
@@ -1,7 +1,7 @@
 import { UniformsGroup } from '../../core/UniformsGroup.js';
 import { IUniform } from './UniformsLib.js';
 
-export function cloneUniforms(uniformsSrc: { [uniform: string]: IUniform }): { [uniform: string]: IUniform };
+export function cloneUniforms<T extends { [uniform: string]: IUniform }>(uniformsSrc: T): T;
 export function mergeUniforms(uniforms: Array<{ [uniform: string]: IUniform }>): { [uniform: string]: IUniform };
 
 export function cloneUniformsGroups(src: UniformsGroup[]): UniformsGroup[];
