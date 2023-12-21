11f362025ab4628f42c0519d1642f2be8cb87bda
Raoul v. R <vanruesc@users.noreply.github.com>
2023-12-21 19:23:16 +0100

    feat: Specify more types in WebGLPrograms

--- types/three/src/renderers/webgl/WebGLPrograms.d.ts
+++ types/three/src/renderers/webgl/WebGLPrograms.d.ts
@@ -1,12 +1,16 @@
+import { Material } from '../../materials/Material.js';
+import { Object3D } from '../../core/Object3D.js';
+import { Light } from '../../lights/Light.js';
+import { Scene } from '../../scenes/Scene.js';
+import { IUniform } from '../shaders/UniformsLib.js';
 import { WebGLRenderer } from '../WebGLRenderer.js';
-import { WebGLProgram } from './WebGLProgram.js';
+import { WebGLBindingStates } from './WebGLBindingStates.js';
 import { WebGLCapabilities } from './WebGLCapabilities.js';
+import { WebGLClipping } from './WebGLClipping.js';
 import { WebGLCubeMaps } from './WebGLCubeMaps.js';
 import { WebGLExtensions } from './WebGLExtensions.js';
-import { WebGLClipping } from './WebGLClipping.js';
-import { WebGLBindingStates } from './WebGLBindingStates.js';
-import { Material } from '../../materials/Material.js';
-import { Scene } from '../../scenes/Scene.js';
+import { WebGLLightsState } from './WebGLLights.js';
+import { WebGLProgram } from './WebGLProgram.js';
 import { WebGLProgramParameters } from './WebGLProgramParameters.js';
 
 export class WebGLPrograms {
@@ -23,14 +27,14 @@ export class WebGLPrograms {
 
     getParameters(
         material: Material,
-        lights: any,
-        shadows: object[],
+        lights: WebGLLightsState,
+        shadows: Light[],
         scene: Scene,
-        object: any
+        object: Object3D
     ): WebGLProgramParameters;
 
     getProgramCacheKey(parameters: WebGLProgramParameters): string;
-    getUniforms(material: Material): object;
+    getUniforms(material: Material): { [uniform: string]: IUniform };
     acquireProgram(parameters: WebGLProgramParameters, cacheKey: string): WebGLProgram;
     releaseProgram(program: WebGLProgram): void;
 }
