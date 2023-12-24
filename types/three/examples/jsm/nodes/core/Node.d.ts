83a50c7c6aa36e391084d34373c457ece2a3bd20
Nathan Bierema <nbierema@gmail.com>
2023-12-24 11:23:49 -0500

    Add updateBefore

--- types/three/examples/jsm/nodes/core/Node.d.ts
+++ types/three/examples/jsm/nodes/core/Node.d.ts
@@ -3,29 +3,53 @@ import NodeBuilder from './NodeBuilder.js';
 import NodeFrame from './NodeFrame.js';
 
 export default abstract class Node {
-    uuid: string;
-    type: string;
-    isNode: true;
+    readonly isNode: true;
+
     nodeType: NodeTypeOption | null;
+
     updateType: NodeUpdateType;
-    id: number;
+    updateBeforeType: NodeUpdateType;
+
+    uuid: string;
+
+    readonly id: number;
 
     constructor(nodeType?: NodeTypeOption | null);
 
+    get type(): number;
+
     isGlobal(builder: NodeBuilder): boolean;
+
     getChildren(): Node[];
+
+    getCacheKey(): string;
+
     getHash(builder: NodeBuilder): string;
-    getUpdateType(builder: NodeBuilder): NodeUpdateType;
+
+    getUpdateType(): NodeUpdateType;
+
+    getUpdateBeforeType(): NodeUpdateType;
+
     getNodeType(builder: NodeBuilder, output?: string | null): NodeTypeOption | null;
+
     getReference(builder: NodeBuilder): Node;
+
     setup(builder: NodeBuilder): Node | null;
+
     analyze(builder: NodeBuilder): void;
+
     generate(builder: NodeBuilder, output?: string | null): string;
+
+    updateBefore(frame: NodeFrame): void;
+
     /** This method must be overriden when {@link updateType} !== 'none' */
     update(frame: NodeFrame): void;
+
     build(builder: NodeBuilder, output?: string | null): string;
+
     serialize(json: AnyJson): void;
+
     deserialize(json: AnyJson): void;
+
     toJSON(meta?: string | { textures: {}; images: {}; nodes: {} }): AnyJson;
-    getCacheKey(): string;
 }
