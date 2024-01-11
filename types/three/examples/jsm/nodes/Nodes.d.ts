fb1e461080cd1c8e429161b571c54188126333f6
0b5vr <0b5vr@0b5vr.com>
2024-01-11 13:37:07 +0900

    fix: `globalCache` should not be exported from `nodes/Nodes`

--- types/three/examples/jsm/nodes/Nodes.d.ts
+++ types/three/examples/jsm/nodes/Nodes.d.ts
@@ -6,7 +6,7 @@ export { default as ArrayUniformNode } from './core/ArrayUniformNode.js';
 export { default as AssignNode, assign } from './core/AssignNode.js';
 export { default as AttributeNode, attribute } from './core/AttributeNode.js';
 export { default as BypassNode, bypass } from './core/BypassNode.js';
-export { default as CacheNode, cache, globalCache } from './core/CacheNode.js';
+export { default as CacheNode, cache } from './core/CacheNode.js';
 export { default as ConstNode } from './core/ConstNode.js';
 export { default as ContextNode, context, label } from './core/ContextNode.js';
 export { default as Node } from './core/Node.js';
