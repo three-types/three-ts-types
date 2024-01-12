11b2657dccad4c3b52d09fc69abd7d9fa36503a9
Tom Schwarz <t.schwarz@axial3d.com>
2024-01-12 15:37:12 +0000

    remove duplicate copy definition and switch Color4 set to take ColorRepresentation

--- types/three/examples/jsm/renderers/common/Color4.d.ts
+++ types/three/examples/jsm/renderers/common/Color4.d.ts
@@ -5,9 +5,7 @@ export type Color4Representation = ColorRepresentation | Color4;
 export default class Color4 extends Color {
     constructor(r: number, g: number, b: number, a?: number);
 
-    set(...args: [color: Color4Representation] | [r: number, g: number, b: number, a?: number]): this;
-
-    copy(color: this): this;
+    set(...args: [color: ColorRepresentation] | [r: number, g: number, b: number, a?: number]): this;
 
     clone(): this;
 }
