7ae6a965e237fb8b4b1ef1e3fe54c0222b380156
schwyzl <tpschwarz@gmail.com>
2024-01-15 13:45:19 +0000

    Add generic Renderer type and Color4 (#741)
    * Add generic Renderer type and Color4
    
    * method had an implicet any type, changed to function type
    
    * additional commenting for Renderer functions
    
    * explicit file path for Color import in Color4
    
    * solve test fail: Relative import paths need explicit file extensions in ECMAScript imports
    
    * remove duplicate copy definition and switch Color4 set to take ColorRepresentation
    
    * make isRenderer readonly
    
    * switch any args to unknowns
    
    * remove unused Color4Representation
    
    ---------
    
    Co-authored-by: Tom Schwarz <t.schwarz@axial3d.com>

--- types/three/examples/jsm/renderers/common/Renderer.d.ts
+++ types/three/examples/jsm/renderers/common/Renderer.d.ts
@@ -1,27 +1,83 @@
-import { Camera, ColorSpace, CoordinateSystem, Scene, ToneMapping } from '../../../../src/Three.js';
+import {
+    Camera,
+    ColorSpace,
+    CoordinateSystem,
+    Scene,
+    ToneMapping,
+    Vector2,
+    Vector4,
+    Color,
+    Object3D,
+    RenderTarget,
+} from '../../../../src/Three.js';
 import Backend from './Backend.js';
 import Info from './Info.js';
+import Color4 from './../common/Color4.js';
 
+/**
+ * Generic Renderer interface containing either a WebGL or WebGPU backend.
+ */
 export default class Renderer {
-    isRenderer: true;
+    /**
+     * @default true
+     */
+    readonly isRenderer: true;
 
     domElement: HTMLCanvasElement;
 
+    /**
+     * The renderer backend; could be WebGLBackend or WebGPUBackend
+     */
     backend: Backend;
 
+    /**
+     * @default true
+     */
     autoClear: boolean;
+
+    /**
+     * @default true
+     */
     autoClearColor: boolean;
+
+    /**
+     * @default true
+     */
     autoClearDepth: boolean;
+
+    /**
+     * @default true
+     */
     autoClearStencil: boolean;
 
+    /**
+     * @default SRGBColorSpace
+     */
     outputColorSpace: ColorSpace;
 
+    /**
+     * @default NoToneMapping
+     */
     toneMapping: ToneMapping;
+
+    /**
+     * @default 1.0
+     */
     toneMappingExposure: number;
 
+    /**
+     * @default true
+     */
     sortObjects: boolean;
 
+    /**
+     * @default true
+     */
     depth: boolean;
+
+    /**
+     * @default true
+     */
     stencil: boolean;
 
     info: Info;
@@ -36,7 +92,121 @@ export default class Renderer {
 
     setAnimationLoop(callback: ((time: DOMHighResTimeStamp) => void) | null): Promise<void>;
 
+    /**
+     * Sets the pixel ratio of the Renderer.
+     */
     setPixelRatio(value?: number): void;
 
+    /**
+     * Sets the width and height of the output canvas and optionally updates the CSS style on the DOM element.
+     */
     setSize(width: number, height: number, updateStyle?: boolean): void;
+
+    /**
+     * Gets the Rendering Context of the renderer.
+     */
+    getContext(): RenderingContext;
+
+    /**
+     * Gets the current pixel ratio of the renderer.
+     */
+    getPixelRatio(): number;
+
+    getDrawingBufferSize(target: Vector2): Vector2;
+
+    getSize(target: Vector2): Vector2;
+
+    setDrawingBufferSize(width: number, height: number, pixelRatio: number): void;
+
+    /**
+     * Sets the custom opaque sort function for the RenderLists. Pass null to use the default painterSortStable function.
+     */
+    setOpaqueSort(method: (a: unknown, b: unknown) => number): void;
+
+    /**
+     * Sets the custom transparent sort function for the RenderLists. Pass null to use the default reversePainterSortStable function.
+     */
+    setTransparentSort(method: (a: unknown, b: unknown) => number): void;
+
+    /**
+     * Copies the scissor area into target.
+     */
+    getScissor(target: Vector4): Vector4;
+
+    /**
+     * Sets the scissor area from (x, y) to (x + width, y + height).
+     */
+    setScissor(x: number, y: number, width: number, height: number): void;
+
+    /**
+     * Returns true if scissor test is enabled; returns false otherwise.
+     */
+    getScissorTest(): boolean;
+
+    /**
+     * Enable the scissor test. When this is enabled, only the pixels within the defined scissor area will be affected by further renderer actions.
+     */
+    setScissorTest(boolean: boolean): void;
+
+    /**
+     * Copies the viewport into target.
+     */
+    getViewport(target: Vector4): Vector4;
+
+    /**
+     * Sets the viewport to render from (x, y) to (x + width, y + height).
+     * (x, y) is the lower-left corner of the region.
+     */
+    setViewport(x: number, y: number, width: number, height: number, minDepth?: number, maxDepth?: number): void;
+
+    /**
+     * Returns a Color4 instance with the current clear color.
+     */
+    getClearColor(target: Color4): Color4;
+
+    /**
+     * Sets the clear color, using color for the color and alpha for the opacity.
+     */
+    setClearColor(color: Color, alpha?: number): void;
+
+    /**
+     * Returns a float with the current clear alpha. Ranges from 0 to 1.
+     */
+    getClearAlpha(): number;
+
+    setClearAlpha(alpha: number): void;
+
+    getClearDepth(): number;
+
+    setClearDepth(depth: number): void;
+
+    getClearStencil(): number;
+
+    setClearStencil(stencil: number): void;
+
+    isOccluded(object: Object3D): boolean;
+
+    /**
+     * Tells the renderer to clear its color, depth or stencil drawing buffer(s).
+     * Arguments default to true
+     */
+    clear(color?: boolean, depth?: boolean, stencil?: boolean): void;
+
+    clearColor(): void;
+
+    clearDepth(): void;
+
+    clearStencil(): void;
+
+    get currentColorSpace(): ColorSpace;
+
+    dispose(): void;
+
+    setRenderTarget(renderTarget: RenderTarget, activeCubeFace?: number, activeMipmapLevel?: number): void;
+
+    getRenderTarget(): RenderTarget;
+
+    setRenderObjectFunction(renderObjectFunction: () => {}): void;
+
+    getRenderObjectFunction(): () => {};
 }
