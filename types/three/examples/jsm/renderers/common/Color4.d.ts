831fd9b4601df153a45d84ee027bb67e8f5d0320
Tom Schwarz <t.schwarz@axial3d.com>
2024-01-15 10:14:51 +0000

    remove unused Color4Representation

--- types/three/examples/jsm/renderers/common/Color4.d.ts
+++ types/three/examples/jsm/renderers/common/Color4.d.ts
@@ -1,7 +1,5 @@
 import { Color, ColorRepresentation } from '../../../../src/math/Color.ts';
 
-export type Color4Representation = ColorRepresentation | Color4;
-
 export default class Color4 extends Color {
     constructor(r: number, g: number, b: number, a?: number);
 
