d9f2d7a7ba57ba0087659bff4904d1e54db7272d
0b5vr <0b5vr@0b5vr.com>
2024-01-16 18:59:56 +0900

    feat (Nodes): Update MaterialX nodes
    - Fix type of mx_hsv, mx_noise, MaterialXNodes
    - Add missing methods of mx_noise
    - Add mx_transform_color
    - Update test cases
        - the directory name was `materials` instead of `materialx`, fixed this
    
    `mx_noise.js` exports requires all arguments to exist, while `MaterialXNodes.js` exports don't

--- types/three/examples/jsm/nodes/materialx/lib/mx_hsv.d.ts
+++ types/three/examples/jsm/nodes/materialx/lib/mx_hsv.d.ts
@@ -1,6 +1,5 @@
 import Node from '../../core/Node.js';
-import { Fn } from '../../code/FunctionNode.js';
-import { ShaderNodeObject } from '../../shadernode/ShaderNode.js';
+import { NodeRepresentation, ShaderNodeObject } from '../../shadernode/ShaderNode.js';
 
-export function mx_hsvtorgb(...params: Fn<[Node]>): ShaderNodeObject<Node>;
-export function mx_rgbtohsv(...params: Fn<[Node]>): ShaderNodeObject<Node>;
+export const mx_hsvtorgb: (hsv_immutable: NodeRepresentation) => ShaderNodeObject<Node>;
+export const mx_rgbtohsv: (c_immutable: NodeRepresentation) => ShaderNodeObject<Node>;
