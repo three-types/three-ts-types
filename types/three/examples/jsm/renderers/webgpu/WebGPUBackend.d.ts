7c0dc8e23e9d48048d6d4704443ae85c01a691e5
Nathan Bierema <nbierema@gmail.com>
2024-01-24 09:38:58 -0500

    WebGPURenderer: Add timestamp queries support in WebGPU

--- types/three/examples/jsm/renderers/webgpu/WebGPUBackend.d.ts
+++ types/three/examples/jsm/renderers/webgpu/WebGPUBackend.d.ts
@@ -2,8 +2,10 @@ import { CoordinateSystem } from '../../../../src/Three.js';
 import Backend, { BackendParameters } from '../common/Backend.js';
 
 export interface WebGPUBackendParameters extends BackendParameters {
+    alpha?: boolean | undefined;
     antialias?: boolean | undefined;
     sampleCount?: number | undefined;
+    trackTimestamp?: boolean | undefined;
 }
 
 export default class WebGPUBackend extends Backend {
