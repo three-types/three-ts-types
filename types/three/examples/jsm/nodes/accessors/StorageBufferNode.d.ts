880a804ead91f0e2f1f63f86e535b093d81b83df
Nathan Bierema <nbierema@gmail.com>
2024-02-17 09:39:28 -0500

    WebGPURenderer: StorageBufferNode Support reading external elements in the WebGL Backend (#818)
    * Updates
    
    * Update three.js
    
    * Add examples
    
    * Update
    
    * Update
    
    * Delete examples

--- types/three/examples/jsm/nodes/accessors/StorageBufferNode.d.ts
+++ types/three/examples/jsm/nodes/accessors/StorageBufferNode.d.ts
@@ -4,12 +4,22 @@ import StorageArrayElementNode from "../utils/StoargeArrayElementNode.js";
 import BufferNode from "./BufferNode.js";
 
 export default class StorageBufferNode extends BufferNode {
+    readonly isStorageBufferNode: true;
+    bufferObject: boolean;
+
     constructor(value: ArrayLike<number>, bufferType: NodeTypeOption, bufferCount?: number);
 
     element(indexNode: NodeRepresentation): ShaderNodeObject<StorageArrayElementNode>;
+
+    setBufferObject(value: boolean): this;
 }
 
 export const storage: (
+    value: ArrayLike<number>,
+    nodeOrType: NodeOrType,
+    count: number,
+) => ShaderNodeObject<StorageBufferNode>;
+export const storageObject: (
     value: ArrayLike<number>,
     nodeOrType: NodeOrType,
     count: number,
