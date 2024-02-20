b127f4f7b36001b322b776bd446f9a0dc7dea82f
Nathan Bierema <nbierema@gmail.com>
2024-02-20 09:44:12 -0500

    RenderTarget: Add count option for MRT. (#827)
    * RenderTarget: Add count option for MRT.
    
    * Fix
    
    * Fix

--- types/three/src/core/RenderTarget.d.ts
+++ types/three/src/core/RenderTarget.d.ts
@@ -29,12 +29,11 @@ export interface RenderTargetOptions {
      * Defines the count of MSAA samples. Can only be used with WebGL 2. Default is **0**.
      * @default 0
      */
-    samples?: number;
+    samples?: number | undefined;
+    count?: number | undefined;
 }
 
 export class RenderTarget<TTexture extends Texture | Texture[] = Texture> extends EventDispatcher<{ dispose: {} }> {
-    constructor(width?: number, height?: number, options?: RenderTargetOptions);
-
     readonly isRenderTarget: true;
 
     width: number;
@@ -47,7 +46,7 @@ export class RenderTarget<TTexture extends Texture | Texture[] = Texture> extend
      */
     scissorTest: boolean;
     viewport: Vector4;
-    texture: TTexture;
+    textures: TTexture[];
 
     /**
      * @default true
@@ -69,6 +68,11 @@ export class RenderTarget<TTexture extends Texture | Texture[] = Texture> extend
      * @default 0
      */
     samples: number;
+
+    constructor(width?: number, height?: number, options?: RenderTargetOptions);
+
+    get texture(): TTexture;
+    set texture(value: TTexture);
 
     setSize(width: number, height: number, depth?: number): void;
     clone(): this;
