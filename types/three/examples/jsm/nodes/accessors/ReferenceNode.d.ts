1ada12ed8f94ac8ca4843210a46a5ce242437880
Nathan Bierema <nbierema@gmail.com>
2024-02-20 09:06:38 -0500

    Add indexNode to ReferenceNode (#824)

--- types/three/examples/jsm/nodes/accessors/ReferenceNode.d.ts
+++ types/three/examples/jsm/nodes/accessors/ReferenceNode.d.ts
@@ -1,16 +1,27 @@
 import { NodeTypeOption } from "../core/constants.js";
 import Node from "../core/Node.js";
-import { NodeOrType, ShaderNodeObject } from "../shadernode/ShaderNode.js";
+import { NodeOrType, NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
 export default class ReferenceNode<T> extends Node {
-    object: T;
     property: string;
-    uniformType: string;
+    indexNode: Node | null;
+
+    uniformType: NodeTypeOption;
+
+    object: T;
+    reference: T | null;
+
     node: Node | null;
 
-    constructor(property: string, uniformType: NodeTypeOption, object?: T | null);
+    constructor(property: string, uniformType: NodeTypeOption, object?: T | null, indexNode?: Node | null);
 
     setNodeType(uniformType: NodeTypeOption): void;
 }
 
 export const reference: <T>(name: string, nodeOrType: NodeOrType, object: T) => ShaderNodeObject<ReferenceNode<T>>;
+export const referenceIndex: <T>(
+    name: string,
+    index: NodeRepresentation,
+    nodeOrType: NodeOrType,
+    object: T,
+) => ShaderNodeObject<ReferenceNode<T>>;
