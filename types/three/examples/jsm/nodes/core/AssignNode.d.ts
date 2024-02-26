ff215165232231152054b566d853cf5857a583a5
Nathan Bierema <nbierema@gmail.com>
2024-02-26 08:36:03 -0500

    Format

--- types/three/examples/jsm/nodes/core/AssignNode.d.ts
+++ types/three/examples/jsm/nodes/core/AssignNode.d.ts
@@ -1,7 +1,7 @@
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 import Node from "./Node.js";
+import NodeBuilder from "./NodeBuilder.js";
 import TempNode from "./TempNode.js";
-import NodeBuilder from './NodeBuilder.js';
 
 export default class AssignNode extends TempNode {
     constructor(targetNode: Node, sourceNode: Node);
