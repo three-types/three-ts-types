5189f6c27c6adc208275177127dc73add75e2781
Nathan Bierema <nbierema@gmail.com>
2024-03-22 20:28:33 -0400

    Renderer: Set stencil to false by default. (#865)

--- types/three/src/renderers/WebGLRenderer.d.ts
+++ types/three/src/renderers/WebGLRenderer.d.ts
@@ -65,7 +65,7 @@ export interface WebGLRendererParameters {
     antialias?: boolean | undefined;
 
     /**
-     * default is true.
+     * default is false.
      */
     stencil?: boolean | undefined;
 
