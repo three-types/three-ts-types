52f943e8a1d0c7af4ba2ae30a3425091abc89bee
Nathan Bierema <nbierema@gmail.com>
2024-03-29 08:43:33 -0400

    Revert "DataUtils: Add toRGB9E5() and fromRGB9E5(). (#887)"
    This reverts commit 487620930f97363578f75313237a0b4357c1807b.

--- types/three/src/extras/DataUtils.d.ts
+++ types/three/src/extras/DataUtils.d.ts
@@ -13,26 +13,3 @@ export function toHalfFloat(val: number): number;
  * @see {@link https://github.com/mrdoob/three.js/blob/master/src/extras/DataUtils.js | Source}
  */
 export function fromHalfFloat(val: number): number;
-
-/**
- * This method packs three floats into a single Uint32 value which is required for the `RGB9E5` texture format.
- * @param r A float representing the R channel.
- * @param g A float representing the G channel.
- * @param b A float representing the B channel.
- * @param target An instance of `Uint32Array` with length `1`.
- */
-export function toRGB9E5(r: number, g: number, b: number, target: Uint32Array): Uint32Array;
-
-/**
- * This method unpacks three floats from a single Uint32 value holding a `RGB9E5` texel.
- * @param val An instance of `Uint32Array` with length `1`.
- * @param target An array holding the three unpacked floats.
- */
-export function fromRGB9E5(val: Uint32Array, target: number[]): number[];
-
-export const DataUtils: {
-    toHalfFloat: typeof toHalfFloat;
-    fromHalfFloat: typeof fromHalfFloat;
-    toRGB9E5: typeof toRGB9E5;
-    fromRGB9E5: typeof fromRGB9E5;
-};
