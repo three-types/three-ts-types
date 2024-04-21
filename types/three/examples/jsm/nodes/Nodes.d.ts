1c91bbf828b4f7e3dd44c2322390dfa0bd47866b
Nathan Bierema <nbierema@gmail.com>
2024-04-21 13:54:36 -0400

    Remove deprecated code. (#898)
    * Remove deprecated code.
    
    * More
    
    * Fix test

--- types/three/examples/jsm/nodes/Nodes.d.ts
+++ types/three/examples/jsm/nodes/Nodes.d.ts
@@ -365,10 +365,10 @@ export { default as ViewportTextureNode, viewportMipTexture, viewportTexture } f
 export { default as PassNode, depthPass, pass, PassNodeScope } from "./display/PassNode.js";
 
 // code
-export { code, CodeNodeInclude, default as CodeNode } from "./code/CodeNode.js";
+export { code, CodeNodeInclude, default as CodeNode, glsl, js, wgsl } from "./code/CodeNode.js";
 export { default as ExpressionNode, expression } from "./code/ExpressionNode.js";
 export { call, default as FunctionCallNode } from "./code/FunctionCallNode.js";
-export { default as FunctionNode, Fn, fn, func, FunctionNodeArguments } from "./code/FunctionNode.js";
+export { default as FunctionNode, Fn, FunctionNodeArguments, glslFn, wgslFn } from "./code/FunctionNode.js";
 
 // fog
 export { default as FogExp2Node, densityFog } from "./fog/FogExp2Node.js";
