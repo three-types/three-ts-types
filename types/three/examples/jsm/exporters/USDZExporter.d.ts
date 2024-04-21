24afdce478d5b0c57aaf369742fa01148a466756
Nathan Bierema <nbierema@gmail.com>
2024-04-21 10:02:53 -0400

    Format

--- types/three/examples/jsm/exporters/USDZExporter.d.ts
+++ types/three/examples/jsm/exporters/USDZExporter.d.ts
@@ -8,7 +8,12 @@ export interface USDZExporterOptions {
 export class USDZExporter {
     constructor();
 
-    parse(scene: Object3D, onDone: (result: Uint8Array) => void, onError: (error: unknown) => void, options?: USDZExporterOptions): void;
+    parse(
+        scene: Object3D,
+        onDone: (result: Uint8Array) => void,
+        onError: (error: unknown) => void,
+        options?: USDZExporterOptions,
+    ): void;
 
     parseAsync(scene: Object3D, options?: USDZExporterOptions): Promise<Uint8Array>;
 }
