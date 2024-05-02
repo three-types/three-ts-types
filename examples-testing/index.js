1072c406a57ee1b8e91312440f8e0bfba4f69627
Nathan Bierema <nbierema@gmail.com>
2024-05-01 20:32:40 -0400

    Updates

--- examples-testing/index.js
+++ examples-testing/index.js
@@ -55,6 +55,7 @@ const exceptionList = [
     'webgpu_compute_particles',
     'webgpu_compute_particles_rain',
     'webgpu_compute_particles_snow',
+    'webgpu_compute_points',
     'webgpu_compute_texture',
     'webgpu_compute_texture_pingpong',
     'webgpu_cubemap_adjustments',
