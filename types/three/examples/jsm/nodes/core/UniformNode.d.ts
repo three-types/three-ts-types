1cc6ee834cb05620d32c6c1ff784a1e48e9e5583
Christian Helgeson <62450112+cmhhelgeson@users.noreply.github.com>
2024-05-17 14:43:10 -0700

    Added types for UniformGroupNode and UniformGroup, and created type d… (#940)
    * Added types for UniformGroupNode and UniformGroup, and created type definitions for UniformGroupNode values within the UniformNode class
    
    * fix formatting
    
    * Updates
    
    ---------
    
    Co-authored-by: Nathan Bierema <nbierema@gmail.com>

--- types/three/examples/jsm/nodes/core/UniformNode.d.ts
+++ types/three/examples/jsm/nodes/core/UniformNode.d.ts
@@ -3,11 +3,18 @@ import { NodeTypeOption } from "./constants.js";
 import InputNode from "./InputNode.js";
 import Node from "./Node.js";
 import NodeBuilder from "./NodeBuilder.js";
+import UniformGroupNode from "./UniformGroupNode.js";
 
 export default class UniformNode<Value> extends InputNode<Value> {
     readonly isUniformNode: true;
 
+    groupNode: UniformGroupNode;
+
     constructor(value: Value, nodeType?: NodeTypeOption | null);
+
+    setGroup(group: UniformGroupNode): this;
+
+    getGroup(): UniformGroupNode;
 
     getUniformHash(builder: NodeBuilder): string;
 }
