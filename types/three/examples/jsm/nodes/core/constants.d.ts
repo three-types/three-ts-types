b6e6cb2596b0ff0ef810615960aabead2f0a9da6
Nathan Bierema <nbierema@gmail.com>
2024-05-24 15:07:51 -0400

    Update

--- types/three/examples/jsm/nodes/core/constants.d.ts
+++ types/three/examples/jsm/nodes/core/constants.d.ts
@@ -1,25 +1,26 @@
 export declare const NodeShaderStage: {
-    VERTEX: string;
-    FRAGMENT: string;
+    readonly VERTEX: "vertex";
+    readonly FRAGMENT: "fragment";
 };
 export declare const NodeUpdateType: {
-    NONE: string;
-    FRAME: string;
-    RENDER: string;
-    OBJECT: string;
+    readonly NONE: "none";
+    readonly FRAME: "frame";
+    readonly RENDER: "render";
+    readonly OBJECT: "object";
 };
 export declare const NodeType: {
-    BOOLEAN: string;
-    INTEGER: string;
-    FLOAT: string;
-    VECTOR2: string;
-    VECTOR3: string;
-    VECTOR4: string;
-    MATRIX2: string;
-    MATRIX3: string;
-    MATRIX4: string;
+    readonly BOOLEAN: "bool";
+    readonly INTEGER: "int";
+    readonly FLOAT: "float";
+    readonly VECTOR2: "vec2";
+    readonly VECTOR3: "vec3";
+    readonly VECTOR4: "vec4";
+    readonly MATRIX2: "mat2";
+    readonly MATRIX3: "mat3";
+    readonly MATRIX4: "mat4";
 };
 export type NodeShaderStage = "vertex" | "fragment" | "compute";
+export type NodeUpdateType = "none" | "frame" | "render" | "object";
 export declare const defaultShaderStages: NodeShaderStage[];
 export declare const defaultBuildStages: string[];
 export declare const shaderStages: NodeShaderStage[];
