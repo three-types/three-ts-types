e3e1af894be5b865168ff83d5eb754abe88deec3
Nathan Bierema <nbierema@gmail.com>
2024-05-24 15:09:06 -0400

    Reset

--- types/three/examples/jsm/nodes/core/NodeCache.d.ts
+++ types/three/examples/jsm/nodes/core/NodeCache.d.ts
@@ -1,31 +1,9 @@
 import Node from "./Node.js";
-import NodeAttribute from "./NodeAttribute.js";
-import NodeCode from "./NodeCode.js";
-import NodeUniform from "./NodeUniform.js";
-import NodeVar from "./NodeVar.js";
-import NodeVarying from "./NodeVarying.js";
-interface ShaderStageNodeData {
-    properties?: {
-        outputNode: Node | null;
-    } | undefined;
-    bufferAttribute?: NodeAttribute | undefined;
-    structType?: Node | undefined;
-    uniform?: NodeUniform | undefined;
-    variable?: NodeVar | undefined;
-    varying?: NodeVarying | undefined;
-    code?: NodeCode | undefined;
-}
-interface NodeData {
-    vertex?: ShaderStageNodeData | undefined;
-    fragment?: ShaderStageNodeData | undefined;
-    compute?: ShaderStageNodeData | undefined;
-    any?: ShaderStageNodeData | undefined;
-}
-declare class NodeCache {
+import { NodeData } from "./NodeBuilder.js";
+
+export default class NodeCache {
     id: number;
     nodesData: WeakMap<Node, NodeData>;
+
     constructor();
-    getNodeData(node: Node): NodeData | undefined;
-    setNodeData(node: Node, data: NodeData): void;
 }
-export default NodeCache;
