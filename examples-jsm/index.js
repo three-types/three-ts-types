90b7b1b6431e5edb3bc79b21116302d4338eeec1
Nathan Bierema <nbierema@gmail.com>
2024-05-23 20:12:29 -0400

    Add examples-jsm

---
+++ examples-jsm/index.js
@@ -0,0 +1,24 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+import prettier from 'prettier';
+
+const files = ['nodes/core/Node', 'nodes/core/NodeBuilder'];
+
+const inDir = '../three.js/examples/jsm';
+const outDir = './examples';
+
+fs.rmSync(outDir, { recursive: true, force: true });
+fs.mkdirSync(outDir);
+
+for (const file of files) {
+    console.log(file);
+    const fileContents = fs.readFileSync(path.join(inDir, `${file}.js`), {
+        encoding: 'utf-8',
+    });
+    const options = await prettier.resolveConfig(file);
+    const formattedFile = await prettier.format(fileContents, { ...options, parser: 'babel' });
+    const outPath = path.join(outDir, `${file}.ts`);
+    fs.mkdirSync(path.dirname(outPath), { recursive: true });
+    fs.writeFileSync(outPath, formattedFile);
+}
