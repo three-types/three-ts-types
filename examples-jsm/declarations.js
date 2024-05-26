c92062b6f765665786a90fe84e8a7de61c787430
Nathan Bierema <nbierema@gmail.com>
2024-05-26 15:36:25 -0400

    Fix

--- examples-jsm/declarations.js
+++ examples-jsm/declarations.js
@@ -18,16 +18,12 @@ const outDir = '../types/three/examples/jsm';
 
 for (const file of files) {
     console.log(file);
-    const fileContents = fs.readFileSync(path.join(inDir, `${file}.d.ts`), {
-        encoding: 'utf-8',
-    });
+    const fileContents = fs.readFileSync(path.join(inDir, `${file}.d.ts`), { encoding: 'utf-8' });
     const outFile = path.join(outDir, `${file}.d.ts`);
     if (argv[2] === 'copy') {
         fs.writeFileSync(outFile, fileContents);
     } else if (argv[2] === 'check') {
-        const outFileContents = fs.readFileSync(path.join(inDir, `${file}.d.ts`), {
-            encoding: 'utf-8',
-        });
+        const outFileContents = fs.readFileSync(outFile, { encoding: 'utf-8' });
         if (fileContents !== outFileContents) {
             throw new Error(`The built declaration file for ${file} differs.`);
         }
