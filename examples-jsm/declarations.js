18c2f496a5c5aedc79bd9d3fa52f9598c9930556
Nathan Bierema <nbierema@gmail.com>
2024-05-26 15:16:44 -0400

    Add tool to copy/check declaration files

---
+++ examples-jsm/declarations.js
@@ -0,0 +1,36 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { argv } from 'node:process';
+
+const files = [
+    'nodes/core/Node',
+    'nodes/core/NodeAttribute',
+    'nodes/core/NodeKeywords',
+    'nodes/core/NodeParser',
+    'nodes/core/NodeVar',
+    'nodes/core/NodeVarying',
+    'nodes/core/constants',
+];
+
+const inDir = './examples';
+const outDir = '../types/three/examples/jsm';
+
+for (const file of files) {
+    console.log(file);
+    const fileContents = fs.readFileSync(path.join(inDir, `${file}.d.ts`), {
+        encoding: 'utf-8',
+    });
+    const outFile = path.join(outDir, `${file}.d.ts`);
+    if (argv[2] === 'copy') {
+        fs.writeFileSync(outFile, fileContents);
+    } else if (argv[2] === 'check') {
+        const outFileContents = fs.readFileSync(path.join(inDir, `${file}.d.ts`), {
+            encoding: 'utf-8',
+        });
+        if (fileContents !== outFileContents) {
+            throw new Error(`The built declaration file for ${file} differs.`);
+        }
+    } else {
+        throw new Error('Expected program argument.');
+    }
+}
