fe0bb613f2f32fcce16c4d5708df90f140ceabf1
Nathan Bierema <nbierema@gmail.com>
2024-05-26 16:40:31 -0400

    Not ready

--- types/three/examples/jsm/renderers/common/Renderer.d.ts
+++ types/three/examples/jsm/renderers/common/Renderer.d.ts
@@ -1,13 +1,12 @@
-/// <reference types="webxr" />
 import {
     Box2,
-    BufferAttribute,
     BufferGeometry,
     Camera,
     Color,
     ColorSpace,
+    CoordinateSystem,
     FramebufferTexture,
-    GeometryGroup,
+    Group,
     Material,
     Object3D,
     Plane,
@@ -22,198 +21,257 @@ import {
 import Node from "../../nodes/core/Node.js";
 import ComputeNode from "../../nodes/gpgpu/ComputeNode.js";
 import LightsNode from "../../nodes/lighting/LightsNode.js";
-import Animation from "./Animation.js";
-import Attributes from "./Attributes.js";
+import Color4 from "./../common/Color4.js";
 import Backend from "./Backend.js";
-import Background from "./Background.js";
-import Bindings from "./Bindings.js";
-import Color4 from "./Color4.js";
-import Geometries from "./Geometries.js";
 import Info from "./Info.js";
-import Nodes from "./nodes/Nodes.js";
-import Pipelines from "./Pipelines.js";
-import RenderBundle from "./RenderBundle.js";
-import RenderBundles from "./RenderBundles.js";
-import RenderContext from "./RenderContext.js";
-import RenderContexts from "./RenderContexts.js";
-import RenderList, { Bundle, RenderItem } from "./RenderList.js";
-import RenderLists from "./RenderLists.js";
-import RenderObjects from "./RenderObjects.js";
-import Textures from "./Textures.js";
+
 export interface RendererParameters {
     logarithmicDepthBuffer?: boolean | undefined;
     alpha?: boolean | undefined;
 }
-declare class Renderer {
+
+/**
+ * Generic Renderer interface containing either a WebGL or WebGPU backend.
+ */
+export default class Renderer {
+    /**
+     * @default true
+     */
     readonly isRenderer: true;
-    domElement: HTMLCanvasElement | null;
+
+    domElement: HTMLCanvasElement;
+
+    /**
+     * The renderer backend; could be WebGLBackend or WebGPUBackend
+     */
     backend: Backend;
+
+    /**
+     * @default true
+     */
     autoClear: boolean;
+
+    /**
+     * @default true
+     */
     autoClearColor: boolean;
+
+    /**
+     * @default true
+     */
     autoClearDepth: boolean;
+
+    /**
+     * @default true
+     */
     autoClearStencil: boolean;
-    alpha: boolean;
-    logarithmicDepthBuffer: boolean;
+
+    /**
+     * @default SRGBColorSpace
+     */
     outputColorSpace: ColorSpace;
+
+    /**
+     * @default NoToneMapping
+     */
     toneMapping: ToneMapping;
+
+    /**
+     * @default 1.0
+     */
     toneMappingExposure: number;
+
+    /**
+     * @default true
+     */
     sortObjects: boolean;
+
+    /**
+     * @default true
+     */
     depth: boolean;
+
+    /**
+     * @default false
+     */
     stencil: boolean;
-    clippingPlanes: Plane[];
-    info: Info;
+
+    clippingPlanes: readonly Plane[];
+
     toneMappingNode: Node | null;
-    _pixelRatio: number;
-    _width: number;
-    _height: number;
-    _viewport: Vector4;
-    _scissor: Vector4;
-    _scissorTest: boolean;
-    _attributes: Attributes | null;
-    _geometries: Geometries | null;
-    _nodes: Nodes | null;
-    _animation: Animation | null;
-    _bindings: Bindings | null;
-    _objects: RenderObjects | null;
-    _pipelines: Pipelines | null;
-    _bundles: RenderBundles | null;
-    _renderLists: RenderLists | null;
-    _renderContexts: RenderContexts | null;
-    _textures: Textures | null;
-    _background: Background | null;
-    _currentRenderContext: RenderContext | null;
-    _opaqueSort: ((a: RenderItem, b: RenderItem) => number) | null;
-    _transparentSort: ((a: RenderItem, b: RenderItem) => number) | null;
-    _frameBufferTarget: RenderTarget | null;
-    _clearColor: Color4;
-    _clearDepth: number;
-    _clearStencil: number;
-    _renderTarget: RenderTarget | null;
-    _activeCubeFace: number;
-    _activeMipmapLevel: number;
-    _renderObjectFunction:
-        | ((
-            object: Object3D,
-            scene: Scene,
-            camera: Camera,
-            geometry: BufferGeometry,
-            material: Material,
-            group: GeometryGroup,
-            lightsNode: LightsNode,
-        ) => void)
-        | null;
-    _currentRenderObjectFunction:
-        | ((
-            object: Object3D,
-            scene: Scene,
-            camera: Camera,
-            geometry: BufferGeometry,
-            material: Material,
-            group: GeometryGroup,
-            lightsNode: LightsNode,
-        ) => void)
-        | null;
-    _currentRenderBundle: RenderBundle | null;
-    _handleObjectFunction: (
-        object: Object3D,
-        material: Material,
-        scene: Scene,
-        camera: Camera,
-        lightsNode: LightsNode,
-        group: GeometryGroup,
-        passId?: string,
-    ) => void;
-    _initialized: boolean;
-    _initPromise: Promise<void> | null;
-    _compilationPromises: Promise<void>[] | null;
-    shadowMap: {
-        enabled: boolean;
-        type: ShadowMapType | null;
-    };
-    xr: {
-        enabled: boolean;
-    };
+
+    info: Info;
+
+    shadowMap: { enabled: boolean; type: ShadowMapType };
+
+    xr: { enabled: boolean };
+
+    localClippingEnabled?: boolean;
+
     constructor(backend: Backend, parameters?: RendererParameters);
+
     init(): Promise<void>;
-    get coordinateSystem(): import("three").CoordinateSystem;
+
+    get coordinateSystem(): CoordinateSystem;
+
     compileAsync(scene: Scene, camera: Camera, targetScene?: Scene | null): Promise<void>;
+
     renderAsync(scene: Scene, camera: Camera): Promise<void>;
-    _renderBundle(bundle: Bundle, sceneRef: Scene, lightsNode: LightsNode): void;
-    render(scene: Scene, camera: Camera): Promise<void> | undefined;
-    _getFrameBufferTarget(): RenderTarget<Texture> | null;
-    _renderScene(scene: Scene, camera: Camera, useFrameBufferTarget?: boolean): RenderContext;
+
+    render(scene: Scene, camera: Camera): void;
+
     getMaxAnisotropy(): number;
+
     getActiveCubeFace(): number;
+
     getActiveMipmapLevel(): number;
-    setAnimationLoop(callback: ((time: DOMHighResTimeStamp, frame?: XRFrame) => void) | null): Promise<void>;
-    getArrayBuffer(attribute: BufferAttribute): Promise<ArrayBuffer>;
-    getArrayBufferAsync(attribute: BufferAttribute): Promise<ArrayBuffer>;
-    getContext(): void;
-    getPixelRatio(): number;
-    getDrawingBufferSize(target: Vector2): Vector2;
-    getSize(target: Vector2): Vector2;
+
+    setAnimationLoop(callback: ((time: DOMHighResTimeStamp) => void) | null): Promise<void>;
+
+    /**
+     * Sets the pixel ratio of the Renderer.
+     */
     setPixelRatio(value?: number): void;
-    setDrawingBufferSize(width: number, height: number, pixelRatio: number): void;
+
+    /**
+     * Sets the width and height of the output canvas and optionally updates the CSS style on the DOM element.
+     */
     setSize(width: number, height: number, updateStyle?: boolean): void;
-    setOpaqueSort(method: ((a: RenderItem, b: RenderItem) => number) | null): void;
-    setTransparentSort(method: ((a: RenderItem, b: RenderItem) => number) | null): void;
+
+    /**
+     * Gets the Rendering Context of the renderer.
+     */
+    getContext(): RenderingContext;
+
+    /**
+     * Gets the current pixel ratio of the renderer.
+     */
+    getPixelRatio(): number;
+
+    getDrawingBufferSize(target: Vector2): Vector2;
+
+    getSize(target: Vector2): Vector2;
+
+    setDrawingBufferSize(width: number, height: number, pixelRatio: number): void;
+
+    /**
+     * Sets the custom opaque sort function for the RenderLists. Pass null to use the default painterSortStable function.
+     */
+    setOpaqueSort(method: (a: unknown, b: unknown) => number): void;
+
+    /**
+     * Sets the custom transparent sort function for the RenderLists. Pass null to use the default reversePainterSortStable function.
+     */
+    setTransparentSort(method: (a: unknown, b: unknown) => number): void;
+
+    /**
+     * Copies the scissor area into target.
+     */
     getScissor(target: Vector4): Vector4;
-    setScissor(x: Vector4): void;
+
+    /**
+     * Sets the scissor area from (x, y) to (x + width, y + height).
+     */
     setScissor(x: number, y: number, width: number, height: number): void;
+
+    /**
+     * Returns true if scissor test is enabled; returns false otherwise.
+     */
     getScissorTest(): boolean;
+
+    /**
+     * Enable the scissor test. When this is enabled, only the pixels within the defined scissor area will be affected by further renderer actions.
+     */
     setScissorTest(boolean: boolean): void;
+
+    /**
+     * Copies the viewport into target.
+     */
     getViewport(target: Vector4): Vector4;
-    setViewport(x: Vector4): void;
+
+    /**
+     * Sets the viewport to render from (x, y) to (x + width, y + height).
+     * (x, y) is the lower-left corner of the region.
+     */
     setViewport(x: number, y: number, width: number, height: number, minDepth?: number, maxDepth?: number): void;
+
+    /**
+     * Returns a Color4 instance with the current clear color.
+     */
     getClearColor(target: Color4): Color4;
+
+    /**
+     * Sets the clear color, using color for the color and alpha for the opacity.
+     */
     setClearColor(color: Color, alpha?: number): void;
+
+    /**
+     * Returns a float with the current clear alpha. Ranges from 0 to 1.
+     */
     getClearAlpha(): number;
+
     setClearAlpha(alpha: number): void;
+
     getClearDepth(): number;
+
     setClearDepth(depth: number): void;
+
     getClearStencil(): number;
+
     setClearStencil(stencil: number): void;
-    isOccluded(object: Object3D): boolean | null;
-    clear(color?: boolean, depth?: boolean, stencil?: boolean): Promise<void> | undefined;
-    clearColor(): Promise<void> | undefined;
-    clearDepth(): Promise<void> | undefined;
-    clearStencil(): Promise<void> | undefined;
+
+    isOccluded(object: Object3D): boolean;
+
+    /**
+     * Tells the renderer to clear its color, depth or stencil drawing buffer(s).
+     * Arguments default to true
+     */
+    clear(color?: boolean, depth?: boolean, stencil?: boolean): void;
+
+    /**
+     * Clear the color buffer. Equivalent to calling .clear( true, false, false ).
+     */
+    clearColor(): void;
+
+    /**
+     * Clear the depth buffer. Equivalent to calling .clear( false, true, false ).
+     */
+    clearDepth(): void;
+
+    /**
+     * Clear the stencil buffer. Equivalent to calling .clear( false, false, true ).
+     */
+    clearStencil(): void;
+
+    /**
+     * Tells the renderer to clear its color, depth or stencil drawing buffer(s).
+     * Arguments default to true
+     */
     clearAsync(color?: boolean, depth?: boolean, stencil?: boolean): Promise<void>;
-    clearColorAsync(): Promise<void>;
-    clearDepthAsync(): Promise<void>;
-    clearStencilAsync(): Promise<void>;
+
     get currentColorSpace(): ColorSpace;
+
     dispose(): void;
+
     setRenderTarget(renderTarget: RenderTarget | null, activeCubeFace?: number, activeMipmapLevel?: number): void;
-    getRenderTarget(): RenderTarget<Texture> | null;
-    setRenderObjectFunction(
-        renderObjectFunction:
-            | ((
-                object: Object3D,
-                scene: Scene,
-                camera: Camera,
-                geometry: BufferGeometry,
-                material: Material,
-                group: GeometryGroup,
-                lightsNode: LightsNode,
-            ) => void)
-            | null,
-    ): void;
-    getRenderObjectFunction():
-        | ((
-            object: Object3D<import("three").Object3DEventMap>,
-            scene: Scene,
-            camera: Camera,
-            geometry: BufferGeometry<import("three").NormalBufferAttributes>,
-            material: Material,
-            group: GeometryGroup,
-            lightsNode: LightsNode,
-        ) => void)
-        | null;
+
+    getRenderTarget(): RenderTarget | null;
+
+    setRenderObjectFunction(renderObjectFunction: () => {}): void;
+
+    getRenderObjectFunction(): () => {};
+
+    /**
+     * Runs a compute pipeline
+     */
     computeAsync(computeNodes: ComputeNode | ComputeNode[]): Promise<void>;
-    hasFeatureAsync(name: string): Promise<void>;
-    hasFeature(name: string): false | void;
+
+    hasFeatureAsync(name: string): Promise<boolean>;
+
+    hasFeature(name: string): boolean;
+
     copyFramebufferToTexture(framebufferTexture: FramebufferTexture): void;
+
     copyTextureToTexture(
         srcTexture: Texture,
         dstTexture: Texture,
@@ -221,6 +279,7 @@ declare class Renderer {
         dstPosition?: Vector2 | null,
         level?: number,
     ): void;
+
     readRenderTargetPixelsAsync(
         renderTarget: RenderTarget,
         x: number,
@@ -228,38 +287,19 @@ declare class Renderer {
         width: number,
         height: number,
         index?: number,
-    ): void;
-    _projectObject(object: Object3D, camera: Camera, groupOrder: number, renderList: RenderList): void;
-    _renderBundles(bundles: Bundle[], sceneRef: Scene, lightsNode: LightsNode): void;
-    _renderObjects(renderList: RenderItem[], camera: Camera, scene: Scene, lightsNode: LightsNode): void;
+    ): Promise<Float32Array | Uint16Array | Uint8Array | Int8Array | Int16Array | Uint32Array | Int32Array>;
+
     renderObject(
         object: Object3D,
         scene: Scene,
         camera: Camera,
         geometry: BufferGeometry,
         material: Material,
-        group: GeometryGroup,
+        group: Group,
         lightsNode: LightsNode,
     ): void;
-    _renderObjectDirect(
-        object: Object3D,
-        material: Material,
-        scene: Scene,
-        camera: Camera,
-        lightsNode: LightsNode,
-        group: GeometryGroup,
-        passId?: string,
-    ): void;
-    _createObjectPipeline(
-        object: Object3D,
-        material: Material,
-        scene: Scene,
-        camera: Camera,
-        lightsNode: LightsNode,
-        group: GeometryGroup,
-        passId?: string,
-    ): void;
+
     get compute(): (computeNodes: ComputeNode | ComputeNode[]) => Promise<void>;
+
     get compile(): (scene: Scene, camera: Camera, targetScene?: Scene | null) => Promise<void>;
 }
-export default Renderer;
