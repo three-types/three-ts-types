7c7d9cfa07814802a7a8ff6e284e572c7bed6470
Nathan Bierema <nbierema@gmail.com>
2024-05-26 16:35:07 -0400

    Use build declaration files

---
+++ types/three/examples/jsm/renderers/common/ChainMap.d.ts
@@ -0,0 +1,10 @@
+type RecursiveWeakMap<K extends readonly object[], V> = WeakMap<K[number], V | RecursiveWeakMap<K, V>>;
+export default class ChainMap<K extends readonly object[], V> {
+    weakMap: RecursiveWeakMap<K, V>;
+    constructor();
+    get(keys: K): V | undefined;
+    set(keys: K, value: V): V;
+    delete(keys: K): boolean;
+    dispose(): void;
+}
+export {};
