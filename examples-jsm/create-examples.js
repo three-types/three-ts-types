d24c6476b11ad2697b9656b02cfb1ca361d1eb95
Nathan Bierema <nbierema@gmail.com>
2024-05-27 16:23:18 -0400

    Add CubeRenderTarget

--- examples-jsm/create-examples.js
+++ examples-jsm/create-examples.js
@@ -45,6 +45,7 @@ const files = [
     'renderers/common/Color4',
     'renderers/common/ComputePipeline',
     'renderers/common/Constants',
+    'renderers/common/CubeRenderTarget',
     'renderers/common/DataMap',
     'renderers/common/Geometries',
     'renderers/common/Info',
