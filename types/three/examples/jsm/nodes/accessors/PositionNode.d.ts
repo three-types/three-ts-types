ae833f466fdee9e16ef32f7457a9f1a516126653
Nathan Bierema <nbierema@gmail.com>
2024-05-29 18:04:27 -0400

    Get things to pass

--- types/three/examples/jsm/nodes/accessors/PositionNode.d.ts
+++ types/three/examples/jsm/nodes/accessors/PositionNode.d.ts
@@ -1,11 +1,9 @@
-import AttributeNode from "../core/AttributeNode.js";
-import VarNode from "../core/VarNode.js";
-import VaryingNode from "../core/VaryingNode.js";
+import Node from "../core/Node.js";
 import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
-export const positionGeometry: ShaderNodeObject<AttributeNode>;
-export const positionLocal: ShaderNodeObject<VarNode>;
-export const positionWorld: ShaderNodeObject<VaryingNode>;
-export const positionWorldDirection: ShaderNodeObject<VarNode>;
-export const positionView: ShaderNodeObject<VaryingNode>;
-export const positionViewDirection: ShaderNodeObject<VarNode>;
+export const positionGeometry: ShaderNodeObject<Node>;
+export const positionLocal: ShaderNodeObject<Node>;
+export const positionWorld: ShaderNodeObject<Node>;
+export const positionWorldDirection: ShaderNodeObject<Node>;
+export const positionView: ShaderNodeObject<Node>;
+export const positionViewDirection: ShaderNodeObject<Node>;
