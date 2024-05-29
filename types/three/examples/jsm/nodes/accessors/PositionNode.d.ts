b463a90e5ae7985b34e47d48017507673da52690
Nathan Bierema <nbierema@gmail.com>
2024-05-29 17:56:54 -0400

    TSL: PositionNode/ReflectVectorNode - Move to TSL approach

--- types/three/examples/jsm/nodes/accessors/PositionNode.d.ts
+++ types/three/examples/jsm/nodes/accessors/PositionNode.d.ts
@@ -1,29 +1,11 @@
-import Node from "../core/Node.js";
+import AttributeNode from "../core/AttributeNode.js";
+import VarNode from "../core/VarNode.js";
+import VaryingNode from "../core/VaryingNode.js";
 import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
-export type PositionNodeScope =
-    | typeof PositionNode.GEOMETRY
-    | typeof PositionNode.LOCAL
-    | typeof PositionNode.WORLD
-    | typeof PositionNode.WORLD_DIRECTION
-    | typeof PositionNode.VIEW
-    | typeof PositionNode.VIEW_DIRECTION;
-
-export default class PositionNode extends Node {
-    static GEOMETRY: "geometry";
-    static LOCAL: "local";
-    static WORLD: "world";
-    static WORLD_DIRECTION: "worldDirection";
-    static VIEW: "view";
-    static VIEW_DIRECTION: "viewDirection";
-    scope: PositionNodeScope;
-
-    constructor(scope?: PositionNodeScope);
-}
-
-export const positionGeometry: ShaderNodeObject<PositionNode>;
-export const positionLocal: ShaderNodeObject<PositionNode>;
-export const positionWorld: ShaderNodeObject<PositionNode>;
-export const positionWorldDirection: ShaderNodeObject<PositionNode>;
-export const positionView: ShaderNodeObject<PositionNode>;
-export const positionViewDirection: ShaderNodeObject<PositionNode>;
+export const positionGeometry: ShaderNodeObject<AttributeNode>;
+export const positionLocal: ShaderNodeObject<VarNode>;
+export const positionWorld: ShaderNodeObject<VaryingNode>;
+export const positionWorldDirection: ShaderNodeObject<VarNode>;
+export const positionView: ShaderNodeObject<VaryingNode>;
+export const positionViewDirection: ShaderNodeObject<VarNode>;
