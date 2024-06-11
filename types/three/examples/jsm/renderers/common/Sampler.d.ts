7d4e6c0f4672a41791511687e7b9a9d3f1fc0f80
Christian Helgeson <cmhhelgeson@gmail.com>
2024-06-11 15:53:28 -0700

    fix: fix formatting in Sampler and NodeSampler

--- types/three/examples/jsm/renderers/common/Sampler.d.ts
+++ types/three/examples/jsm/renderers/common/Sampler.d.ts
@@ -1,11 +1,11 @@
-import Binding from "./Binding";
 import TextureNode from "../../nodes/accessors/TextureNode";
+import Binding from "./Binding";
 
 declare class Sampler extends Binding {
-	texture: TextureNode | null;
-	version: number;
-	readonly isSampler: true;
-	constructor(name: string, texture: TextureNode | null);
+    texture: TextureNode | null;
+    version: number;
+    readonly isSampler: true;
+    constructor(name: string, texture: TextureNode | null);
 }
 
 export default Sampler;
--- types/three/examples/jsm/renderers/common/nodes/NodeSampler.d.ts
+++ types/three/examples/jsm/renderers/common/nodes/NodeSampler.d.ts
@@ -1,10 +1,10 @@
-import TextureNode from '../../../nodes/accessors/TextureNode.js';
-import Sampler from '../Sampler.js';
+import TextureNode from "../../../nodes/accessors/TextureNode.js";
+import Sampler from "../Sampler.js";
 
 declare class NodeSampler extends Sampler {
-	textureNode: TextureNode | undefined;
-	constructor(name: string, textureNode: TextureNode | undefined);
-	update(): void;
+    textureNode: TextureNode | undefined;
+    constructor(name: string, textureNode: TextureNode | undefined);
+    update(): void;
 }
 
 export default NodeSampler;
