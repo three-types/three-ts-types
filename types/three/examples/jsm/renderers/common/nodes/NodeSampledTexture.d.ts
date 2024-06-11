8346b0bef6b85de4b4904b73357a7a2175f4096a
Christian Helgeson <cmhhelgeson@gmail.com>
2024-06-11 15:39:25 -0700

    feat: Added class declarations for SampledTexture and NodeSampledTexture types

---
+++ types/three/examples/jsm/renderers/common/nodes/NodeSampledTexture.d.ts
@@ -0,0 +1,22 @@
+import TextureNode from "../../../nodes/accessors/TextureNode.js";
+import { SampledTexture } from "../SampledTexture.js";
+
+type GPUStorageTextureAccess = "read-only" | "read-write" | "write-only";
+
+declare class NodeSampledTexture extends SampledTexture {
+    textureNode: TextureNode | undefined;
+    access: "read-write" | "read-only" | "write-only";
+    constructor(name: string, textureNode: TextureNode | undefined, access: GPUStorageTextureAccess | null);
+    get needsBindingsUpdate(): boolean;
+    update(): boolean;
+}
+
+declare class NodeSampledCubeTexture extends NodeSampledTexture {
+    readonly isSampledCubeTexture: true;
+}
+
+declare class NodeSampledTexture3D extends NodeSampledTexture {
+    readonly isSampledTexture3D = true;
+}
+
+export { NodeSampledCubeTexture, NodeSampledTexture, NodeSampledTexture3D };
