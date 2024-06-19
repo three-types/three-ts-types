0e84a519da4e95bfee3a190ba15aa84c4b41b34c
Nathan Bierema <nbierema@gmail.com>
2024-06-18 23:11:11 -0400

    Fix

--- types/three/test/unit/src/core/EventDispatcher.ts
+++ types/three/test/unit/src/core/EventDispatcher.ts
@@ -32,8 +32,6 @@ eveDisForTestEvent.addEventListener("foo", e => {
     e.target; // $ExpectType EventDispatcher<TestEvent>
     e.foo; // $ExpectType number
 
-    // NOTE: Error in ts lower than 3.9. The minimum typescript version cannot be raised from 3.6 because of the dependency from aframe.
-    e.foo; // $ExpectType number
     // @ts-expect-error
     e.bar;
 });
