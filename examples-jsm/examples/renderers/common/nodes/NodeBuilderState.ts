19555fbba8c0be105623518e071a8d0f7b1bfd9a
Nathan Bierema <nbierema@gmail.com>
2024-06-19 09:22:44 -0400

    Add examples

---
+++ examples-jsm/examples/renderers/common/nodes/NodeBuilderState.ts
@@ -0,0 +1,45 @@
+class NodeBuilderState {
+    constructor(
+        vertexShader,
+        fragmentShader,
+        computeShader,
+        nodeAttributes,
+        bindings,
+        updateNodes,
+        updateBeforeNodes,
+        updateAfterNodes,
+        transforms = [],
+    ) {
+        this.vertexShader = vertexShader;
+        this.fragmentShader = fragmentShader;
+        this.computeShader = computeShader;
+        this.transforms = transforms;
+
+        this.nodeAttributes = nodeAttributes;
+        this.bindings = bindings;
+
+        this.updateNodes = updateNodes;
+        this.updateBeforeNodes = updateBeforeNodes;
+        this.updateAfterNodes = updateAfterNodes;
+
+        this.usedTimes = 0;
+    }
+
+    createBindings() {
+        const bindingsArray = [];
+
+        for (const instanceBinding of this.bindings) {
+            let binding = instanceBinding;
+
+            if (instanceBinding.shared !== true) {
+                binding = instanceBinding.clone();
+            }
+
+            bindingsArray.push(binding);
+        }
+
+        return bindingsArray;
+    }
+}
+
+export default NodeBuilderState;
