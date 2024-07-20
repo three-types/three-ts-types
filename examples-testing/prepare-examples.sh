9de778e9d97ed62c8c470cab381452690e202237
Nathan Bierema <nbierema@gmail.com>
2024-07-20 12:39:50 -0400

    Update

--- examples-testing/prepare-examples.sh
+++ examples-testing/prepare-examples.sh
@@ -1,10 +1,3 @@
-cd ../three.js
-git fetch
-git checkout $1
-cd ..
-git add -A
-git commit -m "Update three.js"
-cd examples-testing
 pnpm run create-examples
 git add -A
 git commit -m "Add examples"
