71e4f569d505bc911aa75a0c9ddf2a0585da6297
Nathan Bierema <nbierema@gmail.com>
2024-07-20 15:42:36 -0400

    Update

--- types/three/src/nodes/display/Lut3DNode.d.ts
+++ types/three/src/nodes/display/Lut3DNode.d.ts
@@ -4,19 +4,19 @@ import UniformNode from "../core/UniformNode.js";
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
 declare class Lut3DNode extends TempNode {
-    inputNode: Node;
-    lutNode: Node;
+    inputNode: Node | undefined;
+    lutNode: Node | undefined;
     size: ShaderNodeObject<UniformNode<number>>;
-    intensityNode: Node;
+    intensityNode: Node | undefined;
 
-    constructor(inputNode: Node, lutNode: Node, size: number, intensityNode: Node);
+    constructor(inputNode: Node, lutNode?: Node, size?: number, intensityNode?: Node);
 }
 
 export const lut3D: (
     node: NodeRepresentation,
-    lut: NodeRepresentation,
-    size: number,
-    intensity: NodeRepresentation,
+    lut?: NodeRepresentation,
+    size?: number,
+    intensity?: NodeRepresentation,
 ) => ShaderNodeObject<Lut3DNode>;
 
 declare module "../shadernode/ShaderNode.js" {
