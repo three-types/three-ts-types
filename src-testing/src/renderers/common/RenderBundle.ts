dafbcebbb4cba98019f86779d5c1d92c12e94071
Nathan Bierema <nbierema@gmail.com>
2024-07-21 13:24:28 -0400

    Add src

---
+++ src-testing/src/renderers/common/RenderBundle.ts
@@ -0,0 +1,12 @@
+class RenderBundle {
+    constructor(scene, camera) {
+        this.scene = scene;
+        this.camera = camera;
+    }
+
+    clone() {
+        return Object.assign(new this.constructor(), this);
+    }
+}
+
+export default RenderBundle;
