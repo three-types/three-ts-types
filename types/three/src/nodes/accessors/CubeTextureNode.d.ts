2534297d231e78a56faee252f69d553c9ee4abf0
Nathan Bierema <nbierema@gmail.com>
2024-07-21 13:30:01 -0400

    TSL: Added texture.bias( value ) syntax (#1092)
    * TSL: Added texture.bias( value ) syntax
    
    * Update three.js
    
    * Format
    
    * Add src
    
    * Update patch and delete src
    
    * Update

--- types/three/src/nodes/accessors/CubeTextureNode.d.ts
+++ types/three/src/nodes/accessors/CubeTextureNode.d.ts
@@ -3,20 +3,28 @@ import Node from "../core/Node.js";
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 import TextureNode from "./TextureNode.js";
 
-export default class CubeTextureNode extends TextureNode {
+declare class CubeTextureNode extends TextureNode {
     isCubeTextureNode: boolean;
     uvNode: ShaderNodeObject<Node> | null;
     levelNode: ShaderNodeObject<Node> | null;
 
-    constructor(value: CubeTexture, uvNode?: ShaderNodeObject<Node> | null, levelNode?: ShaderNodeObject<Node> | null);
+    constructor(
+        value: CubeTexture,
+        uvNode?: ShaderNodeObject<Node> | null,
+        levelNode?: ShaderNodeObject<Node> | null,
+        biasNode?: ShaderNodeObject<Node> | null,
+    );
 
     getDefaultUV(): Node;
 }
+
+export default CubeTextureNode;
 
 export const cubeTexture: (
     value: CubeTexture,
     uvNode?: NodeRepresentation,
     levelNode?: NodeRepresentation,
+    biasNode?: NodeRepresentation,
 ) => ShaderNodeObject<CubeTextureNode>;
 
 declare module "../shadernode/ShaderNode.js" {
--- types/three/src/nodes/utils/MaxMipLevelNode.d.ts
+++ types/three/src/nodes/utils/MaxMipLevelNode.d.ts
@@ -4,9 +4,9 @@ import UniformNode from "../core/UniformNode.js";
 import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
 export default class MaxMipLevelNode extends UniformNode<0> {
-    textureNode: TextureNode;
-
     constructor(textureNode: TextureNode);
+
+    get textureNode(): TextureNode;
 
     get texture(): Texture;
 }
