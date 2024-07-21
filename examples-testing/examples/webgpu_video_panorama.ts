ff523d4a0dc1b1e9339041c0d0f3a5d6d5c15d84
Nathan Bierema <nbierema@gmail.com>
2024-07-21 17:53:45 -0400

    Add examples

---
+++ examples-testing/examples/webgpu_video_panorama.ts
@@ -0,0 +1,99 @@
+import * as THREE from 'three';
+
+let camera, scene, renderer;
+
+let isUserInteracting = false,
+    lon = 0,
+    lat = 0,
+    phi = 0,
+    theta = 0,
+    onPointerDownPointerX = 0,
+    onPointerDownPointerY = 0,
+    onPointerDownLon = 0,
+    onPointerDownLat = 0;
+
+const distance = 0.5;
+
+init();
+
+function init() {
+    const container = document.getElementById('container');
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.25, 10);
+
+    scene = new THREE.Scene();
+
+    const geometry = new THREE.SphereGeometry(5, 60, 40);
+    // invert the geometry on the x-axis so that all of the faces point inward
+    geometry.scale(-1, 1, 1);
+
+    const video = document.getElementById('video');
+    video.play();
+
+    const texture = new THREE.VideoTexture(video);
+    texture.colorSpace = THREE.SRGBColorSpace;
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGPURenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    container.appendChild(renderer.domElement);
+
+    document.addEventListener('pointerdown', onPointerDown);
+    document.addEventListener('pointermove', onPointerMove);
+    document.addEventListener('pointerup', onPointerUp);
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onPointerDown(event) {
+    isUserInteracting = true;
+
+    onPointerDownPointerX = event.clientX;
+    onPointerDownPointerY = event.clientY;
+
+    onPointerDownLon = lon;
+    onPointerDownLat = lat;
+}
+
+function onPointerMove(event) {
+    if (isUserInteracting === true) {
+        lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
+        lat = (onPointerDownPointerY - event.clientY) * 0.1 + onPointerDownLat;
+    }
+}
+
+function onPointerUp() {
+    isUserInteracting = false;
+}
+
+function animate() {
+    update();
+}
+
+function update() {
+    lat = Math.max(-85, Math.min(85, lat));
+    phi = THREE.MathUtils.degToRad(90 - lat);
+    theta = THREE.MathUtils.degToRad(lon);
+
+    camera.position.x = distance * Math.sin(phi) * Math.cos(theta);
+    camera.position.y = distance * Math.cos(phi);
+    camera.position.z = distance * Math.sin(phi) * Math.sin(theta);
+
+    camera.lookAt(0, 0, 0);
+
+    renderer.render(scene, camera);
+}
