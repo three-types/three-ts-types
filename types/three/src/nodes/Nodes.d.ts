f7974f765d289e56a7267240a5d61a446effbb22
Nathan Bierema <nbierema@gmail.com>
2024-07-22 22:21:29 -0400

    Nodes: Add BloomNode.

--- types/three/src/nodes/Nodes.d.ts
+++ types/three/src/nodes/Nodes.d.ts
@@ -295,6 +295,7 @@ export { default as VertexColorNode, vertexColor } from "./accessors/VertexColor
 export { afterImage, default as AfterImageNode } from "./display/AfterImageNode.js";
 export { anamorphic, default as AnamorphicNode } from "./display/AnamorphicNode.js";
 export { BlendMode, burn, default as BlendModeNode, dodge, overlay, screen } from "./display/BlendModeNode.js";
+export { bloom, default as BloomNode } from "./display/BloomNode.js";
 export {
     ColorAdjustmentMethod,
     default as ColorAdjustmentNode,
