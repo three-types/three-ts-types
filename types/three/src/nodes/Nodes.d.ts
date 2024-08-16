5ead91dd7cc4738813128dd607b4d71774982bf6
Nathan Bierema <nbierema@gmail.com>
2024-08-16 09:27:08 -0400

    Nodes: Add SepiaNode

--- types/three/src/nodes/Nodes.d.ts
+++ types/three/src/nodes/Nodes.d.ts
@@ -327,6 +327,7 @@ export { default as PixelationPassNode, pixelationPass } from "./display/Pixelat
 export { default as PosterizeNode, posterize } from "./display/PosterizeNode.js";
 export { default as RenderOutputNode, renderOutput } from "./display/RenderOutputNode.js";
 export { default as RGBShiftNode, rgbShift } from "./display/RGBShiftNode.js";
+export { sepia } from "./display/SepiaNode.js";
 export { default as SobelOperatorNode, sobel } from "./display/SobelOperatorNode.js";
 export { default as ToneMappingNode, toneMapping } from "./display/ToneMappingNode.js";
 export { default as TransitionNode, transition } from "./display/TransitionNode.js";
---
+++ types/three/src/nodes/display/SepiaNode.d.ts
@@ -0,0 +1,10 @@
+import Node from "../core/Node.js";
+import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
+
+export const sepia: (args: { color: NodeRepresentation }) => ShaderNodeObject<Node>;
+
+declare module "../shadernode/ShaderNode.js" {
+    interface NodeElements {
+        sepia: typeof sepia;
+    }
+}
