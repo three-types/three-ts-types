f75a9be6b87acc0be77340ca895033b89ccaf04f
Nathan Bierema <nbierema@gmail.com>
2024-08-16 09:20:56 -0400

    Nodes: Rename uniforms() to uniformArray(). (#1153)

--- types/three/src/nodes/accessors/UniformArrayNode.d.ts
+++ types/three/src/nodes/accessors/UniformArrayNode.d.ts
@@ -3,11 +3,11 @@ import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
 import ArrayElementNode from "../utils/ArrayElementNode.js";
 import BufferNode from "./BufferNode.js";
 
-declare class UniformsElementNode extends ArrayElementNode {
+declare class UniformArrayElementNode extends ArrayElementNode {
     constructor(arrayBuffer: Node, indexNode: Node);
 }
 
-export default class UniformsNode extends BufferNode {
+declare class UniformArrayNode extends BufferNode {
     array: unknown[];
     elementType: string | null;
 
@@ -17,7 +17,9 @@ export default class UniformsNode extends BufferNode {
 
     getElementLength(): number;
 
-    element(indexNode: number): ShaderNodeObject<UniformsElementNode>;
+    element(indexNode: number): ShaderNodeObject<UniformArrayElementNode>;
 }
 
-export const uniforms: (values: unknown[], nodeType?: string | null) => ShaderNodeObject<UniformsNode>;
+export default UniformArrayNode;
+
+export const uniformArray: (values: unknown[], nodeType?: string | null) => ShaderNodeObject<UniformArrayNode>;
