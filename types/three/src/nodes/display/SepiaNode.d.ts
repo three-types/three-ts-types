6eff818ed85c5a9269c66f3392462b57769f6615
Nathan Bierema <nbierema@gmail.com>
2024-08-20 08:39:17 -0400

    SepiaNode: Clean up

--- types/three/src/nodes/display/SepiaNode.d.ts
+++ types/three/src/nodes/display/SepiaNode.d.ts
@@ -1,7 +1,7 @@
 import Node from "../core/Node.js";
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
-export const sepia: (args: { color: NodeRepresentation }) => ShaderNodeObject<Node>;
+export const sepia: (args: [NodeRepresentation]) => ShaderNodeObject<Node>;
 
 declare module "../shadernode/ShaderNode.js" {
     interface NodeElements {
