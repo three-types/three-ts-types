828ca195dbdc6a01b7716ba03d95548471d02833
Nathan Bierema <nbierema@gmail.com>
2024-08-21 09:39:23 -0400

    Sky: Add WebGPURenderer version.

---
+++ types/three/examples/jsm/objects/SkyGPU.d.ts
@@ -0,0 +1,18 @@
+import { ShaderNodeObject, UniformNode } from "three/tsl";
+import { BoxGeometry, Mesh, NodeMaterial, Vector3 } from "three/webgpu";
+
+declare class Sky extends Mesh<BoxGeometry, NodeMaterial> {
+    turbidity: ShaderNodeObject<UniformNode<number>>;
+    rayleigh: ShaderNodeObject<UniformNode<number>>;
+    mieCoefficient: ShaderNodeObject<UniformNode<number>>;
+    mieDirectionalG: ShaderNodeObject<UniformNode<number>>;
+    sunPosition: ShaderNodeObject<UniformNode<Vector3>>;
+    up: ShaderNodeObject<UniformNode<Vector3>>;
+    cameraPosition: ShaderNodeObject<Node>;
+
+    readonly isSky: true;
+
+    constructor();
+}
+
+export { Sky };
