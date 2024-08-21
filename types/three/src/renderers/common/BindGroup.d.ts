5ee5d95908338208218b2154914d8e20fc2be4ac
Nathan Bierema <nbierema@gmail.com>
2024-08-21 09:30:29 -0400

    Update declarations

--- types/three/src/renderers/common/BindGroup.d.ts
+++ types/three/src/renderers/common/BindGroup.d.ts
@@ -3,7 +3,8 @@ import NodeUniformsGroup from "./nodes/NodeUniformsGroup.js";
 declare class BindGroup {
     name: string;
     bindings: NodeUniformsGroup[] | Binding[];
+    index: number;
     id: number;
-    constructor(name?: string, bindings?: NodeUniformsGroup[]);
+    constructor(name?: string, bindings?: NodeUniformsGroup[], index?: number);
 }
 export default BindGroup;
