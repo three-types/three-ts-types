b4be445cbb8f7d8da2a2457b56a421609fb8a074
Nathan Bierema <nbierema@gmail.com>
2024-08-22 09:42:52 -0400

    Water2: Add WebGPURenderer version.

---
+++ types/three/examples/jsm/objects/Water2Mesh.d.ts
@@ -0,0 +1,41 @@
+import { TempNode, TextureNode, UniformNode, Vector2, Vector3 } from "three/tsl";
+import { BufferGeometry, Color, ColorRepresentation, Mesh, Texture } from "three/webgpu";
+
+export interface WaterMeshOptions {
+    normalMap0: Texture;
+    normalMap1: Texture;
+    flowMap?: Texture | null | undefined;
+
+    color?: ColorRepresentation | undefined;
+    flowDirection?: Vector2 | undefined;
+    flowSpeed?: number | undefined;
+    reflectivity?: number | undefined;
+    scale?: number | undefined;
+}
+
+declare class WaterMesh extends Mesh {
+    readonly isWater: true;
+
+    constructor(geometry: BufferGeometry, options: WaterMeshOptions);
+}
+
+declare class WaterNode extends TempNode {
+    waterBody: WaterMesh;
+
+    normalMap0: TextureNode;
+    normalMap1: TextureNode;
+    flowMap: TextureNode;
+
+    color: UniformNode<Color>;
+    flowDirection: UniformNode<Vector2>;
+    flowSpeed: UniformNode<number>;
+    reflectivity: UniformNode<number>;
+    scale: UniformNode<number>;
+    flowConfig: UniformNode<Vector3>;
+
+    constructor(options: WaterMeshOptions, waterBody: WaterMesh);
+
+    updateFlow(delta: number): void;
+}
+
+export { WaterNode };
