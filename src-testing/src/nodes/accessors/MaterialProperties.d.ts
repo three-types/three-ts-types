a80dfaa1f137d5771e6568abed6473fa8d6b2704
Nathan Bierema <nbierema@gmail.com>
2024-08-22 23:13:45 -0400

    Update patch and delete src

--- src-testing/src/nodes/accessors/MaterialProperties.d.ts
+++
@@ -1,4 +0,0 @@
-import UniformNode from "../core/UniformNode.js";
-import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
-
-export const materialRefractionRatio: ShaderNodeObject<UniformNode<number>>;
