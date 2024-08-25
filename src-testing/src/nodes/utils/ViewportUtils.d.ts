e941e81b274cf90b47f5edf533b5e606e680b23f
Nathan Bierema <nbierema@gmail.com>
2024-08-25 16:22:00 -0400

    Update patch and delete src

--- src-testing/src/nodes/utils/ViewportUtils.d.ts
+++
@@ -1,4 +0,0 @@
-import Node from "../core/Node.js";
-import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
-
-export const viewportSafeUV: (uv?: NodeRepresentation | null) => ShaderNodeObject<Node>;
