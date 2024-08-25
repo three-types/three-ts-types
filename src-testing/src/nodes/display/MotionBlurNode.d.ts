4b676bd7b7cef8f5e5144a962f4c84b166313a09
Nathan Bierema <nbierema@gmail.com>
2024-08-25 17:35:30 -0400

    Update patch and delete src

--- src-testing/src/nodes/display/MotionBlurNode.d.ts
+++
@@ -1,8 +0,0 @@
-import Node from "../core/Node.js";
-import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
-
-export const motionBlur: (
-    inputNode: NodeRepresentation,
-    velocity: NodeRepresentation,
-    numSamples?: NodeRepresentation,
-) => ShaderNodeObject<Node>;
