f6168fc16934ab657839a69f98871412b0040f8a
Nathan Bierema <nbierema@gmail.com>
2024-08-25 17:23:30 -0400

    Nodes: StereoCompositePassNode.js (#1202)
    * Nodes: StereoCompositePassNode.js
    
    * Update three.js
    
    * Add examples
    
    * Update patch and delete examples
    
    * Add src
    
    * Update patch and delete src
    
    * Fix

--- types/three/src/nodes/display/AnaglyphPassNode.d.ts
+++ types/three/src/nodes/display/AnaglyphPassNode.d.ts
@@ -1,17 +1,14 @@
 import { Camera } from "../../cameras/Camera.js";
-import { StereoCamera } from "../../cameras/StereoCamera.js";
 import { Scene } from "../../scenes/Scene.js";
 import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
-import PassNode from "./PassNode.js";
+import StereoCompositePassNode from "./StereoCompositePassNode.js";
 
-declare class AnaglyphPassNode extends PassNode {
+declare class AnaglyphPassNode extends StereoCompositePassNode {
     readonly isAnaglyphPassNode: true;
-
-    stereo: StereoCamera;
 
     constructor(scene: Scene, camera: Camera);
 }
 
-export const anaglyphPass: (scene: Scene, camera: Camera) => ShaderNodeObject<AnaglyphPassNode>;
-
 export default AnaglyphPassNode;
+
+export const anaglyphPass: (scene: Scene, camera: Camera) => ShaderNodeObject<AnaglyphPassNode>;
--- types/three/src/nodes/display/ParallaxBarrierPassNode.d.ts
+++ types/three/src/nodes/display/ParallaxBarrierPassNode.d.ts
@@ -1,17 +1,14 @@
 import { Camera } from "../../cameras/Camera.js";
-import { StereoCamera } from "../../cameras/StereoCamera.js";
 import { Scene } from "../../scenes/Scene.js";
 import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
-import PassNode from "./PassNode.js";
+import StereoCompositePassNode from "./StereoCompositePassNode.js";
 
-declare class ParallaxBarrierPassNode extends PassNode {
+declare class ParallaxBarrierPassNode extends StereoCompositePassNode {
     readonly isParallaxBarrierPassNode: true;
-
-    stereo: StereoCamera;
 
     constructor(scene: Scene, camera: Camera);
 }
 
-export const parallaxBarrierPass: (scene: Scene, camera: Camera) => ShaderNodeObject<ParallaxBarrierPassNode>;
-
 export default ParallaxBarrierPassNode;
+
+export const parallaxBarrierPass: (scene: Scene, camera: Camera) => ShaderNodeObject<ParallaxBarrierPassNode>;
