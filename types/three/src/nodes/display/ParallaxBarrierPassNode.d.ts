bfb3d50fdaa3496a4bcc015cf07443dbe5622d0e
Nathan Bierema <nbierema@gmail.com>
2024-08-25 17:01:26 -0400

    Nodes: Add anaglyph and parallax barrier pass nodes. (#1200)
    * Nodes: Add anaglyph and parallax barrier pass nodes.
    
    * Fix
    
    * Update three.js
    
    * Add examples
    
    * Update patch and delete examples
    
    * Add examples
    
    * Update patch and delete examples

---
+++ types/three/src/nodes/display/ParallaxBarrierPassNode.d.ts
@@ -0,0 +1,17 @@
+import { Camera } from "../../cameras/Camera.js";
+import { StereoCamera } from "../../cameras/StereoCamera.js";
+import { Scene } from "../../scenes/Scene.js";
+import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
+import PassNode from "./PassNode.js";
+
+declare class ParallaxBarrierPassNode extends PassNode {
+    readonly isParallaxBarrierPassNode: true;
+
+    stereo: StereoCamera;
+
+    constructor(scene: Scene, camera: Camera);
+}
+
+export const parallaxBarrierPass: (scene: Scene, camera: Camera) => ShaderNodeObject<ParallaxBarrierPassNode>;
+
+export default ParallaxBarrierPassNode;
