d1a14836d1ff58e3f2c692cca2070cd334702a87
Nathan Bierema <nbierema@gmail.com>
2024-08-25 09:09:09 -0400

    Nodes: Add VelocityNode and MotionBlurNode.

--- types/three/src/nodes/accessors/PositionNode.d.ts
+++ types/three/src/nodes/accessors/PositionNode.d.ts
@@ -3,6 +3,7 @@ import { ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
 export const positionGeometry: ShaderNodeObject<Node>;
 export const positionLocal: ShaderNodeObject<Node>;
+export const positionPrevious: ShaderNodeObject<Node>;
 export const positionWorld: ShaderNodeObject<Node>;
 export const positionWorldDirection: ShaderNodeObject<Node>;
 export const positionView: ShaderNodeObject<Node>;
