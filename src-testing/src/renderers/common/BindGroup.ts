58d86e844f27c31bbc46899d778ddd8ce72d85d7
Nathan Bierema <nbierema@gmail.com>
2024-08-25 20:06:16 -0400

    Add src

---
+++ src-testing/src/renderers/common/BindGroup.ts
@@ -0,0 +1,14 @@
+let _id = 0;
+
+class BindGroup {
+    constructor(name = '', bindings = [], index = 0, bindingsReference = []) {
+        this.name = name;
+        this.bindings = bindings;
+        this.index = index;
+        this.bindingsReference = bindingsReference;
+
+        this.id = _id++;
+    }
+}
+
+export default BindGroup;
