4756ccfd402ca5e1e816ca9a6c75184501258db2
Nathan Bierema <nbierema@gmail.com>
2024-08-27 22:54:35 -0400

    math

--- types/three/src/nodes/math/ConditionalNode.d.ts
+++ types/three/src/nodes/math/ConditionalNode.d.ts
@@ -1,13 +1,15 @@
 import Node from "../core/Node.js";
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
-export default class CondNode extends Node {
+declare class ConditionalNode extends Node {
     condNode: Node;
     ifNode: Node;
     elseNode: Node;
 
     constructor(condNode: Node, ifNode: Node, elseNode: Node);
 }
+
+export default ConditionalNode;
 
 export const select: (
     condNode: NodeRepresentation,
--- types/three/src/nodes/math/Hash.d.ts
+++ types/three/src/nodes/math/Hash.d.ts
@@ -2,9 +2,3 @@ import Node from "../core/Node.js";
 import { NodeRepresentation, ShaderNodeObject } from "../shadernode/ShaderNode.js";
 
 export const hash: (seed: NodeRepresentation) => ShaderNodeObject<Node>;
-
-declare module "../shadernode/ShaderNode.js" {
-    interface NodeElements {
-        hash: typeof hash;
-    }
-}
