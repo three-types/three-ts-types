0c489d8cd61113d22eaaab74a37012ac926096fd
Nathan Bierema <nbierema@gmail.com>
2024-08-29 19:38:47 -0400

    Add src

---
+++ src-testing/src/renderers/common/UniformBuffer.ts
@@ -0,0 +1,11 @@
+import Buffer from './Buffer.js';
+
+class UniformBuffer extends Buffer {
+    constructor(name, buffer = null) {
+        super(name, buffer);
+
+        this.isUniformBuffer = true;
+    }
+}
+
+export default UniformBuffer;
