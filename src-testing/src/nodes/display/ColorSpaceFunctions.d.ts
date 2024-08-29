3955a51ffb0a6f75f7ec569e2ecd187680e7c919
Nathan Bierema <nbierema@gmail.com>
2024-08-29 19:46:08 -0400

    Update patch and delete src

--- src-testing/src/nodes/display/ColorSpaceFunctions.d.ts
+++
@@ -1,6 +0,0 @@
-import Node from "../core/Node.js";
-import { NodeRepresentation, ShaderNodeObject } from "../tsl/TSLCore.js";
-
-export const sRGBToLinearShader: (color: NodeRepresentation) => ShaderNodeObject<Node>;
-
-export const LinearTosRGBShader: (color: NodeRepresentation) => ShaderNodeObject<Node>;
