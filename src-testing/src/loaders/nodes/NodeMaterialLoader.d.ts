5a0070ee4dd0f56c84661ba80670dc5322951c1a
Nathan Bierema <nbierema@gmail.com>
2024-08-29 08:36:51 -0400

    Update patch and delete src

--- src-testing/src/loaders/nodes/NodeMaterialLoader.d.ts
+++
@@ -1,8 +0,0 @@
-import { MaterialLoader } from "../MaterialLoader.js";
-import { NodeLoaderResult } from "./NodeLoader.js";
-
-export default class NodeMaterialLoader extends MaterialLoader {
-    nodes: NodeLoaderResult;
-
-    setNodes(value: NodeLoaderResult): this;
-}
