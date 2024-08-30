b15d97f4d60c08a05f2e65bc32078928e8dd7c6b
Nathan Bierema <nbierema@gmail.com>
2024-08-29 21:02:31 -0400

    Add src

---
+++ src-testing/src/nodes/core/StructTypeNode.ts
@@ -0,0 +1,18 @@
+import Node, { registerNode } from './Node.js';
+
+class StructTypeNode extends Node {
+    constructor(types) {
+        super();
+
+        this.types = types;
+        this.isStructTypeNode = true;
+    }
+
+    getMemberTypes() {
+        return this.types;
+    }
+}
+
+export default StructTypeNode;
+
+StructTypeNode.type = /*@__PURE__*/ registerNode('StructType', StructTypeNode);
