f62295ebbe5a7725fe13c3db8b93e7585d62960a
Nathan Bierema <nbierema@gmail.com>
2024-09-22 20:48:11 -0400

    Examples: add webgpu_animation_retargeting (#1248)
    * Update SkeletonUtils
    
    * Update three.js
    
    * Add examples
    
    * Update patch and delete examples
    
    * Format

--- types/three/examples/jsm/utils/SkeletonUtils.d.ts
+++ types/three/examples/jsm/utils/SkeletonUtils.d.ts
@@ -1,12 +1,29 @@
 import { AnimationClip, Object3D, Skeleton } from "three";
 
-export function clone(source: Object3D): Object3D;
+export interface RetargetOptions {
+    preserveBoneMatrix?: boolean | undefined;
+    preserveBonePositions?: boolean | undefined;
+    preserveHipPosition?: boolean | undefined;
+    useTargetMatrix?: boolean | undefined;
+    hip?: string | undefined;
+    scale?: number | undefined;
+    names?: { [boneName: string]: string } | undefined;
+}
 
-export function retarget(target: Object3D | Skeleton, source: Object3D | Skeleton, options: {}): void;
+declare function retarget(target: Object3D | Skeleton, source: Object3D | Skeleton, options: {}): void;
 
-export function retargetClip(
+export interface RetargetClipOptions extends RetargetOptions {
+    useFirstFramePosition?: boolean | undefined;
+    fps?: number | undefined;
+}
+
+declare function retargetClip(
     target: Skeleton | Object3D,
     source: Skeleton | Object3D,
     clip: AnimationClip,
     options: {},
 ): AnimationClip;
+
+declare function clone(source: Object3D): Object3D;
+
+export { clone, retarget, retargetClip };
