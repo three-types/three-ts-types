c4c3a5d51ef459f3a0ae9195b330a30aa4a60604
Nathan Bierema <nbierema@gmail.com>
2024-09-22 21:26:50 -0400

    Add src

---
+++ src-testing/src/renderers/common/BundleGroup.ts
@@ -0,0 +1,13 @@
+import { Group } from '../../objects/Group.js';
+
+class BundleGroup extends Group {
+    constructor() {
+        super();
+
+        this.isBundleGroup = true;
+
+        this.type = 'BundleGroup';
+    }
+}
+
+export default BundleGroup;
