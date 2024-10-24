ebb1b984923ccd8049e6ccfacdf4773aec642ae9
Nathan Bierema <nbierema@gmail.com>
2024-10-23 21:50:51 -0400

    Add src

---
+++ src-testing/src/renderers/common/StorageBuffer.ts
@@ -0,0 +1,13 @@
+import Buffer from './Buffer.js';
+
+class StorageBuffer extends Buffer {
+    constructor(name, attribute) {
+        super(name, attribute ? attribute.array : null);
+
+        this.attribute = attribute;
+
+        this.isStorageBuffer = true;
+    }
+}
+
+export default StorageBuffer;
