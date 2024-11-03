8ef92a43506f64e91cfa5802ce85b44fc126af2e
Nathan Bierema <nbierema@gmail.com>
2024-11-03 15:54:52 -0500

    Update patch and delete src

--- src-testing/src/nodes/utils/PostProcessingUtils.d.ts
+++
@@ -1,8 +0,0 @@
-import Node from "../core/Node.js";
-import { NodeRepresentation, ShaderNodeObject } from "../tsl/TSLCore.js";
-
-export const getViewPosition: (
-    screenPosition: NodeRepresentation,
-    depth: NodeRepresentation,
-    projectionMatrixInverse: NodeRepresentation,
-) => ShaderNodeObject<Node>;
