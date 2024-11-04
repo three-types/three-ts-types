35f543c47aeaf215d5b4b59205686cca30d45ed2
Nathan Bierema <nbierema@gmail.com>
2024-11-03 20:45:53 -0500

    TSL: Introduce premultipliedGaussianBlur (#1330)

--- types/three/examples/jsm/tsl/display/GaussianBlurNode.d.ts
+++ types/three/examples/jsm/tsl/display/GaussianBlurNode.d.ts
@@ -8,7 +8,13 @@ declare class GaussianBlurNode extends TempNode {
 
     resolution: Vector2;
 
+    premultipliedAlpha: boolean;
+
     constructor(textureNode: TextureNode, directionNode?: Node | null, sigma?: number);
+
+    setPremultipliedAlpha(value: boolean): this;
+
+    getPremultipliedAlpha(): boolean;
 
     setSize(width: number, height: number): void;
 
@@ -18,6 +24,11 @@ declare class GaussianBlurNode extends TempNode {
 export default GaussianBlurNode;
 
 export const gaussianBlur: (
+    node: NodeRepresentation,
+    directionNode?: NodeRepresentation | null,
+    sigma?: number,
+) => ShaderNodeObject<GaussianBlurNode>;
+export const premultipliedGaussianBlur: (
     node: NodeRepresentation,
     directionNode?: NodeRepresentation | null,
     sigma?: number,
