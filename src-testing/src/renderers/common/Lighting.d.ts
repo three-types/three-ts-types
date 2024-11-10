df06096cd60c08c27b2566cb0e616b950246203b
Nathan Bierema <nbierema@gmail.com>
2024-11-10 15:34:29 -0500

    Add src

---
+++ src-testing/src/renderers/common/Lighting.d.ts
@@ -0,0 +1,15 @@
+import { Camera } from "../../cameras/Camera.js";
+import { Light } from "../../lights/Light.js";
+import LightsNode from "../../nodes/lighting/LightsNode.js";
+import { Scene } from "../../scenes/Scene.js";
+import ChainMap from "./ChainMap.js";
+
+declare class Lighting extends ChainMap<[Scene, Camera], LightsNode> {
+    constructor();
+
+    createNode(lights?: Light[]): LightsNode;
+
+    getNode(scene: Scene, camera: Camera): LightsNode;
+}
+
+export default Lighting;
