bd2bf63c443afd98238571169895529a63c84980
Nathan Bierema <nbierema@gmail.com>
2024-11-10 17:38:02 -0500

    Update

--- types/three/examples/jsm/tsl/display/LensflareNode.d.ts
+++ types/three/examples/jsm/tsl/display/LensflareNode.d.ts
@@ -2,12 +2,12 @@ import { Vector3 } from "three";
 import { NodeRepresentation, ShaderNodeObject, TempNode, TextureNode, UniformNode } from "three/tsl";
 
 interface LensflareNodeParams {
-    ghostTint: ShaderNodeObject<UniformNode<Vector3>>;
-    threshold: ShaderNodeObject<UniformNode<number>>;
-    ghostSamples: ShaderNodeObject<UniformNode<number>>;
-    ghostSpacing: ShaderNodeObject<UniformNode<number>>;
-    ghostAttenuationFactor: ShaderNodeObject<UniformNode<number>>;
-    downSampleRatio: number;
+    ghostTint?: ShaderNodeObject<UniformNode<Vector3>> | undefined;
+    threshold?: ShaderNodeObject<UniformNode<number>> | undefined;
+    ghostSamples?: ShaderNodeObject<UniformNode<number>> | undefined;
+    ghostSpacing?: ShaderNodeObject<UniformNode<number>> | undefined;
+    ghostAttenuationFactor?: ShaderNodeObject<UniformNode<number>> | undefined;
+    downSampleRatio?: number | undefined;
 }
 
 declare class LensflareNode extends TempNode {
