6c2eadb677bf918325fd1ccec429ca73f119fa11
Nathan Bierema <nbierema@gmail.com>
2024-11-12 11:36:56 -0500

    Add shadowNode property to LightShadow (#1376)
    * Add examples
    
    * Update patch and delete examples
    
    * Add shadowNode
    
    * Add examples
    
    * Fix
    
    * Delete examples

--- types/three/src/nodes/lighting/AnalyticLightNode.d.ts
+++ types/three/src/nodes/lighting/AnalyticLightNode.d.ts
@@ -1,5 +1,12 @@
 import { Light } from "../../lights/Light.js";
+import Node from "../core/Node.js";
 import LightingNode from "./LightingNode.js";
+
+declare module "../../lights/LightShadow.js" {
+    export interface LightShadow {
+        shadowNode?: Node;
+    }
+}
 
 export default class AnalyticLightNode<T extends Light> extends LightingNode {
     light: T | null;
