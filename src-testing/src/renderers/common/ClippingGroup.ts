42b322b9134198ec1655c49a5b9156d6880d3693
Nathan Bierema <nbierema@gmail.com>
2024-12-08 12:41:09 -0500

    Add src

---
+++ src-testing/src/renderers/common/ClippingGroup.ts
@@ -0,0 +1,15 @@
+import { Group } from '../../objects/Group.js';
+
+class ClippingGroup extends Group {
+    constructor() {
+        super();
+
+        this.isClippingGroup = true;
+        this.clippingPlanes = [];
+        this.enabled = true;
+        this.clipIntersection = false;
+        this.clipShadows = false;
+    }
+}
+
+export default ClippingGroup;
