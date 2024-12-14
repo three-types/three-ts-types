bdaed4394331aa4962c672c3fb7bdd2fc4116aa4
Nathan Bierema <nbierema@gmail.com>
2024-12-14 11:14:19 -0500

    PMREMGenerator: Add fromSceneAsync()

--- types/three/src/renderers/common/extras/PMREMGenerator.d.ts
+++ types/three/src/renderers/common/extras/PMREMGenerator.d.ts
@@ -5,7 +5,9 @@ import Renderer from "../Renderer.js";
 declare class PMREMGenerator {
     constructor(renderer: Renderer);
 
-    fromScene(scene: Scene, sigma?: number, near?: number, far?: number): RenderTarget;
+    fromScene(scene: Scene, sigma?: number, near?: number, far?: number): RenderTarget | Promise<RenderTarget>;
+
+    fromSceneAsync(scene: Scene, sigma?: number, near?: number, far?: number): Promise<RenderTarget>;
 
     dispose(): void;
 }
