9632520c29d83da9b00772faa628b33b3b8c040c
Nathan Bierema <nbierema@gmail.com>
2024-12-14 10:22:28 -0500

    WebGPURenderer: Add PointShadowNode

--- types/three/src/nodes/lighting/AnalyticLightNode.d.ts
+++ types/three/src/nodes/lighting/AnalyticLightNode.d.ts
@@ -1,6 +1,8 @@
 import { Light } from "../../lights/Light.js";
 import Node from "../core/Node.js";
+import { ShaderNodeObject } from "../tsl/TSLCore.js";
 import LightingNode from "./LightingNode.js";
+import ShadowNode from "./ShadowNode.js";
 
 declare module "../../lights/LightShadow.js" {
     export interface LightShadow {
@@ -12,4 +14,6 @@ export default class AnalyticLightNode<T extends Light> extends LightingNode {
     light: T | null;
 
     constructor(light?: T | null);
+
+    setupShadowNode(): ShaderNodeObject<ShadowNode>;
 }
