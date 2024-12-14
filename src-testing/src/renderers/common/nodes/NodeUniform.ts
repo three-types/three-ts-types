b775f1379b55058e23186ac843c083126a9105ea
Nathan Bierema <nbierema@gmail.com>
2024-12-14 15:15:53 -0500

    Add src

---
+++ src-testing/src/renderers/common/nodes/NodeUniform.ts
@@ -0,0 +1,131 @@
+import {
+    NumberUniform,
+    Vector2Uniform,
+    Vector3Uniform,
+    Vector4Uniform,
+    ColorUniform,
+    Matrix3Uniform,
+    Matrix4Uniform,
+} from '../Uniform.js';
+
+class NumberNodeUniform extends NumberUniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class Vector2NodeUniform extends Vector2Uniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class Vector3NodeUniform extends Vector3Uniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class Vector4NodeUniform extends Vector4Uniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class ColorNodeUniform extends ColorUniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class Matrix3NodeUniform extends Matrix3Uniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+class Matrix4NodeUniform extends Matrix4Uniform {
+    constructor(nodeUniform) {
+        super(nodeUniform.name, nodeUniform.value);
+
+        this.nodeUniform = nodeUniform;
+    }
+
+    getValue() {
+        return this.nodeUniform.value;
+    }
+
+    getType() {
+        return this.nodeUniform.type;
+    }
+}
+
+export {
+    NumberNodeUniform,
+    Vector2NodeUniform,
+    Vector3NodeUniform,
+    Vector4NodeUniform,
+    ColorNodeUniform,
+    Matrix3NodeUniform,
+    Matrix4NodeUniform,
+};
