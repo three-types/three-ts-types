21a50163f0337132669a92ca801cfe7942f567a1
Nathan Bierema <nbierema@gmail.com>
2024-12-14 15:42:01 -0500

    Add src

---
+++ src-testing/src/renderers/common/Animation.ts
@@ -0,0 +1,46 @@
+class Animation {
+    constructor(nodes, info) {
+        this.nodes = nodes;
+        this.info = info;
+
+        this._context = self;
+        this._animationLoop = null;
+        this._requestId = null;
+    }
+
+    start() {
+        const update = (time, frame) => {
+            this._requestId = this._context.requestAnimationFrame(update);
+
+            if (this.info.autoReset === true) this.info.reset();
+
+            this.nodes.nodeFrame.update();
+
+            this.info.frame = this.nodes.nodeFrame.frameId;
+
+            if (this._animationLoop !== null) this._animationLoop(time, frame);
+        };
+
+        update();
+    }
+
+    stop() {
+        this._context.cancelAnimationFrame(this._requestId);
+
+        this._requestId = null;
+    }
+
+    setAnimationLoop(callback) {
+        this._animationLoop = callback;
+    }
+
+    setContext(context) {
+        this._context = context;
+    }
+
+    dispose() {
+        this.stop();
+    }
+}
+
+export default Animation;
