6f927fcd29410981a6547fe292eaeeaa4a016e2d
Nathan Bierema <nbierema@gmail.com>
2024-12-26 10:48:04 -0500

    WebGPURenderer: Introduce RenderTarget3D and RenderTargetArray (#1467)
    * Update three.js
    
    * Add new RenderTarget classes
    
    * Update three.js
    
    * Add src
    
    * Update patch and delete src
    
    * Update declarations
    
    * Update three.js
    
    * Add src
    
    * Update patch and delete src
    
    * Update declarations
    
    * Update three.js
    
    * Add src
    
    * Update patch and delete src
    
    * Update declarations
    
    * Add examples
    
    * Update
    
    * Update patch and delete examples
    
    * Add examples
    
    * Update patch and delete examples

--- types/three/src/renderers/common/ProgrammableStage.d.ts
+++ types/three/src/renderers/common/ProgrammableStage.d.ts
@@ -1,13 +1,28 @@
 import NodeAttribute from "../../nodes/core/NodeAttribute.js";
+/**
+ * Class for representing programmable stages which are vertex,
+ * fragment or compute shaders. Unlike fixed-function states (like blending),
+ * they represent the programmable part of a pipeline.
+ *
+ * @private
+ */
 declare class ProgrammableStage {
     id: number;
     code: string;
     stage: "compute" | "vertex" | "fragment";
     attributes: NodeAttribute[] | null;
     usedTimes: number;
+    /**
+     * Constructs a new programmable stage.
+     *
+     * @param {String} code - The shader code.
+     * @param {('vertex'|'fragment'|'compute')} stage - The type of stage.
+     * @param {Array<Object>?} [transforms=null] - The transforms (only relevant for compute stages with WebGL 2 which uses Transform Feedback).
+     * @param {Array<Object>?} [attributes=null] - The attributes (only relevant for compute stages with WebGL 2 which uses Transform Feedback).
+     */
     constructor(
         code: string,
-        type: "compute" | "vertex" | "fragment",
+        stage: "compute" | "vertex" | "fragment",
         transforms?: null,
         attributes?: NodeAttribute[] | null,
     );
--- types/three/src/renderers/common/RenderContexts.d.ts
+++ types/three/src/renderers/common/RenderContexts.d.ts
@@ -5,13 +5,27 @@ import ChainMap from "./ChainMap.js";
 import RenderContext from "./RenderContext.js";
 declare class RenderContexts {
     chainMaps: {
-        [attachmentState: string]: ChainMap<readonly [Object3D, Camera], RenderContext> | undefined;
+        [attachmentState: string]:
+            | ChainMap<
+                | readonly [Object3D, Camera]
+                | readonly [Object3D, Camera, {
+                    id: "default";
+                }],
+                RenderContext
+            >
+            | undefined;
     };
     constructor();
-    get(scene: Object3D, camera: Camera, renderTarget?: RenderTarget | null): RenderContext;
+    get(scene?: Object3D | null, camera?: Camera | null, renderTarget?: RenderTarget | null): RenderContext;
     getChainMap(
         attachmentState: string,
-    ): ChainMap<readonly [Object3D<import("../../core/Object3D.js").Object3DEventMap>, Camera], RenderContext>;
+    ): ChainMap<
+        | readonly [Object3D<import("../../core/Object3D.js").Object3DEventMap>, Camera]
+        | readonly [Object3D<import("../../core/Object3D.js").Object3DEventMap>, Camera, {
+            id: "default";
+        }],
+        RenderContext
+    >;
     dispose(): void;
 }
 export default RenderContexts;
