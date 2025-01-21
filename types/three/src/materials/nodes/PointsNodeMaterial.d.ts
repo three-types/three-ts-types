82712df7283d8cf5a28a788e9142f4e20ca722de
Nathan Bierema <nbierema@gmail.com>
2025-01-21 09:00:12 -0500

    Update

--- types/three/src/materials/nodes/PointsNodeMaterial.d.ts
+++ types/three/src/materials/nodes/PointsNodeMaterial.d.ts
@@ -1,6 +1,8 @@
+import Node from "../../nodes/core/Node.js";
 import SpriteNodeMaterial, { SpriteNodeMaterialParameters } from "./SpriteNodeMaterial.js";
 
 export interface PointsNodeMaterialParameters extends SpriteNodeMaterialParameters {
+    sizeNode?: Node | null | undefined;
 }
 
 export default class PointsNodeMaterial extends SpriteNodeMaterial {
