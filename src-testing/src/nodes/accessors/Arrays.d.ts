6728b8633a4d2a00ee2a7d195b32a3b56fdba520
Nathan Bierema <nbierema@gmail.com>
2025-01-26 18:19:07 -0500

    Update patch and delete src

--- src-testing/src/nodes/accessors/Arrays.d.ts
+++
@@ -1,7 +0,0 @@
-import { TypedArray } from "../../core/BufferAttribute.js";
-import { ShaderNodeObject } from "../tsl/TSLCore.js";
-import StorageBufferNode from "./StorageBufferNode.js";
-
-export const attributeArray: (count: TypedArray | number, type?: string) => ShaderNodeObject<StorageBufferNode>;
-
-export const instancedArray: (count: TypedArray | number, type?: string) => ShaderNodeObject<StorageBufferNode>;
