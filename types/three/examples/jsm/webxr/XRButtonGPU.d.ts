f02ca436e98d6b17883cc51068fef933fd37f581
Nathan Bierema <nbierema@gmail.com>
2025-01-26 18:38:16 -0500

    Remove XRButtonGPU

--- types/three/examples/jsm/webxr/XRButtonGPU.d.ts
+++
@@ -1,7 +0,0 @@
-import { Renderer } from "three/webgpu";
-
-declare class XRButton {
-    static createButton(renderer: Renderer, sessionInit?: XRSessionInit): HTMLElement;
-}
-
-export { XRButton };
