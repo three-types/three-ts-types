1705c448239b2189b93941e9e73575fa22192787
Nathan Bierema <nbierema@gmail.com>
2025-01-30 13:48:57 -0500

    Update declarations

--- types/three/src/nodes/core/NodeVar.d.ts
+++ types/three/src/nodes/core/NodeVar.d.ts
@@ -9,13 +9,15 @@ declare class NodeVar {
     name: string;
     type: string;
     readOnly: boolean;
+    count: number | null;
     /**
      * Constructs a new node variable.
      *
      * @param {String} name - The name of the variable.
      * @param {String} type - The type of the variable.
      * @param {Boolean} [readOnly=false] - The read-only flag.
+     * @param {Number?} [count=null] - The size.
      */
-    constructor(name: string, type: string, readOnly?: boolean);
+    constructor(name: string, type: string, readOnly?: boolean, count?: number | null);
 }
 export default NodeVar;
