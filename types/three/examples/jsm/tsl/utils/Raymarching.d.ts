de35c6a7a6f1736ae1bf160a933224fffd26683b
Nathan Bierema <nbierema@gmail.com>
2025-02-15 12:27:59 -0500

    TSL: Introduce RaymarchingBox and raymarchingTexture3D

---
+++ types/three/examples/jsm/tsl/utils/Raymarching.d.ts
@@ -0,0 +1,16 @@
+import { FunctionNode, Node, Texture, TextureNode } from "three/tsl";
+
+export const RaymarchingBox: (
+    steps: number | Node,
+    callback: ((params: { positionRay: Node }) => void) | FunctionNode,
+) => void;
+
+export const raymarchingTexture3D: (
+    params: {
+        texture: Texture | TextureNode;
+        range: number | Node;
+        threshold: number | Node;
+        opacity: number | Node;
+        steps: number | Node;
+    },
+) => Node;
