c82ceb4bba77c6cf7b5c5f204889a053e4629609
Nathan Bierema <nbierema@gmail.com>
2025-02-16 12:35:09 -0500

    Raymarching: Move raymarchingTexture3D to example and update webgpu_volume_perlin

--- types/three/examples/jsm/tsl/utils/Raymarching.d.ts
+++ types/three/examples/jsm/tsl/utils/Raymarching.d.ts
@@ -1,17 +1,6 @@
-import { ShaderNodeObject } from "three/tsl";
-import { FunctionNode, Node, Texture, TextureNode } from "three/webgpu";
+import { FunctionNode, Node } from "three/webgpu";
 
 export const RaymarchingBox: (
     steps: number | Node,
     callback: ((params: { positionRay: Node }) => void) | FunctionNode<{ positionRay: Node }>,
 ) => void;
-
-export const raymarchingTexture3D: (
-    params: {
-        texture: Texture | TextureNode;
-        range: number | Node;
-        threshold: number | Node;
-        opacity: number | Node;
-        steps: number | Node;
-    },
-) => ShaderNodeObject<Node>;
