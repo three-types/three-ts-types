8d6f4a1c50abf4f0bf9985f6a55fb78cd6089b3d
Nathan Bierema <nbierema@gmail.com>
2025-02-16 12:43:20 -0500

    Format

--- types/three/examples/jsm/tsl/utils/Raymarching.d.ts
+++ types/three/examples/jsm/tsl/utils/Raymarching.d.ts
@@ -3,5 +3,7 @@ import { FunctionNode, Node } from "three/webgpu";
 
 export const RaymarchingBox: (
     steps: number | Node,
-    callback: ((params: { positionRay: ShaderNodeObject<Node> }) => void) | FunctionNode<{ positionRay: ShaderNodeObject<Node> }>,
+    callback:
+        | ((params: { positionRay: ShaderNodeObject<Node> }) => void)
+        | FunctionNode<{ positionRay: ShaderNodeObject<Node> }>,
 ) => void;
