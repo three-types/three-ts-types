622137fa6fdd795cf7be0adc9bc08d3fad851d53
Samuel Rigaud <srigaud@duodisplay.com>
2025-03-11 09:48:13 -0400

    TSLEncoder: add missing types

--- types/three/examples/jsm/transpiler/TSLEncoder.d.ts
+++ types/three/examples/jsm/transpiler/TSLEncoder.d.ts
@@ -1,7 +1,30 @@
-import { Program } from "./AST.js";
+import { Program, Statement } from "./AST.js";
 
 export default class TSLEncoder {
     constructor();
 
+    tab: string;
+    imports: Set<string>;
+    global: Set<string>;
+    overloadings: Map<string, string>;
+    iife: boolean;
+    uniqueNames: boolean;
+    reference: boolean;
+
+    addImport(name: string): void;
+    emitUniform(node: Statement): string;
+    emitExpression(node: Statement): string;
+    emitBody(body: Statement): string;
+    emitTernary(node: Statement): string;
+    emitConditional(node: Statement): string;
+    emitLoop(node: Statement): string;
+    emitFor(node: Statement): string;
+    emitForWhile(node: Statement): string;
+    emitVariables(node: Statement, isRoot?: boolean): string;
+    emitVarying(node: Statement): string;
+    emitOverloadingFunction(nodes: Statement): string;
+    emitFunction(node: Statement): string;
+    setLastStatement(statement: Statement): void;
+    emitExtraLine(statement: Statement): string;
     emit(ast: Program): string;
 }
