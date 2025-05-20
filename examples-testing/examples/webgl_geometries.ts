17a38422a803a80128673b84589ec3d5d1d22d35
Nathan Bierema <nbierema@gmail.com>
2025-05-20 17:09:59 -0400

    Add examples

---
+++ examples-testing/examples/webgl_geometries.ts
@@ -0,0 +1,137 @@
+import * as THREE from 'three';
+
+import Stats from 'three/addons/libs/stats.module.js';
+
+let camera, scene, renderer, stats;
+
+init();
+
+function init() {
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.y = 400;
+
+    scene = new THREE.Scene();
+
+    let object;
+
+    const ambientLight = new THREE.AmbientLight(0xcccccc, 1.5);
+    scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 2.5, 0, 0);
+    camera.add(pointLight);
+    scene.add(camera);
+
+    const map = new THREE.TextureLoader().load('textures/uv_grid_opengl.jpg');
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.anisotropy = 16;
+    map.colorSpace = THREE.SRGBColorSpace;
+
+    const material = new THREE.MeshPhongMaterial({ map: map, side: THREE.DoubleSide });
+
+    //
+
+    object = new THREE.Mesh(new THREE.SphereGeometry(75, 20, 10), material);
+    object.position.set(-300, 0, 200);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.IcosahedronGeometry(75, 1), material);
+    object.position.set(-100, 0, 200);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.OctahedronGeometry(75, 2), material);
+    object.position.set(100, 0, 200);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.TetrahedronGeometry(75, 0), material);
+    object.position.set(300, 0, 200);
+    scene.add(object);
+
+    //
+
+    object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 4, 4), material);
+    object.position.set(-300, 0, 0);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100, 4, 4, 4), material);
+    object.position.set(-100, 0, 0);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.CircleGeometry(50, 20, 0, Math.PI * 2), material);
+    object.position.set(100, 0, 0);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.RingGeometry(10, 50, 20, 5, 0, Math.PI * 2), material);
+    object.position.set(300, 0, 0);
+    scene.add(object);
+
+    //
+
+    object = new THREE.Mesh(new THREE.CylinderGeometry(25, 75, 100, 40, 5), material);
+    object.position.set(-300, 0, -200);
+    scene.add(object);
+
+    const points = [];
+
+    for (let i = 0; i < 50; i++) {
+        points.push(new THREE.Vector2(Math.sin(i * 0.2) * Math.sin(i * 0.1) * 15 + 50, (i - 5) * 2));
+    }
+
+    object = new THREE.Mesh(new THREE.LatheGeometry(points, 20), material);
+    object.position.set(-100, 0, -200);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.TorusGeometry(50, 20, 20, 20), material);
+    object.position.set(100, 0, -200);
+    scene.add(object);
+
+    object = new THREE.Mesh(new THREE.TorusKnotGeometry(50, 10, 50, 20), material);
+    object.position.set(300, 0, -200);
+    scene.add(object);
+
+    //
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//
+
+function animate() {
+    render();
+    stats.update();
+}
+
+function render() {
+    const timer = Date.now() * 0.0001;
+
+    camera.position.x = Math.cos(timer) * 800;
+    camera.position.z = Math.sin(timer) * 800;
+
+    camera.lookAt(scene.position);
+
+    scene.traverse(function (object) {
+        if (object.isMesh === true) {
+            object.rotation.x = timer * 5;
+            object.rotation.y = timer * 2.5;
+        }
+    });
+
+    renderer.render(scene, camera);
+}
---
+++ examples-testing/examples/webgl_tonemapping.ts
@@ -0,0 +1,163 @@
+import * as THREE from 'three';
+
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+let mesh, renderer, scene, camera, controls;
+let gui,
+    guiExposure = null;
+
+const params = {
+    exposure: 1.0,
+    toneMapping: 'AgX',
+    blurriness: 0.3,
+    intensity: 1.0,
+};
+
+const toneMappingOptions = {
+    None: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+    AgX: THREE.AgXToneMapping,
+    Neutral: THREE.NeutralToneMapping,
+    Custom: THREE.CustomToneMapping,
+};
+
+init().catch(function (err) {
+    console.error(err);
+});
+
+async function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    renderer.toneMapping = toneMappingOptions[params.toneMapping];
+    renderer.toneMappingExposure = params.exposure;
+
+    // Set CustomToneMapping to Uncharted2
+    // source: http://filmicworlds.com/blog/filmic-tonemapping-operators/
+
+    THREE.ShaderChunk.tonemapping_pars_fragment = THREE.ShaderChunk.tonemapping_pars_fragment.replace(
+        'vec3 CustomToneMapping( vec3 color ) { return color; }',
+
+        `#define Uncharted2Helper( x ) max( ( ( x * ( 0.15 * x + 0.10 * 0.50 ) + 0.20 * 0.02 ) / ( x * ( 0.15 * x + 0.50 ) + 0.20 * 0.30 ) ) - 0.02 / 0.30, vec3( 0.0 ) )
+
+					float toneMappingWhitePoint = 1.0;
+
+					vec3 CustomToneMapping( vec3 color ) {
+						color *= toneMappingExposure;
+						return saturate( Uncharted2Helper( color ) / Uncharted2Helper( vec3( toneMappingWhitePoint ) ) );
+
+					}`,
+    );
+
+    scene = new THREE.Scene();
+    scene.backgroundBlurriness = params.blurriness;
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+    camera.position.set(-1.8, 0.6, 2.7);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render); // use if there is no animation loop
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.target.set(0, 0, -0.2);
+    controls.update();
+
+    const rgbeLoader = new RGBELoader().setPath('textures/equirectangular/');
+
+    const gltfLoader = new GLTFLoader().setPath('models/gltf/DamagedHelmet/glTF/');
+
+    const [texture, gltf] = await Promise.all([
+        rgbeLoader.loadAsync('venice_sunset_1k.hdr'),
+        gltfLoader.loadAsync('DamagedHelmet.gltf'),
+    ]);
+
+    // environment
+
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+
+    scene.background = texture;
+    scene.environment = texture;
+
+    // model
+
+    mesh = gltf.scene.getObjectByName('node_damagedHelmet_-6514');
+    scene.add(mesh);
+
+    render();
+
+    window.addEventListener('resize', onWindowResize);
+
+    gui = new GUI();
+    const toneMappingFolder = gui.addFolder('Tone Mapping');
+
+    toneMappingFolder
+        .add(params, 'toneMapping', Object.keys(toneMappingOptions))
+
+        .name('type')
+        .onChange(function () {
+            updateGUI(toneMappingFolder);
+
+            renderer.toneMapping = toneMappingOptions[params.toneMapping];
+            render();
+        });
+
+    guiExposure = toneMappingFolder
+        .add(params, 'exposure', 0, 2)
+
+        .onChange(function (value) {
+            renderer.toneMappingExposure = value;
+            render();
+        });
+
+    const backgroundFolder = gui.addFolder('Background');
+
+    backgroundFolder
+        .add(params, 'blurriness', 0, 1)
+
+        .onChange(function (value) {
+            scene.backgroundBlurriness = value;
+            render();
+        });
+
+    backgroundFolder
+        .add(params, 'intensity', 0, 1)
+
+        .onChange(function (value) {
+            scene.backgroundIntensity = value;
+            render();
+        });
+
+    updateGUI(toneMappingFolder);
+
+    gui.open();
+}
+
+function updateGUI(folder) {
+    if (params.toneMapping === 'None') {
+        guiExposure.hide();
+    } else {
+        guiExposure.show();
+    }
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
