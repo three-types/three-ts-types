deea0d3684fca4fe5e469432856d6943dcfcbcb9
Nathan Bierema <nbierema@gmail.com>
2025-06-09 23:18:43 -0400

    TSL: Introduce Chromatic Aberration

---
+++ types/three/examples/jsm/tsl/display/ChromaticAberrationNode.d.ts
@@ -0,0 +1,19 @@
+import { Node, NodeRepresentation, ShaderNodeObject, TempNode, Vector2 } from "three/webgpu";
+
+declare class ChromaticAberrationNode extends TempNode {
+    textureNode: Node;
+    strengthNode: Node;
+    centerNode: Node;
+    scaleNode: Node;
+
+    constructor(textureNode: Node, strengthNode: Node, centerNode: Node, scaleNode: Node);
+}
+
+export default ChromaticAberrationNode;
+
+export const chromaticAberration: (
+    node: NodeRepresentation,
+    strength?: NodeRepresentation,
+    center?: NodeRepresentation | Vector2 | null,
+    scale?: NodeRepresentation,
+) => ShaderNodeObject<ChromaticAberrationNode>;
