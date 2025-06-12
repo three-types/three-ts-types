04772d7e56124f80ff339cb0193b4c95381ac11c
Nathan Bierema <nbierema@gmail.com>
2025-06-12 18:35:35 -0400

    Strict TSL type functions (#1491)
    * Strict TSL type functions
    
    * Direct?
    
    * Add examples
    
    * Fix?
    
    * Fix?
    
    * Fix?
    
    * Fix?
    
    * Delete examples
    
    * Update ColorFunction
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add examples
    
    * Update

--- types/three/src/nodes/tsl/TSLCore.d.ts
+++ types/three/src/nodes/tsl/TSLCore.d.ts
@@ -1,34 +1,40 @@
-import { Color, ColorRepresentation } from "../../math/Color.js";
+import { Color } from "../../math/Color.js";
+import { Matrix2 } from "../../math/Matrix2.js";
+import { Matrix3 } from "../../math/Matrix3.js";
+import { Matrix4 } from "../../math/Matrix4.js";
+import { Vector2 } from "../../math/Vector2.js";
 import { Vector3 } from "../../math/Vector3.js";
+import { Vector4 } from "../../math/Vector4.js";
 import ConstNode from "../core/ConstNode.js";
 import Node from "../core/Node.js";
 import NodeBuilder from "../core/NodeBuilder.js";
 import StackNode from "../core/StackNode.js";
 import ConvertNode from "../utils/ConvertNode.js";
+import JoinNode from "../utils/JoinNode.js";
 
 export interface NodeElements {
     toStack: typeof Stack;
 
-    toColor: typeof color;
-    toFloat: typeof float;
-    toInt: typeof int;
-    toUint: typeof uint;
-    toBool: typeof bool;
-    toVec2: typeof vec2;
-    toIvec2: typeof ivec2;
-    toUvec2: typeof uvec2;
-    toBvec2: typeof bvec2;
-    toVec3: typeof vec3;
-    toIvec3: typeof ivec3;
-    toUvec3: typeof uvec3;
-    toBvec3: typeof bvec3;
-    toVec4: typeof vec4;
-    toIvec4: typeof ivec4;
-    toUvec4: typeof uvec4;
-    toBvec4: typeof bvec4;
-    toMat2: typeof mat2;
-    toMat3: typeof mat3;
-    toMat4: typeof mat4;
+    toColor: (node: Node) => ShaderNodeObject<Node>;
+    toFloat: (node: Node) => ShaderNodeObject<Node>;
+    toInt: (node: Node) => ShaderNodeObject<Node>;
+    toUint: (node: Node) => ShaderNodeObject<Node>;
+    toBool: (node: Node) => ShaderNodeObject<Node>;
+    toVec2: (node: Node) => ShaderNodeObject<Node>;
+    toIvec2: (node: Node) => ShaderNodeObject<Node>;
+    toUvec2: (node: Node) => ShaderNodeObject<Node>;
+    toBvec2: (node: Node) => ShaderNodeObject<Node>;
+    toVec3: (node: Node) => ShaderNodeObject<Node>;
+    toIvec3: (node: Node) => ShaderNodeObject<Node>;
+    toUvec3: (node: Node) => ShaderNodeObject<Node>;
+    toBvec3: (node: Node) => ShaderNodeObject<Node>;
+    toVec4: (node: Node) => ShaderNodeObject<Node>;
+    toIvec4: (node: Node) => ShaderNodeObject<Node>;
+    toUvec4: (node: Node) => ShaderNodeObject<Node>;
+    toBvec4: (node: Node) => ShaderNodeObject<Node>;
+    toMat2: (node: Node) => ShaderNodeObject<Node>;
+    toMat3: (node: Node) => ShaderNodeObject<Node>;
+    toMat4: (node: Node) => ShaderNodeObject<Node>;
 
     element: typeof element;
     convert: typeof convert;
@@ -184,8 +190,6 @@ type GetConstructorsByScope<T, S> = ConstructorUnion<FilterConstructorsByScope<O
 type GetConstructors<T> = ConstructorUnion<OverloadedConstructorsOf<T>>;
 type GetPossibleScopes<T> = ExtractScopes<OverloadedConstructorsOf<T>>;
 
-export type ConvertType = (...params: unknown[]) => ShaderNodeObject<Node>;
-
 type NodeArray<T extends NodeObjectOption[]> = { [index in keyof T]: NodeObject<T[index]> };
 type NodeObjects<T> = { [key in keyof T]: T[key] extends NodeObjectOption ? NodeObject<T[key]> : T[key] };
 type ConstructedNode<T> = T extends new(...args: any[]) => infer R ? (R extends Node ? R : never) : never;
@@ -276,36 +280,153 @@ export const Switch: (expression: NodeRepresentation) => StackNode;
 export function Stack(node: Node): Node;
 
 interface ColorFunction {
-    (color?: ColorRepresentation): ShaderNodeObject<ConstNode<Color>>;
+    // The first branch in `ConvertType` will forward the parameters to the `Color` constructor if there are no
+    //   parameters or all the parameters are non-objects
+    (color?: string | number): ShaderNodeObject<ConstNode<Color>>;
     (r: number, g: number, b: number): ShaderNodeObject<ConstNode<Color>>;
-    (node: Node): ShaderNodeObject<ConvertNode>;
+
+    // The second branch does not apply because `cacheMap` is `null`
+
+    // The third branch will be triggered if there is a single parameter.
+    (color: Color): ShaderNodeObject<ConstNode<Color>>;
+    (node: Node): ShaderNodeObject<Node>;
+
+    // The fall-through branch will be triggered if there is more than one parameter, or one of the parameters is an
+    // object. Not sure which cases are worth considering here.
 }
 
 export const color: ColorFunction;
 
-export const float: ConvertType;
-export const int: ConvertType;
-export const uint: ConvertType;
-export const bool: ConvertType;
+interface NumberFunction {
+    (value?: number): ShaderNodeObject<ConstNode<number>>;
+    (node: Node): ShaderNodeObject<Node>;
+}
 
-export const vec2: ConvertType;
-export const ivec2: ConvertType;
-export const uvec2: ConvertType;
-export const bvec2: ConvertType;
+export const float: NumberFunction;
+export const int: NumberFunction;
+export const uint: NumberFunction;
 
-export const vec3: ConvertType;
-export const ivec3: ConvertType;
-export const uvec3: ConvertType;
-export const bvec3: ConvertType;
+interface BooleanFunction {
+    (value?: boolean): ShaderNodeObject<ConstNode<boolean>>;
+    (node: Node): ShaderNodeObject<Node>;
+}
 
-export const vec4: ConvertType;
-export const ivec4: ConvertType;
-export const uvec4: ConvertType;
-export const bvec4: ConvertType;
+export const bool: BooleanFunction;
 
-export const mat2: ConvertType;
-export const mat3: ConvertType;
-export const mat4: ConvertType;
+interface Vector2Function {
+    // The first branch in `ConvertType` will forward the parameters to the `Vector2` constructor if there are no
+    //   parameters or all the parameters are non-objects
+    (x?: number, y?: number): ShaderNodeObject<ConstNode<Vector2>>;
+
+    // The second branch does not apply because `cacheMap` is `null`
+
+    // The third branch will be triggered if there is a single parameter.
+    (value: Vector2): ShaderNodeObject<ConstNode<Vector2>>;
+    (node: Node): ShaderNodeObject<Node>;
+
+    // The fall-through branch will be triggered if there is more than one parameter, or one of the parameters is an
+    // object.
+    (x: Node | number, y: Node | number): ShaderNodeObject<JoinNode>;
+}
+
+export const vec2: Vector2Function;
+export const ivec2: Vector2Function;
+export const uvec2: Vector2Function;
+export const bvec2: (node: Node) => ShaderNodeObject<Node>;
+
+interface Vector3Function {
+    // The first branch in `ConvertType` will forward the parameters to the `Vector3` constructor if there are no
+    //   parameters or all the parameters are non-objects
+    (x?: number, y?: number, z?: number): ShaderNodeObject<ConstNode<Vector3>>;
+
+    // The second branch does not apply because `cacheMap` is `null`
+
+    // The third branch will be triggered if there is a single parameter.
+    (value: Vector3): ShaderNodeObject<ConstNode<Vector3>>;
+    (node: Node): ShaderNodeObject<Node>;
+
+    // The fall-through branch will be triggered if there is more than one parameter, or one of the parameters is an
+    // object.
+    (x: Node | number, y: Node | number, z?: Node | number): ShaderNodeObject<JoinNode>;
+}
+
+export const vec3: Vector3Function;
+export const ivec3: Vector3Function;
+export const uvec3: Vector3Function;
+export const bvec3: (node: Node) => ShaderNodeObject<Node>;
+
+interface Vector4Function {
+    // The first branch in `ConvertType` will forward the parameters to the `Vector4` constructor if there are no
+    //   parameters or all the parameters are non-objects
+    (x?: number, y?: number, z?: number, w?: number): ShaderNodeObject<ConstNode<Vector4>>;
+
+    // The second branch does not apply because `cacheMap` is `null`
+
+    // The third branch will be triggered if there is a single parameter.
+    (value: Vector4): ShaderNodeObject<ConstNode<Vector4>>;
+    (node: Node): ShaderNodeObject<Node>;
+
+    // The fall-through branch will be triggered if there is more than one parameter, or one of the parameters is an
+    // object.
+    (x: Node | number, y: Node | number, z?: Node | number, w?: Node | number): ShaderNodeObject<JoinNode>;
+}
+
+export const vec4: Vector4Function;
+export const ivec4: Vector4Function;
+export const uvec4: Vector4Function;
+export const bvec4: (node: Node) => ShaderNodeObject<Node>;
+
+interface Matrix2Function {
+    (value: Matrix2): ShaderNodeObject<ConstNode<Matrix2>>;
+    (node: Node): ShaderNodeObject<Node>;
+}
+
+export const mat2: Matrix2Function;
+
+interface Matrix3Function {
+    (value: Matrix3): ShaderNodeObject<ConstNode<Matrix3>>;
+    (
+        n11: number,
+        n12: number,
+        n13: number,
+        n21: number,
+        n22: number,
+        n23: number,
+        n31: number,
+        n32: number,
+        n33: number,
+    ): ShaderNodeObject<ConstNode<Matrix3>>;
+    (): ShaderNodeObject<ConstNode<Matrix3>>;
+    (node: Node): ShaderNodeObject<Node>;
+}
+
+export const mat3: Matrix3Function;
+
+interface Matrix4Function {
+    (value: Matrix4): ShaderNodeObject<ConstNode<Matrix4>>;
+    (
+        n11: number,
+        n12: number,
+        n13: number,
+        n14: number,
+        n21: number,
+        n22: number,
+        n23: number,
+        n24: number,
+        n31: number,
+        n32: number,
+        n33: number,
+        n34: number,
+        n41: number,
+        n42: number,
+        n43: number,
+        n44: number,
+    ): ShaderNodeObject<ConstNode<Matrix4>>;
+    (): ShaderNodeObject<ConstNode<Matrix4>>;
+    (node: Node): ShaderNodeObject<Node>;
+}
+
+export const mat4: Matrix4Function;
 
 export const string: (value?: string) => ShaderNodeObject<ConstNode<string>>;
 export const arrayBuffer: (value: ArrayBuffer) => ShaderNodeObject<ConstNode<ArrayBuffer>>;
