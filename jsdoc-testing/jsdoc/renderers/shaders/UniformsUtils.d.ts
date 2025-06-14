9d4a917f858f877e460ef2492d80d6550a3b800c
Nathan Bierema <nbierema@gmail.com>
2025-06-13 21:57:37 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/renderers/shaders/UniformsUtils.d.ts
@@ -0,0 +1,8 @@
+export function cloneUniforms(src: any): {};
+export function mergeUniforms(uniforms: any): {};
+export function cloneUniformsGroups(src: any): any[];
+export function getUnlitUniformColorSpace(renderer: any): any;
+export namespace UniformsUtils {
+    export { cloneUniforms as clone };
+    export { mergeUniforms as merge };
+}
