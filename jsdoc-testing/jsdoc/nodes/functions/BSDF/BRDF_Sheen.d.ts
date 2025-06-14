c5df43dfe6f9aaf88d53a24acf9c7de184ec20c1
Nathan Bierema <nbierema@gmail.com>
2025-06-13 21:58:57 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/functions/BSDF/BRDF_Sheen.d.ts
+++
@@ -1,3 +0,0 @@
-export default BRDF_Sheen;
-declare function BRDF_Sheen(...params: any[]): any;
-declare namespace BRDF_Sheen { }
--- jsdoc-testing/jsdoc/nodes/functions/BSDF/F_Schlick.d.ts
+++
@@ -1,3 +0,0 @@
-export default F_Schlick;
-declare function F_Schlick(...params: any[]): any;
-declare namespace F_Schlick { }
