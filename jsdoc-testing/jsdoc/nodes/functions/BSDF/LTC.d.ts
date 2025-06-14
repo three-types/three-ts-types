62f29183f7d4649d1dd3cfef8bc8994b50ddad7b
Nathan Bierema <nbierema@gmail.com>
2025-06-13 22:08:39 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/functions/BSDF/LTC.d.ts
+++
@@ -1,7 +0,0 @@
-export function LTC_Evaluate(...params: any[]): any;
-export namespace LTC_Evaluate { }
-export function LTC_Evaluate(...params: any[]): any;
-export namespace LTC_Evaluate { }
-export function LTC_Evaluate(...params: any[]): any;
-export namespace LTC_Evaluate { }
-export { LTC_Evaluate_Volume, LTC_Uv };
