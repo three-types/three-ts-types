afca04eacd9668307f9166da2bfb8c82a63e4cf3
Nathan Bierema <nbierema@gmail.com>
2025-06-14 12:28:38 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/nodes/display/ViewportTextureNode.d.ts
@@ -0,0 +1,60 @@
+export default ViewportTextureNode;
+/**
+ * TSL function for creating a viewport texture node.
+ *
+ * @tsl
+ * @function
+ * @param {?Node} [uvNode=screenUV] - The uv node.
+ * @param {?Node} [levelNode=null] - The level node.
+ * @param {?Texture} [framebufferTexture=null] - A framebuffer texture holding the viewport data. If not provided, a framebuffer texture is created automatically.
+ * @returns {ViewportTextureNode}
+ */
+export const viewportTexture: any;
+/**
+ * TSL function for creating a viewport texture node with enabled mipmap generation.
+ *
+ * @tsl
+ * @function
+ * @param {?Node} [uvNode=screenUV] - The uv node.
+ * @param {?Node} [levelNode=null] - The level node.
+ * @param {?Texture} [framebufferTexture=null] - A framebuffer texture holding the viewport data. If not provided, a framebuffer texture is created automatically.
+ * @returns {ViewportTextureNode}
+ */
+export const viewportMipTexture: any;
+/**
+ * A special type of texture node which represents the data of the current viewport
+ * as a texture. The module extracts data from the current bound framebuffer with
+ * a copy operation so no extra render pass is required to produce the texture data
+ * (which is good for performance). `ViewportTextureNode` can be used as an input for a
+ * variety of effects like refractive or transmissive materials.
+ *
+ * @augments TextureNode
+ */
+declare class ViewportTextureNode extends TextureNode {
+    /**
+     * Constructs a new viewport texture node.
+     *
+     * @param {Node} [uvNode=screenUV] - The uv node.
+     * @param {?Node} [levelNode=null] - The level node.
+     * @param {?Texture} [framebufferTexture=null] - A framebuffer texture holding the viewport data. If not provided, a framebuffer texture is created automatically.
+     */
+    constructor(uvNode?: Node, levelNode?: Node | null, framebufferTexture?: Texture | null);
+    /**
+     * Whether to generate mipmaps or not.
+     *
+     * @type {boolean}
+     * @default false
+     */
+    generateMipmaps: boolean;
+    /**
+     * This flag can be used for type testing.
+     *
+     * @type {boolean}
+     * @readonly
+     * @default true
+     */
+    readonly isOutputTextureNode: boolean;
+    updateBefore(frame: any): void;
+    clone(): any;
+}
+import TextureNode from '../accessors/TextureNode.js';
