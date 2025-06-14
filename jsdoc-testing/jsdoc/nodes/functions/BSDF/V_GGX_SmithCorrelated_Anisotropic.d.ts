4d5addaa42bb47b7eeca132324d0acc40142a32e
Nathan Bierema <nbierema@gmail.com>
2025-06-14 09:06:26 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/functions/BSDF/V_GGX_SmithCorrelated_Anisotropic.d.ts
+++
@@ -1,3 +0,0 @@
-export default V_GGX_SmithCorrelated_Anisotropic;
-declare function V_GGX_SmithCorrelated_Anisotropic(...params: any[]): any;
-declare namespace V_GGX_SmithCorrelated_Anisotropic { }
