4f05765333d11164a099bac473212ccd42f10583
Nathan Bierema <nbierema@gmail.com>
2025-06-17 21:03:06 -0400

    TSL: Remove transformed* prefix

--- types/three/src/nodes/accessors/AccessorsUtils.d.ts
+++ types/three/src/nodes/accessors/AccessorsUtils.d.ts
@@ -6,4 +6,4 @@ export const TBNViewMatrix: ShaderNodeObject<Node>;
 export const parallaxDirection: ShaderNodeObject<Node>;
 export const parallaxUV: (uv: ShaderNodeObject<Node>, scale: Node) => ShaderNodeObject<Node>;
 
-export const transformedBentNormalView: ShaderNodeObject<Node>;
+export const bentNormalView: ShaderNodeObject<Node>;
