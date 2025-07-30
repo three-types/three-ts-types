31db0ec841f4630267df8226b61b06661748be4c
Yusuke Kawamoto <novogrammer@gmail.com>
2025-07-30 22:43:40 +0900

    add get

--- types/three/src/nodes/core/StructNode.d.ts
+++ types/three/src/nodes/core/StructNode.d.ts
@@ -6,6 +6,7 @@ declare class StructNode extends Node {
     values: Node[];
 
     constructor(structLayoutNode: StructTypeNode, values: Node[]);
+    get(name:string):ShaderNodeObject<Node>;
 }
 
 export default StructNode;
