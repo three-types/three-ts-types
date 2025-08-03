921bf4569f90e1de2e7d8127909962d6d85e30a8
Nathan Bierema <nbierema@gmail.com>
2025-08-03 13:18:17 -0400

    Update patch and delete src

--- src-testing/src/nodes/functions/material/getAlphaHashThreshold.d.ts
+++
@@ -1,6 +0,0 @@
-import Node from "../../core/Node.js";
-import { ShaderNodeObject } from "../../tsl/TSLCore.js";
-
-declare const getAlphaHashThreshold: (position: Node) => ShaderNodeObject<Node>;
-
-export default getAlphaHashThreshold;
