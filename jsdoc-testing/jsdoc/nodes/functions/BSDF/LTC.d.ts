189a2364b178f144b152b555402a9b087a96903c
Nathan Bierema <nbierema@gmail.com>
2025-08-19 16:43:16 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/nodes/functions/BSDF/LTC.d.ts
@@ -0,0 +1,7 @@
+export function LTC_Evaluate(...params: any[]): any;
+export namespace LTC_Evaluate { }
+export function LTC_Evaluate(...params: any[]): any;
+export namespace LTC_Evaluate { }
+export function LTC_Evaluate(...params: any[]): any;
+export namespace LTC_Evaluate { }
+export { LTC_Evaluate_Volume, LTC_Uv };
