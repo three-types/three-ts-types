4755754183a07e675109a7c188a861012dd2e94c
Nathan Bierema <nbierema@gmail.com>
2025-08-22 18:17:39 -0400

    Format

--- types/three/examples/jsm/tsl/display/DepthOfFieldNode.d.ts
+++ types/three/examples/jsm/tsl/display/DepthOfFieldNode.d.ts
@@ -9,7 +9,13 @@ declare class DepthOfFieldNode extends TempNode {
     focalLengthNode: Node;
     bokehScaleNode: Node;
 
-    constructor(textureNode: TextureNode, viewZNode: Node, focusDistanceNode: Node, focalLengthNode: Node, bokehScaleNode: Node);
+    constructor(
+        textureNode: TextureNode,
+        viewZNode: Node,
+        focusDistanceNode: Node,
+        focalLengthNode: Node,
+        bokehScaleNode: Node,
+    );
 }
 
 export default DepthOfFieldNode;
