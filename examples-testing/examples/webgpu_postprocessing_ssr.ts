243a2ae550af962d43835e3cf6d0334755486af3
Nathan Bierema <nbierema@gmail.com>
2025-08-23 19:45:59 -0400

    Add examples

---
+++ examples-testing/examples/webgpu_postprocessing_ssr.ts
@@ -0,0 +1,201 @@
+import * as THREE from 'three/webgpu';
+import {
+    pass,
+    mrt,
+    output,
+    normalView,
+    metalness,
+    roughness,
+    blendColor,
+    screenUV,
+    color,
+    sample,
+    directionToColor,
+    colorToDirection,
+    vec2,
+} from 'three/tsl';
+import { ssr } from 'three/addons/tsl/display/SSRNode.js';
+import { smaa } from 'three/addons/tsl/display/SMAANode.js';
+
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import Stats from 'three/addons/libs/stats.module.js';
+
+const params = {
+    quality: 0.5,
+    blurQuality: 2,
+    maxDistance: 0.5,
+    opacity: 1,
+    thickness: 0.015,
+    roughness: 1,
+    enabled: true,
+};
+
+let camera, scene, model, renderer, postProcessing, ssrPass;
+let gui, stats, controls;
+
+init();
+
+async function init() {
+    camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 50);
+    camera.position.set(3, 2, 3);
+
+    scene = new THREE.Scene();
+    scene.backgroundNode = screenUV.distance(0.5).remap(0, 0.5).mix(color(0x888877), color(0x776666));
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('jsm/libs/draco/');
+    dracoLoader.setDecoderConfig({ type: 'js' });
+
+    const loader = new GLTFLoader();
+    loader.setDRACOLoader(dracoLoader);
+    loader.load('models/gltf/steampunk_camera.glb', function (gltf) {
+        model = gltf.scene;
+
+        model.traverse(function (object) {
+            if (object.material) {
+                if (object.material.name === 'Lense_Casing') {
+                    object.material.transparent = true;
+                }
+
+                // Avoid overdrawing
+                object.material.side = THREE.FrontSide;
+            }
+        });
+
+        model.position.y = 0.1;
+        scene.add(model);
+    });
+
+    //
+
+    renderer = new THREE.WebGPURenderer();
+    // renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    document.body.appendChild(renderer.domElement);
+
+    await renderer.init();
+
+    const environment = new RoomEnvironment();
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+    scene.environment = pmremGenerator.fromScene(environment).texture;
+    scene.environmentIntensity = 1.25;
+    pmremGenerator.dispose();
+
+    //
+
+    postProcessing = new THREE.PostProcessing(renderer);
+
+    const scenePass = pass(scene, camera);
+    scenePass.setMRT(
+        mrt({
+            output: output,
+            normal: directionToColor(normalView),
+            metalrough: vec2(metalness, roughness), // pack metalness and roughness into a single attachement
+        }),
+    );
+
+    const scenePassColor = scenePass.getTextureNode('output');
+    const scenePassNormal = scenePass.getTextureNode('normal');
+    const scenePassDepth = scenePass.getTextureNode('depth');
+    const scenePassMetalRough = scenePass.getTextureNode('metalrough');
+
+    // optional: optimize bandwidth by reducing the texture precision for normals and metal/roughness
+
+    const normalTexture = scenePass.getTexture('normal');
+    normalTexture.type = THREE.UnsignedByteType;
+
+    const metalRoughTexture = scenePass.getTexture('metalrough');
+    metalRoughTexture.type = THREE.UnsignedByteType;
+
+    const sceneNormal = sample(uv => {
+        return colorToDirection(scenePassNormal.sample(uv));
+    });
+
+    //
+
+    ssrPass = ssr(scenePassColor, scenePassDepth, sceneNormal, scenePassMetalRough.r, scenePassMetalRough.g);
+
+    // blend SSR over beauty
+
+    const outputNode = smaa(blendColor(scenePassColor, ssrPass));
+
+    postProcessing.outputNode = outputNode;
+
+    //
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.update();
+
+    // stats
+
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    window.addEventListener('resize', onWindowResize);
+
+    // GUI
+
+    gui = new GUI();
+    const ssrFolder = gui.addFolder('SSR');
+    ssrFolder.add(params, 'quality').min(0).max(1).onChange(updateParameters);
+    ssrFolder.add(params, 'blurQuality').min(1).max(3).step(1).onChange(updateParameters);
+    ssrFolder.add(params, 'maxDistance').min(0).max(1).onChange(updateParameters);
+    ssrFolder.add(params, 'opacity').min(0).max(1).onChange(updateParameters);
+    ssrFolder.add(params, 'thickness').min(0).max(0.05).onChange(updateParameters);
+    ssrFolder.add(params, 'enabled').onChange(() => {
+        if (params.enabled === true) {
+            postProcessing.outputNode = outputNode;
+        } else {
+            postProcessing.outputNode = scenePass;
+        }
+
+        postProcessing.needsUpdate = true;
+    });
+    const modelFolder = gui.addFolder('Model');
+    modelFolder
+        .add(params, 'roughness')
+        .min(0)
+        .max(1)
+        .onChange(value => {
+            model.traverse(function (object) {
+                if (object.material) {
+                    object.material.roughness = value;
+                }
+            });
+        });
+
+    updateParameters();
+}
+
+function updateParameters() {
+    ssrPass.quality.value = params.quality;
+    ssrPass.blurQuality.value = params.blurQuality;
+    ssrPass.maxDistance.value = params.maxDistance;
+    ssrPass.opacity.value = params.opacity;
+    ssrPass.thickness.value = params.thickness;
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    stats.begin();
+
+    controls.update();
+
+    postProcessing.render();
+
+    stats.end();
+}
