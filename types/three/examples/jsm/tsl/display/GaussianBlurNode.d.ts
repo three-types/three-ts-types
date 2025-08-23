13a300548914745a050e2a27ee60c29724450412
Nathan Bierema <nbierema@gmail.com>
2025-08-22 21:27:30 -0400

    TSL: Align API of blur filters and deprecated premultipliedGaussianBlur()

--- types/three/examples/jsm/tsl/display/GaussianBlurNode.d.ts
+++ types/three/examples/jsm/tsl/display/GaussianBlurNode.d.ts
@@ -1,6 +1,11 @@
 import { ShaderNodeObject } from "three/tsl";
 import { Node, TempNode, TextureNode, Vector2 } from "three/webgpu";
 
+export interface GaussianBlurNodeOptions {
+    premultipliedAlpha?: boolean | undefined;
+    resolution?: Vector2 | undefined;
+}
+
 declare class GaussianBlurNode extends TempNode {
     textureNode: TextureNode;
     directionNode: Node | null;
@@ -10,11 +15,12 @@ declare class GaussianBlurNode extends TempNode {
 
     premultipliedAlpha: boolean;
 
-    constructor(textureNode: TextureNode, directionNode?: Node | null, sigma?: number);
-
-    setPremultipliedAlpha(value: boolean): this;
-
-    getPremultipliedAlpha(): boolean;
+    constructor(
+        textureNode: TextureNode,
+        directionNode?: Node | null,
+        sigma?: number,
+        options?: GaussianBlurNodeOptions,
+    );
 
     setSize(width: number, height: number): void;
 
@@ -27,7 +33,12 @@ export const gaussianBlur: (
     node: Node,
     directionNode?: Node | number | null,
     sigma?: number,
+    options?: GaussianBlurNodeOptions,
 ) => ShaderNodeObject<GaussianBlurNode>;
+
+/**
+ * @deprecated "premultipliedGaussianBlur()" is deprecated. Use "gaussianBlur()" with "premultipliedAlpha: true" option instead.
+ */
 export const premultipliedGaussianBlur: (
     node: Node,
     directionNode?: Node | number | null,
