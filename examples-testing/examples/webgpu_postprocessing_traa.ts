0f43a1f42635adebc8e8c20006dadd56f46a06f0
Nathan Bierema <nbierema@gmail.com>
2025-08-23 18:50:32 -0400

    Add examples

---
+++ examples-testing/examples/webgpu_postprocessing_traa.ts
@@ -0,0 +1,96 @@
+import * as THREE from 'three/webgpu';
+import { mrt, output, pass, velocity } from 'three/tsl';
+import { traa } from 'three/addons/tsl/display/TRAANode.js';
+
+import Stats from 'three/addons/libs/stats.module.js';
+
+let camera, scene, renderer, postProcessing;
+let stats;
+let index = 0;
+
+init();
+
+function init() {
+    renderer = new THREE.WebGPURenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
+    camera.position.z = 2.5;
+
+    scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry();
+    const material1 = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+
+    const mesh1 = new THREE.Mesh(geometry, material1);
+    mesh1.position.x = -1;
+    scene.add(mesh1);
+
+    const texture = new THREE.TextureLoader().load('textures/brick_diffuse.jpg');
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+    texture.generateMipmaps = false;
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    const material2 = new THREE.MeshBasicMaterial({ map: texture });
+
+    const mesh2 = new THREE.Mesh(geometry, material2);
+    mesh2.position.x = 1;
+    scene.add(mesh2);
+
+    // postprocessing
+
+    postProcessing = new THREE.PostProcessing(renderer);
+    const scenePass = pass(scene, camera);
+    scenePass.setMRT(
+        mrt({
+            output: output,
+            velocity: velocity,
+        }),
+    );
+
+    const scenePassColor = scenePass.getTextureNode('output');
+    const scenePassDepth = scenePass.getTextureNode('depth');
+    const scenePassVelocity = scenePass.getTextureNode('velocity');
+
+    const traaNode = traa(scenePassColor, scenePassDepth, scenePassVelocity, camera);
+
+    postProcessing.outputNode = traaNode;
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+}
+
+function animate() {
+    index++;
+
+    if (Math.round(index / 200) % 2 === 0) {
+        for (let i = 0; i < scene.children.length; i++) {
+            const child = scene.children[i];
+
+            child.rotation.x += 0.005;
+            child.rotation.y += 0.01;
+        }
+    }
+
+    postProcessing.render();
+
+    stats.update();
+}
