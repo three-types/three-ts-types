6b2b2c5ff92796bc027cb54c11cd4dd47d82cf66
Nathan Bierema <nbierema@gmail.com>
2025-08-29 11:20:02 -0400

    Update patch and delete src

--- src-testing/src/nodes/utils/Timer.d.ts
+++
@@ -1,6 +0,0 @@
-import Node from "../core/Node.js";
-import { ShaderNodeObject } from "../tsl/TSLCore.js";
-
-export const time: ShaderNodeObject<Node>;
-export const deltaTime: ShaderNodeObject<Node>;
-export const frameId: ShaderNodeObject<Node>;
