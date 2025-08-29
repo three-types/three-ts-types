f1c4ed1d14a457eea90d4300bf93c836dc12fbdf
Nathan Bierema <nbierema@gmail.com>
2025-08-29 11:03:22 -0400

    Examples: Add TSL Procedural Wood Material

---
+++ types/three/examples/jsm/materials/WoodNodeMaterial.d.ts
@@ -0,0 +1,53 @@
+import * as THREE from "three/webgpu";
+
+export type WoodGenus =
+    | "teak"
+    | "walnut"
+    | "white_oak"
+    | "pine"
+    | "poplar"
+    | "maple"
+    | "red_oak"
+    | "cherry"
+    | "cedar"
+    | "mahogany";
+export type WoodFinish = "raw" | "matte" | "semigloss" | "gloss";
+
+export const WoodGenuses: WoodGenus[];
+export const Finishes: WoodFinish[];
+
+export interface WoodParameters {
+    grainPosition: THREE.Vector3;
+    grainRotation: THREE.Vector3;
+    centerSize: number;
+    largeWarpScale: number;
+    largeGrainStretch: number;
+    smallWarpStrength: number;
+    smallWarpScale: number;
+    fineWarpStrength: number;
+    fineWarpScale: number;
+    ringCount: number;
+    ringBias: number;
+    ringSizeVariance: number;
+    ringVarianceScale: number;
+    barkThickness: number;
+    splotchScale: number;
+    splotchIntensity: number;
+    cellScale: number;
+    cellSize: number;
+    darkGrainColor: string;
+    lightGrainColor: string;
+    genus: WoodGenus;
+    finish: WoodFinish;
+    clearcoat: number;
+    clearcoatRoughness: number;
+    clearcoatDarken: number;
+}
+
+export function GetWoodPreset(genus: WoodGenus, finish: WoodFinish): WoodParameters;
+
+export class WoodNodeMaterial extends THREE.MeshPhysicalNodeMaterial {
+    constructor(params?: Partial<WoodParameters>);
+
+    static fromPreset(genus?: WoodGenus, finish?: WoodFinish);
+}
