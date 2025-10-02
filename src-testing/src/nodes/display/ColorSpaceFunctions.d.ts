e7d26e1e79a6a41ab94e099d30700ad2b1353f7d
Nathan Bierema <nbierema@gmail.com>
2025-10-01 20:46:18 -0400

    Update patch and delete src

--- src-testing/src/nodes/display/ColorSpaceFunctions.d.ts
+++
@@ -1,6 +0,0 @@
-import Node from "../core/Node.js";
-import { ShaderNodeObject } from "../tsl/TSLCore.js";
-
-export const sRGBTransferEOTF: (color: Node) => ShaderNodeObject<Node>;
-
-export const sRGBTransferOETF: (color: Node) => ShaderNodeObject<Node>;
