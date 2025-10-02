6dcbb5c0bb0df371f10b7a3aa05863d5bc51564f
Nathan Bierema <nbierema@gmail.com>
2025-10-01 20:20:59 -0400

    Update

---
+++ types/three/examples/jsm/tsl/display/SSGINode.d.ts
@@ -0,0 +1,31 @@
+import { ShaderNodeObject } from "three/tsl";
+import { Node, PerspectiveCamera, TempNode, UniformNode } from "three/webgpu";
+
+declare class SSGINode extends TempNode {
+    beautyNode: Node;
+    depthNode: Node;
+    normalNode: Node;
+
+    sliceCount: UniformNode<number>;
+    stepCount: UniformNode<number>;
+    aoIntensity: UniformNode<number>;
+    giIntensity: UniformNode<number>;
+    radius: UniformNode<number>;
+    useScreenSpaceSampling: UniformNode<boolean>;
+    expFactor: UniformNode<number>;
+    thickness: UniformNode<number>;
+    useLinearThickness: UniformNode<boolean>;
+    backfaceLighting: UniformNode<number>;
+    useTemporalFiltering: boolean;
+
+    constructor(beautyNode: Node, depthNode: Node, normalNode: Node, camera: PerspectiveCamera);
+}
+
+export default SSGINode;
+
+export const ssgi: (
+    beautyNode: Node,
+    depthNode: Node,
+    normalNode: Node,
+    camera: PerspectiveCamera,
+) => ShaderNodeObject<SSGINode>;
