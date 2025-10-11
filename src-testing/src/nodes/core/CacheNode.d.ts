c1c2cfeaf1cca73e2725d424b7af5cf176c408a8
Nathan Bierema <nbierema@gmail.com>
2025-10-11 11:14:46 -0400

    Add src

---
+++ src-testing/src/nodes/core/CacheNode.d.ts
@@ -0,0 +1,22 @@
+import { ShaderNodeObject } from "../tsl/TSLCore.js";
+import ContextNode from "./ContextNode.js";
+import Node from "./Node.js";
+import NodeCache from "./NodeCache.js";
+
+export default class CacheNode extends Node {
+    node: Node;
+    parent: boolean;
+
+    readonly isCacheNode: true;
+
+    constructor(node: Node, parent?: boolean);
+}
+
+export const cache: (node: Node, cache?: NodeCache) => ShaderNodeObject<CacheNode>;
+
+declare module "../Nodes.js" {
+    interface Node {
+        cache: (cache?: NodeCache) => CacheNode;
+        cacheAssign: (cache?: NodeCache) => this;
+    }
+}
---
+++ src-testing/src/nodes/utils/RemapNode.d.ts
@@ -0,0 +1,59 @@
+import Node from "../core/Node.js";
+import { ShaderNodeObject } from "../tsl/TSLCore.js";
+
+export default class RemapNode extends Node {
+    node: Node;
+    inLowNode: Node;
+    inHighNode: Node;
+    outLowNode: Node;
+    outHighNode: Node;
+
+    doClamp: boolean;
+
+    constructor(node: Node, inLowNode: Node, inHighNode: Node, outLowNode?: Node, outHighNode?: Node);
+}
+
+export const remap: (
+    node: Node,
+    inLowNode: Node | number,
+    inHighNode: Node | number,
+    outLowNode?: Node | number,
+    outHighNode?: Node | number,
+) => ShaderNodeObject<RemapNode>;
+export const remapClamp: (
+    node: Node,
+    inLowNode: Node | number,
+    inHighNode: Node | number,
+    outLowNode?: Node | number,
+    outHighNode?: Node | number,
+) => ShaderNodeObject<RemapNode>;
+
+declare module "../Nodes.js" {
+    interface Node {
+        remap: (
+            inLowNode: Node | number,
+            inHighNode: Node | number,
+            outLowNode?: Node | number,
+            outHighNode?: Node | number,
+        ) => RemapNode;
+        remapAssign: (
+            inLowNode: Node | number,
+            inHighNode: Node | number,
+            outLowNode?: Node | number,
+            outHighNode?: Node | number,
+        ) => this;
+
+        remapClamp: (
+            inLowNode: Node | number,
+            inHighNode: Node | number,
+            outLowNode?: Node | number,
+            outHighNode?: Node | number,
+        ) => RemapNode;
+        remapClampAssign: (
+            inLowNode: Node | number,
+            inHighNode: Node | number,
+            outLowNode?: Node | number,
+            outHighNode?: Node | number,
+        ) => this;
+    }
+}
