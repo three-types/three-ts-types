bd70d967e8e4e800f0ddf8d535c072eff112ca51
Nathan Bierema <nbierema@gmail.com>
2025-10-13 19:16:44 -0400

    Add src

---
+++ src-testing/src/nodes/functions/ShadowMaskModel.d.ts
@@ -0,0 +1,8 @@
+import LightingModel from "../core/LightingModel.js";
+import VarNode from "../core/VarNode.js";
+
+export default class ShadowMaskModel extends LightingModel {
+    shadowNode: VarNode;
+
+    constructor();
+}
