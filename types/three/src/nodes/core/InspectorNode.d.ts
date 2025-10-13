3f9cdd0091e6103bc6fb7006ae168792efe96d8b
Nathan Bierema <nbierema@gmail.com>
2025-10-13 08:58:47 -0400

    Update patch and delete examples

--- types/three/src/nodes/core/InspectorNode.d.ts
+++ types/three/src/nodes/core/InspectorNode.d.ts
@@ -1,4 +1,3 @@
-import { ShaderNodeObject } from "../tsl/TSLCore.js";
 import Node from "./Node.js";
 
 declare class InspectorNode extends Node {
@@ -7,10 +6,11 @@ declare class InspectorNode extends Node {
 
 export default InspectorNode;
 
-export function inspector(node: Node, name?: string, callback?: (node: Node) => Node): ShaderNodeObject<Node>;
+export function inspector<T extends Node>(node: T, name?: string, callback?: (node: T) => Node): T;
 
-declare module "../tsl/TSLCore.js" {
-    interface NodeElements {
-        toInspector: typeof inspector;
+declare module "../Nodes.js" {
+    interface Node {
+        toInspector: (name?: string, callback?: (node: this) => Node) => this;
+        toInspectorAssign: (name?: string, callback?: (node: this) => Node) => this;
     }
 }
