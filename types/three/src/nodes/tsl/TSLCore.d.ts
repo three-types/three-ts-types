4ba26193a313eac80916ffd22b908b3735257736
Nathan Bierema <nbierema@gmail.com>
2025-10-15 09:08:09 -0400

    Format

--- types/three/src/nodes/tsl/TSLCore.d.ts
+++ types/three/src/nodes/tsl/TSLCore.d.ts
@@ -44,7 +44,39 @@ type STPQSwizzle =
 
 export type SwizzleOption = XYZWSwizzle | RGBASwizzle | STPQSwizzle;
 
-export type ArrayElementIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31;
+export type ArrayElementIndex =
+    | 0
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 17
+    | 18
+    | 19
+    | 20
+    | 21
+    | 22
+    | 23
+    | 24
+    | 25
+    | 26
+    | 27
+    | 28
+    | 29
+    | 30
+    | 31;
 
 export type Swizzable =
     & {
