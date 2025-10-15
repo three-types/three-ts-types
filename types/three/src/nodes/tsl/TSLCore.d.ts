a4250fd8876212a5b9ea2b20f8428c904ac09c98
Nathan Bierema <nbierema@gmail.com>
2025-10-15 08:43:50 -0400

    Add index constant

--- types/three/src/nodes/tsl/TSLCore.d.ts
+++ types/three/src/nodes/tsl/TSLCore.d.ts
@@ -37,9 +37,11 @@ type STPQSwizzle =
 
 export type SwizzleOption = XYZWSwizzle | RGBASwizzle | STPQSwizzle;
 
+export type ArrayElementIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31;
+
 export type Swizzable =
     & {
-        [Key in SwizzleOption | number]: Node;
+        [Key in SwizzleOption | ArrayElementIndex]: Node;
     }
     & {
         [Key in SwizzleOption as `set${Uppercase<Key>}`]: (value: Node) => Node;
