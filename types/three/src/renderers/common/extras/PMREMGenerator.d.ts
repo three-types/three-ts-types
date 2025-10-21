666eb5a3369918d29a2202daa99b6da40e7abd51
Nathan Bierema <nbierema@gmail.com>
2025-10-20 21:10:41 -0400

    Format

--- types/three/src/renderers/common/extras/PMREMGenerator.d.ts
+++ types/three/src/renderers/common/extras/PMREMGenerator.d.ts
@@ -1,8 +1,8 @@
 import { RenderTarget } from "../../../core/RenderTarget.js";
 import { Vector3 } from "../../../math/Vector3.js";
 import { Scene } from "../../../scenes/Scene.js";
+import { Texture } from "../../../textures/Texture.js";
 import Renderer from "../Renderer.js";
-import { Texture } from '../../../textures/Texture.js';
 
 export interface PMREMGeneratorOptions {
     size?: number | undefined;
