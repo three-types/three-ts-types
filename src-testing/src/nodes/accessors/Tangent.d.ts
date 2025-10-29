d85e573868a296bbb9eb7c91123348e0e7213ed0
Nathan Bierema <nbierema@gmail.com>
2025-10-28 22:36:51 -0400

    Add src

---
+++ src-testing/src/nodes/accessors/Tangent.d.ts
@@ -0,0 +1,8 @@
+import AttributeNode from "../core/AttributeNode.js";
+import VaryingNode from "../core/VaryingNode.js";
+import MathNode from "../math/MathNode.js";
+
+export const tangentGeometry: AttributeNode;
+export const tangentLocal: VaryingNode;
+export const tangentView: MathNode;
+export const tangentWorld: MathNode;
