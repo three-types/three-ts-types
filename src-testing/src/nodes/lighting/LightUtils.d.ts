a12250068af857bd4975d4d934aeece33bc63615
Nathan Bierema <nbierema@gmail.com>
2025-11-08 18:52:09 -0500

    Update patch and delete src

--- src-testing/src/nodes/lighting/LightUtils.d.ts
+++
@@ -1,7 +0,0 @@
-import Node from "../core/Node.js";
-
-export const getDistanceAttenuation: (args: {
-    lightDistance: Node<"float">;
-    cutoffDistance: Node<"float">;
-    decayExponent: Node<"float">;
-}) => Node<"float">;
