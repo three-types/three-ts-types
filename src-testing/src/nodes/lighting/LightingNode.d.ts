3a244b8c11a2c869ae2aba5d725b1dd75ad50c6a
Nathan Bierema <nbierema@gmail.com>
2025-11-08 18:52:09 -0500

    Update patch and delete src

--- src-testing/src/nodes/lighting/LightingNode.d.ts
+++
@@ -1,7 +0,0 @@
-import Node from "../core/Node.js";
-
-export default abstract class LightingNode extends Node<"vec3"> {
-    readonly isLightingNode: true;
-
-    constructor();
-}
--- src-testing/src/nodes/utils/Packing.d.ts
+++
@@ -1,9 +0,0 @@
-import Node from "../core/Node.js";
-
-interface PackingFunction {
-    (node: Node<"vec3">): Node<"vec3">;
-    (node: Node<"vec4">): Node<"vec4">;
-}
-
-export const directionToColor: PackingFunction;
-export const colorToDirection: PackingFunction;
