5fdc80827e4850b4f71a75a6b9a2c74762e47637
Nathan Bierema <nbierema@gmail.com>
2025-11-10 17:45:01 -0500

    Update patch and delete src

--- src-testing/src/nodes/accessors/TextureBicubic.d.ts
+++
@@ -1,6 +0,0 @@
-import Node from "../core/Node.js";
-import TextureNode from "./TextureNode.js";
-
-export const textureBicubicLevel: (textureNode: TextureNode, lodNode: Node<"float">) => TextureNode;
-
-export const textureBicubic: (textureNode: TextureNode, strength: Node<"float">) => TextureNode;
