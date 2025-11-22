55ace3aad7e589c2e1a623221654ca60e6a4a48d
Nathan Bierema <nbierema@gmail.com>
2025-11-22 10:08:01 -0500

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/nodes/accessors/ReflectVector.d.ts
@@ -0,0 +1,28 @@
+/**
+ * The reflect vector in view space.
+ *
+ * @tsl
+ * @type {Node<vec3>}
+ */
+export const reflectView: Node<vec3>;
+/**
+ * The refract vector in view space.
+ *
+ * @tsl
+ * @type {Node<vec3>}
+ */
+export const refractView: Node<vec3>;
+/**
+ * Used for sampling cube maps when using cube reflection mapping.
+ *
+ * @tsl
+ * @type {Node<vec3>}
+ */
+export const reflectVector: Node<vec3>;
+/**
+ * Used for sampling cube maps when using cube refraction mapping.
+ *
+ * @tsl
+ * @type {Node<vec3>}
+ */
+export const refractVector: Node<vec3>;
