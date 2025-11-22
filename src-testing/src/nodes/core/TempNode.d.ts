011e51dfdce6a337b236ad16ee7092b67c64fc7d
Nathan Bierema <nbierema@gmail.com>
2025-11-22 18:50:26 -0500

    Add src

---
+++ src-testing/src/nodes/core/TempNode.d.ts
@@ -0,0 +1,16 @@
+import Node from "./Node.js";
+import NodeBuilder from "./NodeBuilder.js";
+
+interface TempNodeInterface {
+    isTempNode: true;
+
+    hasDependencies(builder: NodeBuilder): boolean;
+}
+
+declare const TempNode: {
+    new<TNodeValue = unknown>(type: string | null): TempNode<TNodeValue>;
+};
+
+type TempNode<TNodeValue> = Node<TNodeValue> & TempNodeInterface;
+
+export default TempNode;
