f5cf92605291b29df2d60a02473dfe67d7d8936d
Nathan Bierema <nbierema@gmail.com>
2025-11-22 18:50:52 -0500

    Update patch and delete src

--- src-testing/src/nodes/code/ExpressionNode.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ExpressionNode extends Node {
-    snippet: string;
-
-    constructor(snippet?: string, nodeType?: string);
-}
-
-export const expression: (snippet: string, nodeType?: string) => ExpressionNode;
