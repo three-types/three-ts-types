ca0464d09ff436a6d4db996fdb5526a4e8cd84bb
Nathan Bierema <nbierema@gmail.com>
2025-11-22 10:39:13 -0500

    Add src

---
+++ src-testing/src/nodes/accessors/TextureBicubic.d.ts
@@ -0,0 +1,5 @@
+import Node from "../core/Node.js";
+
+export const textureBicubicLevel: (textureNode: Node, lodNode: Node) => Node;
+
+export const textureBicubic: (textureNode: Node, strength: Node) => Node;
