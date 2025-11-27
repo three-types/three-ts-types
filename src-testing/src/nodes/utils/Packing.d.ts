16be1c4ab3b3dac0efd274e4435d4baedb6cb4e7
Nathan Bierema <nbierema@gmail.com>
2025-11-27 15:18:12 -0500

    Update patch and delete src

--- src-testing/src/nodes/utils/Packing.d.ts
+++
@@ -1,5 +0,0 @@
-import Node from "../core/Node.js";
-
-export const directionToColor: (node: Node) => Node;
-export const colorToDirection: (node: Node) => Node;
-export const unpackNormal: (xy: Node) => Node;
--- src-testing/src/nodes/utils/Timer.d.ts
+++
@@ -1,5 +0,0 @@
-import Node from "../core/Node.js";
-
-export const time: Node;
-export const deltaTime: Node;
-export const frameId: Node;
