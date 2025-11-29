893ea820fdec181089f131424857a88392a46aa7
Nathan Bierema <nbierema@gmail.com>
2025-11-29 14:47:46 -0500

    Add src

---
+++ src-testing/src/nodes/accessors/Position.d.ts
@@ -0,0 +1,9 @@
+import Node from "../core/Node.js";
+
+export const positionGeometry: Node;
+export const positionLocal: Node;
+export const positionPrevious: Node;
+export const positionWorld: Node;
+export const positionWorldDirection: Node;
+export const positionView: Node;
+export const positionViewDirection: Node;
