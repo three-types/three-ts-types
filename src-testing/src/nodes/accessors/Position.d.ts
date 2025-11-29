4686209a2b4ce0b1fa6354356d377ee84939a9ce
Nathan Bierema <nbierema@gmail.com>
2025-11-29 14:56:22 -0500

    Update patch and delete src

--- src-testing/src/nodes/accessors/Position.d.ts
+++
@@ -1,9 +0,0 @@
-import Node from "../core/Node.js";
-
-export const positionGeometry: Node;
-export const positionLocal: Node;
-export const positionPrevious: Node;
-export const positionWorld: Node;
-export const positionWorldDirection: Node;
-export const positionView: Node;
-export const positionViewDirection: Node;
