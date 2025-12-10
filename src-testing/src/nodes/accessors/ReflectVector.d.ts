c104f0715c2443c2015c557584da601d01686ec6
Nathan Bierema <nbierema@gmail.com>
2025-12-10 15:51:15 -0500

    Add src

---
+++ src-testing/src/nodes/accessors/ReflectVector.d.ts
@@ -0,0 +1,8 @@
+import Node from "../core/Node.js";
+import VarNode from "../core/VarNode.js";
+
+export const reflectView: Node;
+export const refractView: Node;
+
+export const reflectVector: VarNode;
+export const refractVector: VarNode;
