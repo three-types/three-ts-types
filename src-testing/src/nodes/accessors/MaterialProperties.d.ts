d82ae45842f13b47da338320ade9d7c95f072bc1
Nathan Bierema <nbierema@gmail.com>
2025-12-29 12:35:40 -0500

    Update patch and delete src

--- src-testing/src/nodes/accessors/MaterialProperties.d.ts
+++
@@ -1,8 +0,0 @@
-import { Matrix4 } from "../../math/Matrix4.js";
-import UniformNode from "../core/UniformNode.js";
-
-export const materialRefractionRatio: UniformNode<number>;
-
-export const materialEnvIntensity: UniformNode<number>;
-
-export const materialEnvRotation: UniformNode<Matrix4>;
