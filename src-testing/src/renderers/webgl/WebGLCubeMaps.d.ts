4137ad31aa25e656c1a1e6ed5cfc0722195762f2
Nathan Bierema <nbierema@gmail.com>
2025-12-29 18:25:50 -0500

    Update patch and delete src

--- src-testing/src/renderers/webgl/WebGLCubeMaps.d.ts
+++
@@ -1,9 +0,0 @@
-import { Texture } from "../../textures/Texture.js";
-import { WebGLRenderer } from "../WebGLRenderer.js";
-
-export class WebGLCubeMaps {
-    constructor(renderer: WebGLRenderer);
-
-    get(texture: Texture): Texture | null;
-    dispose(): void;
-}
