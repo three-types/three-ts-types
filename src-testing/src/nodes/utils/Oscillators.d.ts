ebc0c7ede5df5bc5121fc2e915fcd6a686af57e6
Nathan Bierema <nbierema@gmail.com>
2025-12-29 16:10:19 -0500

    Add src

---
+++ src-testing/src/nodes/utils/Oscillators.d.ts
@@ -0,0 +1,6 @@
+import Node from "../core/Node.js";
+
+export const oscSine: (timeNode?: Node) => Node;
+export const oscSquare: (timeNode?: Node) => Node;
+export const oscTriangle: (timeNode?: Node) => Node;
+export const oscSawtooth: (timeNode?: Node) => Node;
