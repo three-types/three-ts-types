c6d0af6c260b1c55288c64b11acf81c7f63088b3
Nathan Bierema <nbierema@gmail.com>
2026-01-15 21:42:15 -0500

    Type TSL flip methods (#1972)
    * Type flip methods
    
    * Add examples
    
    * Type uv()
    
    * Delete examples
    
    * Add src
    
    * Update patch and delete src
    
    * Update declarations
    
    * Add flip methods

---
+++ swizzle-generator.js
@@ -0,0 +1,160 @@
+const swizzleA = ["x", "y", "z", "w"];
+const swizzleB = ["r", "g", "b", "a"];
+const swizzleC = ["s", "t", "p", "q"];
+
+let swizzleOptions = [];
+for (let i = 0; i < 4; i++) {
+    swizzleOptions[i] = [];
+    for (let j = 0; j < 4; j++) {
+        swizzleOptions[i][j] = [];
+    }
+}
+
+let swizzleOptionsNoRepetition = [];
+for (let i = 0; i < 4; i++) {
+    swizzleOptionsNoRepetition[i] = [];
+    for (let j = 0; j < 4; j++) {
+        swizzleOptionsNoRepetition[i][j] = [];
+    }
+}
+
+let swizzleOptionsNoRepetitionOrdered = [];
+for (let i = 0; i < 4; i++) {
+    swizzleOptionsNoRepetitionOrdered[i] = [];
+    for (let j = 0; j < 4; j++) {
+        swizzleOptionsNoRepetitionOrdered[i][j] = [];
+    }
+}
+
+function setProtoSwizzle(a, b, c, d) {
+    let prop = swizzleA[a];
+    let altA = swizzleB[a];
+    let altB = swizzleC[a];
+
+    if (b != null) {
+        prop += swizzleA[b];
+        altA += swizzleB[b];
+        altB += swizzleC[b];
+    }
+
+    if (c != null) {
+        prop += swizzleA[c];
+        altA += swizzleB[c];
+        altB += swizzleC[c];
+    }
+
+    if (d != null) {
+        prop += swizzleA[d];
+        altA += swizzleB[d];
+        altB += swizzleC[d];
+    }
+
+    const validChars = [a, b, c, d].filter(x => x != null);
+    const numChars = validChars.length;
+    const maxCharNum = Math.max(...validChars);
+
+    swizzleOptions[numChars - 1][maxCharNum].push(prop);
+    swizzleOptions[numChars - 1][maxCharNum].push(altA);
+    swizzleOptions[numChars - 1][maxCharNum].push(altB);
+
+    if (b == null || b !== a) {
+        if (c == null || c !== a && c !== b) {
+            if (d == null || d !== c && d !== b && d !== a) {
+                swizzleOptionsNoRepetition[numChars - 1][maxCharNum].push(prop);
+                swizzleOptionsNoRepetition[numChars - 1][maxCharNum].push(altA);
+                swizzleOptionsNoRepetition[numChars - 1][maxCharNum].push(altB);
+            }
+        }
+    }
+
+    if (b == null || a < b) {
+        if (c == null || a < c && b < c) {
+            if (d == null || c < d && b < d && a < d) {
+                swizzleOptionsNoRepetitionOrdered[numChars - 1][maxCharNum].push(prop);
+                swizzleOptionsNoRepetitionOrdered[numChars - 1][maxCharNum].push(altA);
+                swizzleOptionsNoRepetitionOrdered[numChars - 1][maxCharNum].push(altB);
+            }
+        }
+    }
+}
+
+for (let a = 0; a < 4; a++) {
+    setProtoSwizzle(a, null, null, null);
+    for (let b = 0; b < 4; b++) {
+        setProtoSwizzle(a, b, null, null);
+        for (let c = 0; c < 4; c++) {
+            setProtoSwizzle(a, b, c, null);
+            for (let d = 0; d < 4; d++) {
+                setProtoSwizzle(a, b, c, d);
+            }
+        }
+    }
+}
+
+const accessType = [
+    "Node<TNumOrBool>",
+    "Node<NumOrBoolToVec2<TNumOrBool>>",
+    "Node<NumOrBoolToVec3<TNumOrBool>>",
+    "Node<NumOrBoolToVec4<TNumOrBool>>",
+];
+
+const assignType = [
+    "NumOrBool<TNumOrBool>",
+    "Node<NumOrBoolToVec2<TNumOrBool>> | NumOrBool<TNumOrBool>",
+    "Node<NumOrBoolToVec3<TNumOrBool>> | NumOrBool<TNumOrBool>",
+    "Node<NumOrBoolToVec4<TNumOrBool>> | NumOrBool<TNumOrBool>",
+];
+
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+        let interfaceLine = `interface Swizzle${i + 1}From${j + 1}<TNumOrBool extends NumOrBoolType>`;
+        if (j > 0) interfaceLine += ` extends Swizzle${i + 1}From${j}<TNumOrBool>`;
+        interfaceLine += " {";
+        console.log(interfaceLine);
+        const arr = swizzleOptions[i][j];
+        const arrNoRepetition = swizzleOptionsNoRepetition[i][j];
+        for (const val of arr) {
+            console.log(`    get ${val}(): ${accessType[i]};`);
+            if (arrNoRepetition.includes(val)) {
+                console.log(`    set ${val}(value: ${assignType[i]});`);
+            }
+        }
+        console.log("}");
+        console.log();
+    }
+}
+
+for (let i = 0; i < 4; i++) {
+    console.log(`interface SetSwizzle${i + 1}<TNumOrBool extends NumOrBoolType> {`);
+    for (let j = 0; j <= i; j++) {
+        for (let k = 0; k <= i; k++) {
+            const arr = swizzleOptionsNoRepetitionOrdered[j][k];
+            for (const val of arr) {
+                console.log(`    set${val.toUpperCase()}(value: ${assignType[j]}): ${accessType[i]};`);
+            }
+        }
+    }
+    console.log("}");
+    console.log();
+}
+
+const flipAccessType = [
+    "Node<\"float\">",
+    "Node<\"vec2\">",
+    "Node<\"vec3\">",
+    "Node<\"vec4\">",
+];
+
+for (let i = 0; i < 4; i++) {
+    console.log(`interface FlipSwizzle${i + 1} {`);
+    for (let j = 0; j <= i; j++) {
+        for (let k = 0; k <= i; k++) {
+            const arr = swizzleOptionsNoRepetitionOrdered[j][k];
+            for (const val of arr) {
+                console.log(`    flip${val.toUpperCase()}(): ${flipAccessType[i]};`);
+            }
+        }
+    }
+    console.log("}");
+    console.log();
+}
--- types/three/src/nodes/core/AttributeNode.d.ts
+++ types/three/src/nodes/core/AttributeNode.d.ts
@@ -1,13 +1,19 @@
 import Node from "./Node.js";
 import NodeBuilder from "./NodeBuilder.js";
 
-export default class AttributeNode extends Node {
-    constructor(attributeName: string, nodeType?: string | null);
-
+interface AttributeNodeInterface {
     setAttributeName(attributeName: string): this;
 
     getAttributeName(builder: NodeBuilder): string;
 }
+
+declare const AttributeNode: {
+    new(attributeName: string, nodeType?: string | null): AttributeNode;
+};
+
+type AttributeNode<TNodeType = unknown> = Node<TNodeType> & AttributeNodeInterface;
+
+export default AttributeNode;
 
 export const attribute: (
     name: string,
