2cc6d444b1f687414d9551bead262c7f08a9f21d
Nathan Bierema <nbierema@gmail.com>
2026-01-17 16:13:09 -0500

    Update UVUtils (safe)

--- types/three/src/nodes/utils/UVUtils.d.ts
+++ types/three/src/nodes/utils/UVUtils.d.ts
@@ -1,6 +1,5 @@
 import ContextNode from "../core/ContextNode.js";
 import Node from "../core/Node.js";
-import OperatorNode from "../math/OperatorNode.js";
 
 export function replaceDefaultUV(callback: (node: Node) => Node, node?: Node | null): ContextNode;
 
@@ -8,10 +7,10 @@ export const rotateUV: (
     uv: Node,
     rotation: Node,
     center?: Node,
-) => OperatorNode;
+) => Node<"vec2">;
 
 export const spherizeUV: (
     uv: Node,
     strength: Node | number,
     center?: Node,
-) => OperatorNode;
+) => Node<"vec2">;
