f7f6dea8fe473f593d40acc72e72f4bb337862a2
Nathan Bierema <nbierema@gmail.com>
2026-01-21 21:33:09 -0500

    Updates

--- types/three/src/nodes/fog/Fog.d.ts
+++ types/three/src/nodes/fog/Fog.d.ts
@@ -2,7 +2,7 @@ import Node from "../core/Node.js";
 
 export const rangeFogFactor: (near: Node | number, far: Node | number) => Node<"float">;
 
-export const densityFogFactor: (density: Node) => Node;
+export const densityFogFactor: (density: Node) => Node<"float">;
 
 export const exponentialHeightFogFactor: (density: Node, height: Node) => Node;
 
