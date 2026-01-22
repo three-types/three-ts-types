5679378cb7bc60e5f1e38c7f6ecf11e745c9717c
Nathan Bierema <nbierema@gmail.com>
2026-01-21 21:47:25 -0500

    Update declarations

--- types/three/src/materials/nodes/NodeMaterial.d.ts
+++ types/three/src/materials/nodes/NodeMaterial.d.ts
@@ -88,7 +88,7 @@ export interface NodeMaterialNodeProperties {
      *
      * @default null
      */
-    colorNode: Node | null;
+    colorNode: Node<"float"> | Node<"vec2"> | Node<"vec3"> | Node<"vec4"> | Node<"color"> | Node | null;
     /**
      * The normals of node materials are by default inferred from the `normalMap`/`normalScale`
      * or `bumpMap`/`bumpScale` properties. This node property allows to overwrite the default
