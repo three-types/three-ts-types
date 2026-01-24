b991004fa95d55b24b095cbac5293193e9c16bf6
Nathan Bierema <nbierema@gmail.com>
2026-01-24 18:38:31 -0500

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/utils/Packing.d.ts
+++
@@ -1,3 +0,0 @@
-export function directionToColor(node: Node<any>): Node<any>;
-export function colorToDirection(node: Node<any>): Node<any>;
-export function unpackNormal(xy: Node<vec2>): Node<any>;
