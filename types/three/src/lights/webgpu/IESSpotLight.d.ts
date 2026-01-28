ba9ad2df0dfde6e4e2a6153a2443ccf1c3e7cc20
Nathan Bierema <nbierema@gmail.com>
2026-01-27 19:15:02 -0500

    Use JSDoc for lights types (#2022)
    * Update
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Check
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Revert "Check"
    
    This reverts commit f2c645355c4b90da46a4086cc6c31cadf8bbe6ed.

--- types/three/src/lights/webgpu/IESSpotLight.d.ts
+++ types/three/src/lights/webgpu/IESSpotLight.d.ts
@@ -1,6 +1,17 @@
 import { Texture } from "../../textures/Texture.js";
 import { SpotLight } from "../SpotLight.js";
 
-export default class IESSpotLight extends SpotLight {
+/**
+ * A IES version of {@link SpotLight}. Can only be used with {@link WebGPURenderer}.
+ */
+declare class IESSpotLight extends SpotLight {
+    /**
+     * TODO
+     *
+     * @default null
+     */
     iesMap: Texture | null;
+    copy(source: IESSpotLight, recursive?: boolean): this;
 }
+
+export default IESSpotLight;
