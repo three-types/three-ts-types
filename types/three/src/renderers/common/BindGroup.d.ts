cd92134469498ae76125575200bb7313a59f3c1e
Nathan Bierema <nbierema@gmail.com>
2026-02-03 08:41:15 -0500

    Update declarations

--- types/three/src/renderers/common/BindGroup.d.ts
+++ types/three/src/renderers/common/BindGroup.d.ts
@@ -1,5 +1,4 @@
 import Binding from "./Binding.js";
-import NodeUniformsGroup from "./nodes/NodeUniformsGroup.js";
 /**
  * A bind group represents a collection of bindings and thus a collection
  * or resources. Bind groups are assigned to pipelines to provide them
@@ -7,9 +6,8 @@ import NodeUniformsGroup from "./nodes/NodeUniformsGroup.js";
  */
 declare class BindGroup {
     name: string;
-    bindings: NodeUniformsGroup[] | Binding[];
+    bindings: Binding[];
     index: number;
-    bindingsReference: NodeUniformsGroup[] | BindGroup;
     id: number;
     /**
      * Constructs a new bind group.
@@ -17,13 +15,7 @@ declare class BindGroup {
      * @param {string} name - The bind group's name.
      * @param {Array<Binding>} bindings - An array of bindings.
      * @param {number} index - The group index.
-     * @param {Array<Binding>} bindingsReference - An array of reference bindings.
      */
-    constructor(
-        name?: string,
-        bindings?: NodeUniformsGroup[],
-        index?: number,
-        bindingsReference?: NodeUniformsGroup[] | BindGroup,
-    );
+    constructor(name?: string, bindings?: Binding[], index?: number);
 }
 export default BindGroup;
