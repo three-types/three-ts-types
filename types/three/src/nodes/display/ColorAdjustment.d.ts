bab85e58558777d726e0ca11d26503f891a8786e
Nathan Bierema <nbierema@gmail.com>
2026-02-03 22:13:01 -0500

    Update

--- types/three/src/nodes/display/ColorAdjustment.d.ts
+++ types/three/src/nodes/display/ColorAdjustment.d.ts
@@ -51,4 +51,4 @@ export const cdl: (
     luminanceCoefficients?: Node,
 ) => Node<"vec4">;
 
-export const posterize: <TNodeType>(sourceNode: Node<TNodeType>, stepsNode: Node) => Node<TNodeType>;
+export const posterize: <TNodeType>(sourceNode: Node<TNodeType>, stepsNode: Node | number) => Node<TNodeType>;
