2bcc85aa34e521a578d6ebdbbedc5a7faf36e01e
Nathan Bierema <nbierema@gmail.com>
2026-02-07 11:02:05 -0500

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/accessors/Arrays.d.ts
+++
@@ -1,2 +0,0 @@
-export function attributeArray(count: number | TypedArray, type?: string | Struct): StorageBufferNode;
-export function instancedArray(count: number | TypedArray, type?: string | Struct): StorageBufferNode;
