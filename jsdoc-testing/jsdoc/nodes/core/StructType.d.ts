31a84fef89979e40b74088ec48fb278ea159d010
Nathan Bierema <nbierema@gmail.com>
2026-02-07 10:15:17 -0500

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/core/StructType.d.ts
+++
@@ -1,7 +0,0 @@
-export default StructType;
-declare class StructType {
-    constructor(name: any, members: any);
-    name: any;
-    members: any;
-    output: boolean;
-}
--- jsdoc-testing/jsdoc/nodes/shapes/Shapes.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generates a circle based on the uv coordinates.
- *
- * @tsl
- * @function
- * @param {Node<vec2>} coord - The uv to generate the circle.
- * @return {Node<float>} The circle shape.
- */
-export const shapeCircle: () => void;
