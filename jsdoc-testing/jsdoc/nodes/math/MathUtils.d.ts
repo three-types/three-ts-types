bbef8cc1ad5accf1e3be7328d961ccf264abfd51
Nathan Bierema <nbierema@gmail.com>
2026-02-10 21:40:02 -0500

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/math/MathUtils.d.ts
+++
@@ -1,4 +0,0 @@
-export function parabola(x: Node<float>, k: Node<float>): Node<float>;
-export function gain(x: Node<float>, k: Node<float>): Node<float>;
-export function pcurve(x: Node<float>, a: Node<float>, b: Node<float>): Node<float>;
-export function sinc(x: Node<float>, k: Node<float>): Node<float>;
