10065fb8632d6253a4a29fb8f13e5188e9ddfcb6
Nathan Bierema <nbierema@gmail.com>
2026-02-21 20:21:27 -0500

    Naming

--- types/three/src/nodes/tsl/TSLCore.d.ts
+++ types/three/src/nodes/tsl/TSLCore.d.ts
@@ -1746,9 +1746,9 @@ interface AbbreviatedLayout {
 
 type Layout = FullLayout | AbbreviatedLayout;
 
-export interface FnNode<Args extends readonly unknown[], TReturn> {
+export interface FnNode<TArgs extends readonly unknown[], TReturn> {
     // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    (...args: Args): TReturn extends void ? ShaderCallNodeInternal<void> : TReturn;
+    (...args: TArgs): TReturn extends void ? ShaderCallNodeInternal<void> : TReturn;
 
     shaderNode: ShaderNodeInternal<TReturn>;
     id: number;
