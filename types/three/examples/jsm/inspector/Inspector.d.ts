47f8a77b87408d93ac34fd1be0ea709d460d5bec
Nathan Bierema <nbierema@gmail.com>
2026-03-14 17:55:40 -0400

    Updates

--- types/three/examples/jsm/inspector/Inspector.d.ts
+++ types/three/examples/jsm/inspector/Inspector.d.ts
@@ -1,10 +1,20 @@
 import { RendererInspector } from "./RendererInspector.js";
 import { ParametersGroup } from "./tabs/Parameters.js";
+import { Tab } from "./ui/Tab.js";
 
 declare class Inspector extends RendererInspector {
     createParameters(name: string): ParametersGroup;
 
     get domElement(): HTMLDivElement;
+
+    hide(): void;
+    show(): void;
+
+    getSize(): { width: number; height: number };
+
+    setActiveTab(tab: Tab): this;
+    addTab(tab: Tab): this;
+    removeTab(tab: Tab): this;
 }
 
 export { Inspector };
