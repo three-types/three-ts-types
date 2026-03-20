909d08f8ecc9f39a0178e1d18f24c3bda87c1651
Nathan Bierema <nbierema@gmail.com>
2026-03-19 21:29:19 -0500

    Inspector: Added Extension support and revisions (#2089)
    * Update three.js
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add examples
    
    * Update patch and delete examples
    
    * Inspector: Added Extension support and revisions
    
    * Update

--- types/three/examples/jsm/inspector/Inspector.d.ts
+++ types/three/examples/jsm/inspector/Inspector.d.ts
@@ -1,3 +1,4 @@
+import { TSLGraphEditor } from "./extensions/tsl-graph/TSLGraphEditor.js";
 import { RendererInspector, RendererInspectorEventMap } from "./RendererInspector.js";
 import { ParametersGroup } from "./tabs/Parameters.js";
 import { Tab } from "./ui/Tab.js";
@@ -11,6 +12,8 @@ declare class Inspector extends RendererInspector<InspectorEventMap> {
 
     get domElement(): HTMLDivElement;
 
+    onExtension(name: "TSL Graph", callback: (extension: TSLGraphEditor) => void): this;
+
     hide(): void;
     show(): void;
 
@@ -19,6 +22,8 @@ declare class Inspector extends RendererInspector<InspectorEventMap> {
     setActiveTab(tab: Tab): this;
     addTab(tab: Tab): this;
     removeTab(tab: Tab): this;
+
+    setActiveExtension(name: "TSL Graph", value: boolean): this;
 }
 
 export { Inspector };
--- types/three/examples/jsm/inspector/extensions/tsl-graph/TSLGraphEditor.d.ts
+++ types/three/examples/jsm/inspector/extensions/tsl-graph/TSLGraphEditor.d.ts
@@ -1,13 +1,13 @@
 import { Material } from "../../../../../src/materials/Material.js";
 import { Scene } from "../../../../../src/scenes/Scene.js";
-import { Tab, TabEventMap } from "../../ui/Tab.js";
+import { Extension, ExtensionEventMap } from "../../Extension.js";
 
-interface TSLGraphEditorEventMap extends TabEventMap {
+interface TSLGraphEditorEventMap extends ExtensionEventMap {
     change: { material: Material };
     remove: { graphId: string };
 }
 
-export class TSLGraphEditor extends Tab<TSLGraphEditorEventMap> {
+export class TSLGraphEditor extends Extension<TSLGraphEditorEventMap> {
     constructor();
 
     get hasGraphs(): boolean;
