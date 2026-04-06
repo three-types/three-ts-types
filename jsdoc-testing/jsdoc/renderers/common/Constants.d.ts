276fe121533e8f60aa413d9d6733c317a6ad88ab
Nathan Bierema <nbierema@gmail.com>
2026-04-06 14:38:43 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/renderers/common/Constants.d.ts
@@ -0,0 +1,9 @@
+export namespace AttributeType {
+    let VERTEX: number;
+    let INDEX: number;
+    let STORAGE: number;
+    let INDIRECT: number;
+}
+export const GPU_CHUNK_BYTES: 16;
+export const BlendColorFactor: 211;
+export const OneMinusBlendColorFactor: 212;
