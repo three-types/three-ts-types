b203f012b4a5659e405423f2fbb3bec8a79e5208
Nathan Bierema <nbierema@gmail.com>
2026-04-11 08:03:32 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/nodes/lighting/IESSpotLightNode.d.ts
+++
@@ -1,9 +0,0 @@
-export default IESSpotLightNode;
-/**
- * An IES version of the default spot light node.
- *
- * @augments SpotLightNode
- */
-declare class IESSpotLightNode extends SpotLightNode {
-}
-import SpotLightNode from './SpotLightNode.js';
