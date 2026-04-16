530adf33b69e486ba91476146706091c8df8a7e1
Nathan Bierema <nbierema@gmail.com>
2026-04-16 14:23:04 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/renderers/common/PostProcessing.d.ts
@@ -0,0 +1,17 @@
+export default PostProcessing;
+/**
+ * @deprecated since r183. Use {@link RenderPipeline} instead. PostProcessing has been renamed to RenderPipeline.
+ *
+ * This class is a wrapper for backward compatibility and will be removed in a future version.
+ */
+declare class PostProcessing extends RenderPipeline {
+    /**
+     * Constructs a new post processing management module.
+     *
+     * @param {Renderer} renderer - A reference to the renderer.
+     * @param {Node<vec4>} outputNode - An optional output node.
+     * @deprecated since r183. Use {@link RenderPipeline} instead.
+     */
+    constructor(renderer: Renderer, outputNode: Node<vec4>);
+}
+import RenderPipeline from './RenderPipeline.js';
