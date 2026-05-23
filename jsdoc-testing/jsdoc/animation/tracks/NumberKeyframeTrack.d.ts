68cb77f88c6d80aeaa0ea3609e43eea709edad36
Nathan Bierema <nbierema@gmail.com>
2026-05-23 10:49:13 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/animation/tracks/NumberKeyframeTrack.d.ts
@@ -0,0 +1,17 @@
+/**
+ * A track for numeric keyframe values.
+ *
+ * @augments KeyframeTrack
+ */
+export class NumberKeyframeTrack extends KeyframeTrack {
+    /**
+     * Constructs a new number keyframe track.
+     *
+     * @param {string} name - The keyframe track's name.
+     * @param {Array<number>} times - A list of keyframe times.
+     * @param {Array<number>} values - A list of keyframe values.
+     * @param {(InterpolateLinear|InterpolateDiscrete|InterpolateSmooth)} [interpolation] - The interpolation type.
+     */
+    constructor(name: string, times: Array<number>, values: Array<number>, interpolation?: (InterpolateLinear | InterpolateDiscrete | InterpolateSmooth));
+}
+import { KeyframeTrack } from '../KeyframeTrack.js';
