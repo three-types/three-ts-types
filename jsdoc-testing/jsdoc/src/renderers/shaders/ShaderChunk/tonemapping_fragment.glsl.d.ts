b94fb14b4b8d61fe84923520ac1feb27297ca16d
Nathan Bierema <nbierema@gmail.com>
2026-05-28 08:03:26 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/tonemapping_fragment.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#if defined( TONE_MAPPING )\n\n\tgl_FragColor.rgb = toneMapping( gl_FragColor.rgb );\n\n#endif\n";
-export default _default;
--- jsdoc-testing/jsdoc/src/renderers/webgl/WebGLAttributes.d.ts
+++
@@ -1,5 +0,0 @@
-export function WebGLAttributes(gl: any): {
-    get: (attribute: any) => any;
-    remove: (attribute: any) => void;
-    update: (attribute: any, bufferType: any) => void;
-};
