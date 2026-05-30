9ea1babdfba86a33971139f4866092636cd6d1c7
Nathan Bierema <nbierema@gmail.com>
2026-05-30 08:07:49 -0400

    USDZLoader: Add onLoad() and onError() to parse(). (#2183)
    * USDZLoader: Add onLoad() and onError() to parse().
    
    * Update three.js

--- types/three/examples/jsm/loaders/USDLoader.d.ts
+++ types/three/examples/jsm/loaders/USDLoader.d.ts
@@ -3,7 +3,7 @@ import { Group, Loader, LoadingManager } from "three";
 declare class USDLoader extends Loader<Group> {
     constructor(manager?: LoadingManager);
 
-    parse(buffer: ArrayBuffer | string): Group;
+    parse(buffer: ArrayBuffer | string, onLoad?: (group: Group) => void, onError?: (err: unknown) => void): Group;
 }
 
 export { USDLoader };
