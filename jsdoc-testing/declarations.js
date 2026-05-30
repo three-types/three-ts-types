471d561f0ead093f7df08624cd914e775f021c8a
Nathan Bierema <nbierema@gmail.com>
2026-05-30 09:31:03 -0400

    Examples: Add TSL function for grounded skyboxes. (#2189)
    * Update three.js
    
    * Update
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add examples
    
    * Update patch and delete examples
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Update three.js

--- jsdoc-testing/declarations.js
+++ jsdoc-testing/declarations.js
@@ -6,6 +6,7 @@ const files = [
     'examples/jsm/csm/CSMFrustum',
     'examples/jsm/lighting/ClusteredLighting',
     'examples/jsm/tsl/lighting/ClusteredLightsNode',
+    'examples/jsm/tsl/utils/GroundedSkybox',
     'examples/jsm/webxr/WebGLXRFallback',
     'src/animation/tracks/BooleanKeyframeTrack',
     'src/animation/tracks/ColorKeyframeTrack',
