06041022bafb3d3810c891e8950608a7ca1f74bf
Nathan Bierema <nbierema@gmail.com>
2026-06-03 07:18:38 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/skinning_vertex.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#ifdef USE_SKINNING\n\n\tvec4 skinVertex = bindMatrix * vec4( transformed, 1.0 );\n\n\tvec4 skinned = vec4( 0.0 );\n\tskinned += boneMatX * skinVertex * skinWeight.x;\n\tskinned += boneMatY * skinVertex * skinWeight.y;\n\tskinned += boneMatZ * skinVertex * skinWeight.z;\n\tskinned += boneMatW * skinVertex * skinWeight.w;\n\n\ttransformed = ( bindMatrixInverse * skinned ).xyz;\n\n#endif\n";
-export default _default;
