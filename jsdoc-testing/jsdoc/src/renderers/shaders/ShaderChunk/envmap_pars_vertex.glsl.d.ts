ec51036f0568497b1b14b2be9f5eef0c55e5f004
Nathan Bierema <nbierema@gmail.com>
2026-06-11 20:57:39 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/envmap_pars_vertex.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#ifdef USE_ENVMAP\n\n\t#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( PHONG ) || defined( LAMBERT )\n\n\t\t#define ENV_WORLDPOS\n\n\t#endif\n\n\t#ifdef ENV_WORLDPOS\n\t\t\n\t\tvarying vec3 vWorldPosition;\n\n\t#else\n\n\t\tvarying vec3 vReflect;\n\t\tuniform float refractionRatio;\n\n\t#endif\n\n#endif\n";
-export default _default;
