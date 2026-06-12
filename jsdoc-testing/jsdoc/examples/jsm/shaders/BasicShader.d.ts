6885df456c5b13eb257fb2a64606b10768a9f177
Nathan Bierema <nbierema@gmail.com>
2026-06-12 10:58:56 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/examples/jsm/shaders/BasicShader.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @module BasicShader
+ * @three_import import { BasicShader } from 'three/addons/shaders/BasicShader.js';
+ */
+/**
+ * Simple shader for testing.
+ *
+ * @constant
+ * @type {ShaderMaterial~Shader}
+ */
+export const BasicShader: ShaderMaterial;
