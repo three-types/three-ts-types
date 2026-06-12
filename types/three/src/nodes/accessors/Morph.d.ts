f1259e3c7d701e79613acf463e5b54a706d9b9de
Nathan Bierema <nbierema@gmail.com>
2026-06-12 11:22:35 -0400

    Morph: Fix individual morphTargetInfluences per object (#2219)
    * Update three.js
    
    * Add jsdoc
    
    * Update patch and delete jsdoc
    
    * Update declarations
    
    * Add examples
    
    * Update patch and delete examples

--- types/three/src/nodes/accessors/Morph.d.ts
+++ types/three/src/nodes/accessors/Morph.d.ts
@@ -1,12 +1,5 @@
 import { Mesh } from "../../objects/Mesh.js";
-import ReferenceNode from "./ReferenceNode.js";
 
-/**
- * TSL object representing a reference to the mesh's morphTargetInfluences array.
- *
- * @type {ReferenceNode<float>}
- */
-export const morphTargetInfluences: ReferenceNode<"float", unknown>;
 /**
  * TSL function representing the vertex shader morph targets blend setup.
  * Dynamically computes morph targets weights and updates positionLocal and normalLocal in-place.
