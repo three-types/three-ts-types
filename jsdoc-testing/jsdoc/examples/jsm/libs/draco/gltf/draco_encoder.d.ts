3ca4bfc84fc7b5fdf295cd44812385e6257801b4
Nathan Bierema <nbierema@gmail.com>
2026-06-12 21:31:21 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/libs/draco/gltf/draco_encoder.d.ts
+++
@@ -1,5 +0,0 @@
-export = DracoEncoderModule;
-declare function DracoEncoderModule(DracoEncoderModule: any, ...args: any[]): any;
-declare namespace DracoEncoderModule {
-    export { DracoEncoderModule };
-}
