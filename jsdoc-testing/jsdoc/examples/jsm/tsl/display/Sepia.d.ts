cb3742ccf76370770bd6210099da86957f6bd340
Nathan Bierema <nbierema@gmail.com>
2026-06-12 21:06:24 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/tsl/display/Sepia.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Applies a sepia effect to the given color node.
- *
- * @tsl
- * @function
- * @param {Node<vec4>} color - The color node to apply the sepia for.
- * @return {Node<vec4>} The updated color node.
- */
-export const sepia: () => void;
