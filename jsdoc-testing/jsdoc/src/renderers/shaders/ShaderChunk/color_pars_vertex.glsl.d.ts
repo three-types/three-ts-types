8810fc0b4e0af0fc8a47eeefd81c07f0ae7a44e8
Nathan Bierema <nbierema@gmail.com>
2026-06-15 08:07:33 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/color_pars_vertex.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#if defined( USE_COLOR ) || defined( USE_COLOR_ALPHA ) || defined( USE_INSTANCING_COLOR ) || defined( USE_BATCHING_COLOR )\n\n\tvarying vec4 vColor;\n\n#endif\n";
-export default _default;
