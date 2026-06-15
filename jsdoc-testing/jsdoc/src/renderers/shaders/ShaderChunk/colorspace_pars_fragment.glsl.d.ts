1db6d699b829b642bb46a46afd10921439b10428
Nathan Bierema <nbierema@gmail.com>
2026-06-15 07:57:42 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/colorspace_pars_fragment.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n\nvec4 LinearTransferOETF( in vec4 value ) {\n\treturn value;\n}\n\nvec4 sRGBTransferEOTF( in vec4 value ) {\n\treturn vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );\n}\n\nvec4 sRGBTransferOETF( in vec4 value ) {\n\treturn vec4( mix( pow( value.rgb, vec3( 0.41666 ) ) * 1.055 - vec3( 0.055 ), value.rgb * 12.92, vec3( lessThanEqual( value.rgb, vec3( 0.0031308 ) ) ) ), value.a );\n}\n\n";
-export default _default;
