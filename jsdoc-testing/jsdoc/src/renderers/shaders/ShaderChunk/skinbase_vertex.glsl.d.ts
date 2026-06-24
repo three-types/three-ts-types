4df8b88fe9f8be466f414d173f19c93132a51100
Nathan Bierema <nbierema@gmail.com>
2026-06-24 19:44:37 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/skinbase_vertex.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#ifdef USE_SKINNING\n\n\tmat4 boneMatX = getBoneMatrix( skinIndex.x );\n\tmat4 boneMatY = getBoneMatrix( skinIndex.y );\n\tmat4 boneMatZ = getBoneMatrix( skinIndex.z );\n\tmat4 boneMatW = getBoneMatrix( skinIndex.w );\n\n#endif\n";
-export default _default;
