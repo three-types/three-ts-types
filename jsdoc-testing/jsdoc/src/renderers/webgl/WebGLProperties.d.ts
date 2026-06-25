4aa68570972b08338f1127099b120596082dc279
Nathan Bierema <nbierema@gmail.com>
2026-06-25 13:42:14 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/webgl/WebGLProperties.d.ts
+++
@@ -1,7 +0,0 @@
-export function WebGLProperties(): {
-    has: (object: any) => boolean;
-    get: (object: any) => any;
-    remove: (object: any) => void;
-    update: (object: any, key: any, value: any) => void;
-    dispose: () => void;
-};
