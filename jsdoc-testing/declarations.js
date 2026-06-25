598d274098683f2eecb50b049527c2064239963c
Nathan Bierema <nbierema@gmail.com>
2026-06-25 08:31:02 -0400

    Update

--- jsdoc-testing/declarations.js
+++ jsdoc-testing/declarations.js
@@ -5,7 +5,7 @@ import { argv } from 'node:process';
 const files = [
     'examples/jsm/csm/CSMFrustum',
     'examples/jsm/generators/city/SidewalkGenerator',
-    'examples/jsm/generators/city/SkyscapeGenerator',
+    'examples/jsm/generators/city/SkyscaperGenerator',
     'examples/jsm/generators/CityGenerator',
     'examples/jsm/geometries/LoftGeometry',
     'examples/jsm/lighting/ClusteredLighting',
