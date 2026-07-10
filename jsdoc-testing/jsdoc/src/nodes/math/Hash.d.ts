6687a160fe2de27fced9cb059c9d2fb9eef8d9d2
Nathan Bierema <nbierema@gmail.com>
2026-07-10 10:12:41 -0500

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/src/nodes/math/Hash.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Generates a hash value in the range `[0, 1]` from the given seed.
+ *
+ * @tsl
+ * @function
+ * @param {Node<float>} seed - The seed.
+ * @return {Node<float>} The hash value.
+ */
+export const hash: () => void;
