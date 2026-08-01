b1bcea3da5f0e8ac61160455289deb1c615956e2
Nathan Bierema <nbierema@gmail.com>
2026-08-01 14:24:29 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/nodes/functions/material/getShIrradianceAt.d.ts
+++
@@ -1,2 +0,0 @@
-export default getShIrradianceAt;
-declare const getShIrradianceAt: () => void;
--- jsdoc-testing/jsdoc/src/renderers/webgl/WebGLGeometries.d.ts
+++
@@ -1,5 +0,0 @@
-export function WebGLGeometries(gl: any, attributes: any, info: any, bindingStates: any): {
-    get: (object: any, geometry: any) => any;
-    update: (geometry: any) => void;
-    getWireframeAttribute: (geometry: any) => any;
-};
