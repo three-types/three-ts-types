0e8c71baf3bc9a826e42926ed41e6b487b116c71
Nathan Bierema <nbierema@gmail.com>
2026-08-01 21:07:05 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/nodes/utils/Oscillators.d.ts
+++
@@ -1,4 +0,0 @@
-export function oscSine(t?: Node<float>): Node<float>;
-export function oscSquare(t?: Node<float>): Node<float>;
-export function oscTriangle(t?: Node<float>): Node<float>;
-export function oscSawtooth(t?: Node<float>): Node<float>;
