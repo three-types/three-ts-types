1d8220268e3ae98732f9f512b7389429e1b92f70
Nathan Bierema <nbierema@gmail.com>
2026-08-01 22:23:25 -0400

    TSL: Add OnBeforeRenderPipeline and OnAfterRenderPipeline

--- types/three/src/nodes/core/NodeBuilder.d.ts
+++ types/three/src/nodes/core/NodeBuilder.d.ts
@@ -2,6 +2,7 @@ import { BufferGeometry } from "../../core/BufferGeometry.js";
 import { Object3D } from "../../core/Object3D.js";
 import { Material } from "../../materials/Material.js";
 import Renderer from "../../renderers/common/Renderer.js";
+import RenderPipeline from "../../renderers/common/RenderPipeline.js";
 
 export default abstract class NodeBuilder {
     object: Object3D;
@@ -9,4 +10,9 @@ export default abstract class NodeBuilder {
     geometry: BufferGeometry;
     renderer: Renderer;
     context: unknown;
+
+    /**
+     * A reference to the render pipeline.
+     */
+    readonly renderPipeline: RenderPipeline;
 }
