bde95016f578ec51d28d5074a137e71d2089ea7e
Nathan Bierema <nbierema@gmail.com>
2026-08-10 21:30:50 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/examples/jsm/shaders/CopyShader.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @module CopyShader
+ * @three_import import { CopyShader } from 'three/addons/shaders/CopyShader.js';
+ */
+/**
+ * Full-screen copy shader pass.
+ *
+ * @constant
+ * @type {ShaderMaterial~Shader}
+ */
+export const CopyShader: ShaderMaterial;
---
+++ jsdoc-testing/jsdoc/examples/jsm/shaders/DotScreenShader.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @module DotScreenShader
+ * @three_import import { DotScreenShader } from 'three/addons/shaders/DotScreenShader.js';
+ */
+/**
+ * Dot screen shader based on [glfx.js sepia shader](https://github.com/evanw/glfx.js).
+ *
+ * @constant
+ * @type {ShaderMaterial~Shader}
+ */
+export const DotScreenShader: ShaderMaterial;
