0d1ffbfc31bb0fb655896c89c012b9f6eae888ee
Nathan Bierema <nbierema@gmail.com>
2026-08-20 21:35:15 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/loaders/materialx/parse/MaterialXParser.d.ts
+++
@@ -1,2 +0,0 @@
-export function parseMaterialXNodeTree(nodeXML: any, createNode: any, addNode: any, nodePath?: string): any;
-export function parseMaterialXText(text: any, createNode: any, addNode: any): any;
--- jsdoc-testing/jsdoc/src/nodes/pmrem/PMREMUtils.d.ts
+++
@@ -1,3 +0,0 @@
-export const textureCubeUV: () => void;
-export const sphericalGaussianBlur: () => void;
-export const ggxConvolution: () => void;
