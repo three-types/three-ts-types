028d65f371d4c8529e1719555abe09d6a59baecb
Nathan Bierema <nbierema@gmail.com>
2026-08-20 22:25:21 -0400

    GaussianSplatMesh: Remove `getSplat()` (#2363)
    * Update three.js
    
    * GaussianSplatMesh: Remove `getSplat()`

--- types/three/examples/jsm/objects/GaussianSplatMesh.d.ts
+++ types/three/examples/jsm/objects/GaussianSplatMesh.d.ts
@@ -2,26 +2,15 @@ import {
     Box3,
     BufferGeometry,
     Camera,
-    Color,
     InstancedBufferGeometry,
-    Matrix3,
     Mesh,
     NodeMaterial,
     Renderer,
     Sphere,
-    Vector3,
 } from "three/webgpu";
 
 export interface GaussianSplatMeshOptions {
     autoSort?: boolean | undefined;
-}
-
-export interface GaussianSplat {
-    position: Vector3;
-    covariance: Matrix3;
-    color: Color;
-    opacity: number;
-    radius: number;
 }
 
 declare class GaussianSplatMesh extends Mesh<InstancedBufferGeometry, NodeMaterial> {
@@ -35,8 +24,6 @@ declare class GaussianSplatMesh extends Mesh<InstancedBufferGeometry, NodeMateri
     autoSort: boolean;
 
     updateSphericalHarmonics(renderer: Renderer, camera: Camera): boolean;
-
-    getSplat(index: number, target?: Partial<GaussianSplat>): GaussianSplat;
 
     computeBoundingBox(): void;
 
