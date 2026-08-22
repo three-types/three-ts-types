93f66cdf183fabb30821b1d07f2b48af217f5853
Nathan Bierema <nbierema@gmail.com>
2026-08-22 11:18:09 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/inspector/extensions/color-grading/modules/ContrastModule.d.ts
+++
@@ -1,7 +0,0 @@
-export class ContrastModule extends Module {
-    constructor(params?: {}, onChange?: null, onRemove?: null, id?: string);
-    contrastControl: HTMLDivElement;
-    pivotControl: HTMLDivElement;
-    domElement: HTMLDivElement;
-}
-import { Module } from './Module.js';
--- jsdoc-testing/jsdoc/examples/jsm/inspector/extensions/color-grading/modules/ExposureModule.d.ts
+++
@@ -1,6 +0,0 @@
-export class ExposureModule extends Module {
-    constructor(params?: {}, onChange?: null, onRemove?: null, id?: string);
-    exposureControl: HTMLDivElement;
-    domElement: HTMLDivElement;
-}
-import { Module } from './Module.js';
