b742e7174d65d8b59cdd9f0d7978bed6afbd0ccb
Nathan Bierema <nbierema@gmail.com>
2026-08-27 18:41:43 -0400

    Add jsdoc

---
+++ jsdoc-testing/jsdoc/examples/jsm/loaders/materialx/parse/MaterialXParser.d.ts
@@ -0,0 +1,2 @@
+export function parseMaterialXNodeTree(nodeXML: any, createNode: any, addNode: any, nodePath?: string): any;
+export function parseMaterialXText(text: any, createNode: any, addNode: any): any;
