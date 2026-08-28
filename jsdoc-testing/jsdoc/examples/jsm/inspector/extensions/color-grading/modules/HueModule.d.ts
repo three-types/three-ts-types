2826b755d84995bd19cecb3fd8aa0fa3f8c0409d
Nathan Bierema <nbierema@gmail.com>
2026-08-28 07:57:12 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/inspector/extensions/color-grading/modules/HueModule.d.ts
+++
@@ -1,6 +0,0 @@
-export class HueModule extends Module {
-    constructor(params?: {}, onChange?: null, onRemove?: null, id?: string);
-    hueShiftControl: HTMLDivElement;
-    domElement: HTMLDivElement;
-}
-import { Module } from './Module.js';
