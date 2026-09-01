69397dca81dd7aa24999fbbaaf078358b08a112f
Nathan Bierema <nbierema@gmail.com>
2026-09-01 08:46:46 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/examples/jsm/inspector/extensions/color-grading/modules/SatVibranceModule.d.ts
+++
@@ -1,7 +0,0 @@
-export class SatVibranceModule extends Module {
-    constructor(params?: {}, onChange?: null, onRemove?: null, id?: string);
-    saturationControl: HTMLDivElement;
-    vibranceControl: HTMLDivElement;
-    domElement: HTMLDivElement;
-}
-import { Module } from './Module.js';
