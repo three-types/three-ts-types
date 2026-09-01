97d855d88657ff875064ff3cab410b8f361dc862
Nathan Bierema <nbierema@gmail.com>
2026-09-01 08:22:28 -0400

    Update patch and delete jsdoc

--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/logdepthbuf_fragment.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#if defined( USE_LOGARITHMIC_DEPTH_BUFFER )\n\n\t// Doing a strict comparison with == 1.0 can cause noise artifacts\n\t// on some platforms. See issue #17623.\n\tgl_FragDepth = vIsPerspective == 0.0 ? gl_FragCoord.z : log2( vFragDepth ) * logDepthBufFC * 0.5;\n\n#endif\n";
-export default _default;
--- jsdoc-testing/jsdoc/src/renderers/shaders/ShaderChunk/logdepthbuf_vertex.glsl.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "\n#ifdef USE_LOGARITHMIC_DEPTH_BUFFER\n\n\tvFragDepth = 1.0 + gl_Position.w;\n\tvIsPerspective = float( isPerspectiveMatrix( projectionMatrix ) );\n\n#endif\n";
-export default _default;
