428a05871bbac3857b9def3877d3866bb617a840
Nathan Bierema <nbierema@gmail.com>
2026-09-07 13:33:41 -0400

    Add Sculptor to Addons declaration

--- types/three/examples/jsm/Addons.d.ts
+++ types/three/examples/jsm/Addons.d.ts
@@ -152,6 +152,7 @@ export * from "./misc/MorphAnimMesh.js";
 export * from "./misc/MorphBlendMesh.js";
 export * from "./misc/ProgressiveLightMap.js";
 export * from "./misc/RollerCoaster.js";
+export * from "./misc/Sculptor.js";
 export * from "./misc/TubePainter.js";
 export * from "./misc/Volume.js";
 export * from "./misc/VolumeSlice.js";
