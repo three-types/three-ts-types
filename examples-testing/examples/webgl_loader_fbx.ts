cb85fb37b11eb8a8181bcc5971af0a70c66575b4
Nathan Bierema <nbierema@gmail.com>
2026-09-07 13:33:47 -0400

    Add examples

---
+++ examples-testing/examples/webgl_loader_fbx.ts
@@ -0,0 +1,217 @@
+import * as THREE from 'three';
+
+import Stats from 'three/addons/libs/stats.module.js';
+
+import { SunLight } from 'three/addons/lights/SunLight.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+const manager = new THREE.LoadingManager();
+
+let camera, scene, renderer, controls, stats, object, loader, guiMorphsFolder, guiAnimationsFolder;
+let mixer;
+
+const timer = new THREE.Timer();
+timer.connect(document);
+
+const params = {
+    asset: 'Samba Dancing',
+};
+
+const assets = [
+    'Samba Dancing',
+    'morph_test',
+    'monkey',
+    'monkey_embedded_texture',
+    'vCube',
+    'archer/ArcherRi01',
+    'warrior/Warrior',
+    'stanford-bunny',
+    'mixamo',
+    'RotationTest',
+    'exampleWindow',
+    'Head_69',
+    'morph-translation',
+    'ball_anims_asc_2018',
+];
+
+const scales = new Map();
+scales.set('warrior/Warrior', 100);
+scales.set('archer/ArcherRi01', 100);
+scales.set('stanford-bunny', 0.001);
+scales.set('Head_69', 100);
+scales.set('ball_anims_asc_2018', 50);
+
+init();
+
+function init() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.set(100, 200, 300);
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
+    hemiLight.position.set(0, 200, 0);
+    scene.add(hemiLight);
+
+    const sunLight = new SunLight(0xffffff, 5);
+    sunLight.position.set(0, 200, 100);
+    sunLight.castShadow = true;
+    scene.add(sunLight);
+
+    // ground
+    const mesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(2000, 2000),
+        new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }),
+    );
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add(mesh);
+
+    const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add(grid);
+
+    loader = new FBXLoader(manager);
+    loadAsset(params.asset);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.shadowMap.enabled = true;
+    container.appendChild(renderer.domElement);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    controls.target.set(0, 100, 0);
+    controls.update();
+
+    window.addEventListener('resize', onWindowResize);
+
+    // stats
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    const gui = new GUI();
+    gui.add(params, 'asset', assets).onChange(function (value) {
+        loadAsset(value);
+    });
+
+    guiMorphsFolder = gui.addFolder('Morphs').hide();
+    guiAnimationsFolder = gui.addFolder('Animations').hide();
+}
+
+function loadAsset(asset) {
+    loader.trimAnimationClips = asset === 'ball_anims_asc_2018';
+
+    loader.load('models/fbx/' + asset + '.fbx', function (group) {
+        if (object) {
+            object.traverse(function (child) {
+                if (child.isSkinnedMesh) {
+                    child.skeleton.dispose();
+                }
+
+                if (child.material) {
+                    const materials = Array.isArray(child.material) ? child.material : [child.material];
+                    materials.forEach(material => {
+                        if (material.map) material.map.dispose();
+                        material.dispose();
+                    });
+                }
+
+                if (child.geometry) child.geometry.dispose();
+            });
+
+            scene.remove(object);
+        }
+
+        //
+
+        object = group;
+
+        const scale = scales.get(asset);
+        object.scale.setScalar(scale || 1);
+
+        guiAnimationsFolder.children.forEach(child => child.destroy());
+        guiAnimationsFolder.hide();
+
+        if (object.animations && object.animations.length) {
+            mixer = new THREE.AnimationMixer(object);
+
+            const action = mixer.clipAction(object.animations[0]);
+            action.play();
+
+            const clips = object.animations;
+            const animationParams = { clip: clips[0].name };
+
+            guiAnimationsFolder.show();
+            guiAnimationsFolder
+                .add(
+                    animationParams,
+                    'clip',
+                    clips.map(clip => clip.name),
+                )
+                .onChange(function (name) {
+                    mixer.stopAllAction();
+
+                    const clip = THREE.AnimationClip.findByName(clips, name);
+                    mixer.clipAction(clip).play();
+                });
+        } else {
+            mixer = null;
+        }
+
+        guiMorphsFolder.children.forEach(child => child.destroy());
+        guiMorphsFolder.hide();
+
+        object.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+
+                if (child.morphTargetDictionary) {
+                    guiMorphsFolder.show();
+                    const meshFolder = guiMorphsFolder.addFolder(child.name || child.uuid);
+                    Object.keys(child.morphTargetDictionary).forEach(key => {
+                        meshFolder.add(child.morphTargetInfluences, child.morphTargetDictionary[key], 0, 1, 0.01);
+                    });
+                }
+            }
+        });
+
+        scene.add(object);
+    });
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//
+
+function animate() {
+    timer.update();
+
+    const delta = timer.getDelta();
+
+    if (mixer) mixer.update(delta);
+
+    controls.update();
+
+    renderer.render(scene, camera);
+
+    stats.update();
+}
