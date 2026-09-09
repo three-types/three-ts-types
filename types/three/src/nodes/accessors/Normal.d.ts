fe53deedeeabba3b20b6d97df003edd9be96fddf
linbingquan <695601626@qq.com>
2026-09-09 16:14:25 +0800

    chore: remove code deprecated in r177

--- types/three/src/nodes/accessors/Normal.d.ts
+++ types/three/src/nodes/accessors/Normal.d.ts
@@ -19,18 +19,3 @@ export const clearcoatNormalView: Node<"vec3">;
 export const transformNormal: (normal: Node, matrix?: Node) => Node<"vec3">;
 
 export const transformNormalToView: (normal: Node) => Node<"vec3">;
-
-/**
- * @deprecated since r178. Use `normalView` instead.
- */
-export const transformedNormalView: Node<"vec3">;
-
-/**
- * @deprecated since r178. Use `normalWorld` instead.
- */
-export const transformedNormalWorld: Node<"vec3">;
-
-/**
- * @deprecated since r178. Use `clearcoatNormalView` instead.
- */
-export const transformedClearcoatNormalView: Node<"vec3">;
